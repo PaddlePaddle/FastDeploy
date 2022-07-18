a34a815de844834bfcacc8154ab206587b9a7b0b
ziqi-jin <ziqijincoder@gmail.com>
2022-07-18 03:14:38 +0000

    python file modify

--- model_zoo/vision/yolov7/yolov7.py
+++ model_zoo/vision/yolov7/yolov7.py
@@ -2,13 +2,13 @@ import fastdeploy as fd
 import cv2
 
 # 下载模型和测试图片
-model_url = "TODO "
-test_jpg_url = "https://github.com/WongKinYiu/yolov7/blob/main/inference/images/horses.jpg"
-fd.download(model_url, ".", show_progress=True)
+# model_url = "TODO "
+test_jpg_url = "https://raw.githubusercontent.com/WongKinYiu/yolov7/main/inference/images/horses.jpg"
+# fd.download(model_url, ".", show_progress=True)
 fd.download(test_jpg_url, ".", show_progress=True)
 
 # 加载模型
-model = fd.vision.wongkinyiu.YOLOv7("yolov7.onnx")
+model = fd.vision.wongkinyiu.YOLOv7("/home/fastdeploy/yolov7/onnxfiles/yolov7.onnx")
 
 # 预测图片
 im = cv2.imread("horses.jpg")
