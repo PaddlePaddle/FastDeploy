5df0ea68603576a4c872db0ddb8239d4629f4e8b
huangjianhui <852142024@qq.com>
2022-07-26 21:21:58 +0800

    Add detection evaluation function (#37)
    * Detection evaluation function
    
    * Add license
    
    Co-authored-by: Jason <jiangjiajun@baidu.com>

--- fastdeploy/vision/evaluation/utils/util.py
+++ fastdeploy/vision/evaluation/utils/util.py
@@ -11,6 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import
-from .classify import eval_classify
-from .detection import eval_detection
+
+import platform
+import multiprocessing as mp
+
+
+def is_pic(img_name):
+    valid_suffix = ['JPEG', 'jpeg', 'JPG', 'jpg', 'BMP', 'bmp', 'PNG', 'png']
+    suffix = img_name.split('.')[-1]
+    if suffix not in valid_suffix:
+        return False
+    return True
+
+
+def get_num_workers(num_workers):
+    if not platform.system() == 'Linux':
+        # Dataloader with multi-process model is not supported
+        # on MacOS and Windows currently.
+        return 0
+    if num_workers == 'auto':
+        num_workers = mp.cpu_count() // 2 if mp.cpu_count() // 2 < 2 else 2
+    return num_workers
