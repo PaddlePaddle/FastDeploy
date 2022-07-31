7de07405dba54ddc5f921740450e73bfbcee4f44
jiangjiajun <jiangjiajun@baidu.com>
2022-07-31 07:04:57 +0000

    fix conflicts

--- fastdeploy/vision/evaluation/detection.py
+++ fastdeploy/vision/evaluation/detection.py
@@ -28,13 +28,6 @@ def eval_detection(model,
     import cv2
     from tqdm import trange 
 
-    assert isinstance(conf_threshold, (
-        float, int
-    )), "The conf_threshold:{} need to be int or float".format(conf_threshold)
-    assert isinstance(nms_iou_threshold, (
-        float,
-        int)), "The nms_iou_threshold:{} need to be int or float".format(
-            nms_iou_threshold)
     if conf_threshold is not None or nms_iou_threshold is not None:
         assert conf_threshold is not None and nms_iou_threshold is not None, "The conf_threshold and nms_iou_threshold should be setted at the same time"
         assert isinstance(conf_threshold, (
