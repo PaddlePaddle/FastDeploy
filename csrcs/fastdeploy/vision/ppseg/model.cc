c7cf0309c98030dffa150894a7a705339f135651
felixhjh <852142024@qq.com>
2022-08-02 09:52:53 +0000

    Support logits matrix resize

--- csrcs/fastdeploy/vision/ppseg/model.cc
+++ csrcs/fastdeploy/vision/ppseg/model.cc
@@ -110,9 +110,10 @@ bool Model::Postprocess(FDTensor& infer_result, SegmentationResult* result,
                ".");
   result->Clear();
   if (infer_result.shape.size() == 4) {
+    FDASSERT(infer_result.shape[0] == 1, "Only support batch size = 1.");
     result->contain_score_map = true;
     float_t* infer_result_buffer =
-        reinterpret_cast<float_t*>(infer_result.data.data());
+        reinterpret_cast<float_t*>(infer_result.MutableData());
     // NCHW to NHWC
     int num = infer_result.shape[0];
     int channel = infer_result.shape[1];
@@ -134,24 +135,58 @@ bool Model::Postprocess(FDTensor& infer_result, SegmentationResult* result,
         }
       }
     }
-    uint8_t* hwc_data_buffer = reinterpret_cast<uint8_t*>(hwc_data.data());
-    std::copy(hwc_data_buffer, hwc_data_buffer + infer_result.data.size() - 1,
-              infer_result.data.begin());
+    std::memcpy(infer_result.MutableData(), hwc_data.data(),
+                num * chw * sizeof(float_t));
     infer_result.shape = {num, height, width, channel};
   }
-  result->shape = infer_result.shape;
-  for (auto& i : result->shape) {
-    std::cout << "result->shape---" << i << std::endl;
+
+  FDTensor new_infer_result;
+  Mat* mat = nullptr;
+  if (is_resized) {
+    FDASSERT(infer_result.dtype == FDDataType::FP32,
+             "Require the data type of output is fp32, but now it's " +
+                 Str(const_cast<fastdeploy::FDDataType&>(infer_result.dtype)) +
+                 ". Please export PaddleSeg model without argmax and with "
+                 "softmax refer to "
+                 "https://github.com/PaddlePaddle/PaddleSeg/blob/release/2.6/"
+                 "docs/model_export_cn.md");
+
+    int channel = 1;
+    int height = infer_result.shape[1];
+    int width = infer_result.shape[2];
+
+    if (result->contain_score_map) {
+      channel = infer_result.shape[3];
+    }
+    cv::Mat temp_mat =
+        cv::Mat(height, width, CV_32FC(channel), infer_result.Data());
+    auto iter_ipt = (*im_info).find("input_shape");
+    FDASSERT(iter_ipt != im_info->end(),
+             "Cannot find input_shape from im_info.");
+    int ipt_h = iter_ipt->second[0];
+    int ipt_w = iter_ipt->second[1];
+
+    mat = new Mat(temp_mat);
+    Resize::Run(mat, ipt_w, ipt_h, -1, -1, 1);
+    mat->ShareWithTensor(&new_infer_result);
+    new_infer_result.shape.insert(new_infer_result.shape.begin(), 1);
+    result->shape = new_infer_result.shape;
+  } else {
+    result->shape = infer_result.shape;
   }
   int out_num =
       std::accumulate(result->shape.begin(), result->shape.begin() + 3, 1,
                       std::multiplies<int>());
-  std::cout << "out_num---" << out_num << std::endl;
+
   result->shape.erase(result->shape.begin());
   result->label_map.reserve(out_num);
   if (result->contain_score_map) {
-    float_t* infer_result_buffer =
-        reinterpret_cast<float_t*>(infer_result.Data());
+    float_t* infer_result_buffer = nullptr;
+    if (is_resized) {
+      infer_result_buffer = static_cast<float_t*>(new_infer_result.Data());
+    } else {
+      infer_result_buffer = static_cast<float_t*>(infer_result.Data());
+    }
     int64_t height = result->shape[0];
     int64_t width = result->shape[1];
     int64_t num_classes = result->shape[2];
@@ -164,10 +199,13 @@ bool Model::Postprocess(FDTensor& infer_result, SegmentationResult* result,
         int64_t s = (i * width + j) * num_classes;
         float_t* max_class_score = std::max_element(
             infer_result_buffer + s, infer_result_buffer + s + num_classes);
-
-        auto label_id = std::distance(infer_result_buffer + s, max_class_score);
-
-        result->label_map[index] = (static_cast<uint8_t>(label_id));
+        int label_id = std::distance(infer_result_buffer + s, max_class_score);
+        if (label_id >= 255) {
+          FDWARNING << "label_id is stored by uint8_t, now the value is bigger "
+                       "than 255, it's "
+                    << static_cast<int>(label_id) << "." << std::endl;
+        }
+        result->label_map[index] = static_cast<uint8_t>(label_id);
 
         if (with_softmax) {
           double_t total = 0;
@@ -190,6 +228,7 @@ bool Model::Postprocess(FDTensor& infer_result, SegmentationResult* result,
       result->label_map[i] = static_cast<uint8_t>(*(infer_result_buffer + i));
     }
   }
+  std::free(mat);
   return true;
 }
 
