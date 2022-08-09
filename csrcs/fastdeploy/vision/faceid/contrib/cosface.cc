f9bddbf7234b57406e30bb4dc13002459e450292
ziqi-jin <ziqijincoder@gmail.com>
2022-08-09 07:48:37 +0000

    modify namespace for face models

---
+++ csrcs/fastdeploy/vision/faceid/contrib/cosface.cc
@@ -0,0 +1,83 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "fastdeploy/vision/faceid/contrib/cosface.h"
+#include "fastdeploy/utils/perf.h"
+#include "fastdeploy/vision/utils/utils.h"
+
+namespace fastdeploy {
+
+namespace vision {
+
+namespace faceid {
+
+CosFace::CosFace(const std::string& model_file, const std::string& params_file,
+                 const RuntimeOption& custom_option,
+                 const Frontend& model_format)
+    : InsightFaceRecognitionModel(model_file, params_file, custom_option,
+                                  model_format) {
+  initialized = Initialize();
+}
+
+bool CosFace::Initialize() {
+  // 如果初始化有变化 修改该子类函数
+  // 这里需要判断backend是否已经initialized，如果是，则不应该再调用
+  // InsightFaceRecognitionModel::Initialize()
+  // 因为该函数会对backend进行初始化, backend已经在父类的构造函数初始化
+  // 这里只修改一些模型相关的属性
+
+  // (1) 如果父类初始化了backend
+  if (initialized) {
+    // (1.1) re-init parameters for specific sub-classes
+    size = {112, 112};
+    alpha = {1.f / 127.5f, 1.f / 127.5f, 1.f / 127.5f};
+    beta = {-1.f, -1.f, -1.f};  // RGB
+    swap_rb = true;
+    l2_normalize = false;
+    return true;
+  }
+  // (2) 如果父类没有初始化backend
+  if (!InsightFaceRecognitionModel::Initialize()) {
+    FDERROR << "Failed to initialize fastdeploy backend." << std::endl;
+    return false;
+  }
+  // (2.1) re-init parameters for specific sub-classes
+  size = {112, 112};
+  alpha = {1.f / 127.5f, 1.f / 127.5f, 1.f / 127.5f};
+  beta = {-1.f, -1.f, -1.f};  // RGB
+  swap_rb = true;
+  l2_normalize = false;
+  return true;
+}
+
+bool CosFace::Preprocess(Mat* mat, FDTensor* output) {
+  // 如果预处理有变化 修改该子类函数
+  return InsightFaceRecognitionModel::Preprocess(mat, output);
+}
+
+bool CosFace::Postprocess(std::vector<FDTensor>& infer_result,
+                          FaceRecognitionResult* result) {
+  // 如果后处理有变化 修改该子类函数
+  return InsightFaceRecognitionModel::Postprocess(infer_result, result);
+}
+
+bool CosFace::Predict(cv::Mat* im, FaceRecognitionResult* result) {
+  // 如果前后处理有变化 则override子类的Preprocess和Postprocess
+  // 如果前后处理有变化 此处应该调用子类自己的Preprocess和Postprocess
+  return InsightFaceRecognitionModel::Predict(im, result);
+}
+
+}  // namespace faceid
+}  // namespace vision
+}  // namespace fastdeploy
---
+++ csrcs/fastdeploy/vision/matting/contrib/modnet.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include "fastdeploy/fastdeploy_model.h"
+#include "fastdeploy/vision/common/processors/transform.h"
+#include "fastdeploy/vision/common/result.h"
+
+namespace fastdeploy {
+
+namespace vision {
+
+namespace matting {
+
+class FASTDEPLOY_DECL MODNet : public FastDeployModel {
+ public:
+  // 当model_format为ONNX时，无需指定params_file
+  // 当model_format为Paddle时，则需同时指定model_file & params_file
+  MODNet(const std::string& model_file, const std::string& params_file = "",
+         const RuntimeOption& custom_option = RuntimeOption(),
+         const Frontend& model_format = Frontend::ONNX);
+
+  // 定义模型的名称
+  std::string ModelName() const { return "matting/MODNet"; }
+
+  // 以下为一些可供用户修改的属性
+  // tuple of (width, height), default (256, 256)
+  std::vector<int> size;
+  // 归一化的 alpha 和 beta，x'=x*alpha+beta
+  std::vector<float> alpha;
+  std::vector<float> beta;
+  // whether to swap the B and R channel, such as BGR->RGB, default true.
+  bool swap_rb;
+
+  // 模型预测接口，即用户调用的接口
+  // im 为用户的输入数据，目前对于CV均定义为cv::Mat
+  // result 为模型预测的输出结构体
+  bool Predict(cv::Mat* im, MattingResult* result);
+
+ private:
+  // 初始化函数，包括初始化后端，以及其它模型推理需要涉及的操作
+  bool Initialize();
+
+  // 输入图像预处理操作
+  // Mat为FastDeploy定义的数据结构
+  // FDTensor为预处理后的Tensor数据，传给后端进行推理
+  bool Preprocess(Mat* mat, FDTensor* output,
+                  std::map<std::string, std::array<int, 2>>* im_info);
+
+  // 后端推理结果后处理，输出给用户
+  // infer_result 为后端推理后的输出Tensor
+  // result 为模型预测的结果
+  bool Postprocess(std::vector<FDTensor>& infer_result, MattingResult* result,
+                   const std::map<std::string, std::array<int, 2>>& im_info);
+};
+
+}  // namespace matting
+}  // namespace vision
+}  // namespace fastdeploy
