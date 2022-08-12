9cf20b22526000f4d54bd630c23bedd0b6131623
felixhjh <852142024@qq.com>
2022-08-12 11:06:15 +0000

    Update PaddleClas infer.py

--- examples/vision/classification/paddleclas/python/infer.py
+++ examples/vision/classification/paddleclas/python/infer.py
@@ -46,8 +46,7 @@ runtime_option = build_option(args)
 model_file = os.path.join(args.model, "inference.pdmodel")
 params_file = os.path.join(args.model, "inference.pdiparams")
 config_file = os.path.join(args.model, "inference_cls.yaml")
-#model = fd.vision.classification.PaddleClasModel(model_file, params_file, config_file, runtime_option=runtime_option)
-model = fd.vision.classification.ResNet50vd(
+model = fd.vision.classification.PaddleClasModel(
     model_file, params_file, config_file, runtime_option=runtime_option)
 
 # 预测图片分类结果
