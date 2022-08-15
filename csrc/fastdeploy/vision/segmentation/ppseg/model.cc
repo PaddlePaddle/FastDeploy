a016ef99ceb57b461930b837b534317af567f3fd
huangjianhui <852142024@qq.com>
2022-08-15 15:24:38 +0800

    Add PaddleSeg doc and infer.cc demo (#114)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add evaluation calculate time and fix some bugs
    
    * Update classification __init__
    
    * Move to ppseg
    
    * Add segmentation doc
    
    * Add PaddleClas infer.py
    
    * Update PaddleClas infer.py
    
    * Delete .infer.py.swp
    
    * Add PaddleClas infer.cc
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update infer.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add PaddleSeg doc and infer.cc demo
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Jason <jiangjiajun@baidu.com>

--- csrc/fastdeploy/vision/segmentation/ppseg/model.cc
+++ csrc/fastdeploy/vision/segmentation/ppseg/model.cc
@@ -14,7 +14,7 @@ PaddleSegModel::PaddleSegModel(const std::string& model_file,
                                const Frontend& model_format) {
   config_file_ = config_file;
   valid_cpu_backends = {Backend::PDINFER, Backend::ORT};
-  valid_gpu_backends = {Backend::PDINFER, Backend::ORT};
+  valid_gpu_backends = {Backend::PDINFER, Backend::ORT, Backend::TRT};
   runtime_option = custom_option;
   runtime_option.model_format = model_format;
   runtime_option.model_file = model_file;
