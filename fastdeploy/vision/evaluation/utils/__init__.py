e35d6dcfd42028aa96371479162cc959568a7d1e
huangjianhui <852142024@qq.com>
2022-08-22 10:53:54 +0800

    Add PPSeg evaluation function (#137)
    * Add PPSeg evaluation function
    
    * Add average_inference_time function

--- fastdeploy/vision/evaluation/utils/__init__.py
+++ fastdeploy/vision/evaluation/utils/__init__.py
@@ -14,8 +14,10 @@
 
 from . import fd_logging
 from .util import *
-from .metrics import *
+from .coco_metrics import *
+from .seg_metrics import *
 from .json_results import *
 from .map_utils import *
 from .coco_utils import *
 from .coco import *
+from .cityscapes import *
