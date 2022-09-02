b953723c0b30361fd3a46e2aec5d5dcf4c66dc88
wjj19950828 <wjjisloser@163.com>
2022-09-02 22:48:36 +0800

    rm logs

--- csrc/fastdeploy/backends/poros/poros_backend.cc
+++ csrc/fastdeploy/backends/poros/poros_backend.cc
@@ -246,37 +246,32 @@ bool PorosBackend::Infer(std::vector<FDTensor>& inputs, std::vector<FDTensor>* o
     for (size_t i = 0; i < inputs.size(); ++i) {
         poros_inputs.push_back(CreatePorosValue(inputs[i], is_backend_cuda));
     }
-    std::cout << "test_wjj1111111111: " << poros_inputs.size() << std::endl;
+    // std::cout << "test_wjj1111111111: " << poros_inputs.size() << std::endl;
     // Infer
     auto poros_outputs = _poros_module->forward(poros_inputs);
-    std::cout << "test_wjj222222222: " << std::endl;
-    std::cout << "test_wjj : " << poros_outputs.isTensor() << std::endl;
-    std::cout << "test_wjj : " << poros_outputs.isList() << std::endl;
-    std::cout << "test_wjj : " << poros_outputs.isTuple() << std::endl;
+    // std::cout << "test_wjj222222222: " << std::endl;
+    // std::cout << "test_wjj : " << poros_outputs.isTensor() << std::endl;
+    // std::cout << "test_wjj : " << poros_outputs.isList() << std::endl;
+    // std::cout << "test_wjj : " << poros_outputs.isTuple() << std::endl;
     // Convert PyTorch Tensor to FD Tensor
     if (poros_outputs.isTensor()) {
-        std::cout << "test_wjj++++++: " << std::endl;
+        // std::cout << "test_wjj++++++: " << std::endl;
         CopyTensorToCpu(poros_outputs.toTensor().to(at::kCPU), &((*outputs)[0]));
-        std::cout << "test_wjj------: " << std::endl;
+        // std::cout << "test_wjj------: " << std::endl;
     } else if (poros_outputs.isTuple()) {
         // deal with multi outputs
         auto poros_outputs_list = poros_outputs.toTuple();
         size_t index = 0;
         for (size_t i = 0; i < poros_outputs_list->elements().size(); ++i) {
-            std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isTensor() << std::endl;
-            std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isList() << std::endl;
-            std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isTuple() << std::endl;
+            // std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isTensor() << std::endl;
+            // std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isList() << std::endl;
+            // std::cout << "test_wjj33333: " << poros_outputs_list->elements()[i].isTuple() << std::endl;
             auto poros_tensor = poros_outputs_list->elements()[i];
             if (poros_tensor.isTensor()) {
                 CopyTensorToCpu(poros_tensor.toTensor().to(at::kCPU), &((*outputs)[index]));
                 index += 1;
             } else if (poros_tensor.isList()) {
                 auto poros_tensor_list = poros_tensor.toList();
-                // size_t tensor_size = poros_tensor_list.size();
-                // for (size_t j = 0; j < tensor_size; ++j) {
-                //     CopyTensorToCpu(poros_tensor_list.get(j).to(at::kCPU), &((*outputs)[index]));
-                //     index += 1;
-                // }
                 for (const auto list_idx : c10::irange(0, poros_tensor_list.size())) {
                     const auto& elt = poros_tensor_list.get(list_idx);
                     CopyTensorToCpu(elt.toTensor().to(at::kCPU), &((*outputs)[index]));
