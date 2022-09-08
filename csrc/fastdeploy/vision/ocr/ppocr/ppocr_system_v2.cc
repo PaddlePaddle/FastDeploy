6c937cf47a5f46758ce5dc927b4bc5379aaa8eca
jiangjiajun <jiangjiajun@baidu.com>
2022-09-08 14:10:19 +0000

    Optimize ocr system code

--- csrc/fastdeploy/vision/ocr/ppocr/ppocr_system_v2.cc
+++ csrc/fastdeploy/vision/ocr/ppocr/ppocr_system_v2.cc
@@ -19,92 +19,88 @@
 namespace fastdeploy {
 namespace application {
 namespace ocrsystem {
-PPOCRSystemv2::PPOCRSystemv2(fastdeploy::vision::ocr::DBDetector* ocr_det,
-                             fastdeploy::vision::ocr::Classifier* ocr_cls,
-                             fastdeploy::vision::ocr::Recognizer* ocr_rec)
-    : detector(ocr_det), classifier(ocr_cls), recognizer(ocr_rec) {}
-
-void PPOCRSystemv2::Detect(cv::Mat* img,
-                           fastdeploy::vision::OCRResult* result) {
-  std::vector<std::array<int, 8>> boxes;
-
-  this->detector->Predict(img, &boxes);
-
-  result->boxes = boxes;
+PPOCRSystemv2::PPOCRSystemv2(fastdeploy::vision::ocr::DBDetector* det_model,
+                             fastdeploy::vision::ocr::Classifier* cls_model,
+                             fastdeploy::vision::ocr::Recognizer* rec_model)
+    : detector_(det_model), classifier_(cls_model), recognizer_(rec_model) {
+  recognizer_->rec_image_shape[1] = 32;
 }
 
-void PPOCRSystemv2::Recognize(cv::Mat* img,
+PPOCRSystemv2::PPOCRSystemv2(fastdeploy::vision::ocr::DBDetector* det_model,
+                             fastdeploy::vision::ocr::Recognizer* rec_model)
+    : detector_(det_model), recognizer_(rec_model) {
+  recognizer_->rec_image_shape[1] = 32;
+}
+
+bool PPOCRSystemv2::Detect(cv::Mat* img,
+                           fastdeploy::vision::OCRResult* result) {
+  if (!detector_->Predict(img, &(result->boxes))) {
+    FDERROR << "There's error while detecting image in PPOCRSystem." << std::endl;
+    return false;
+  }
+  vision::ocr::SortBoxes(result);
+  return true;
+}
+
+bool PPOCRSystemv2::Recognize(cv::Mat* img,
                               fastdeploy::vision::OCRResult* result) {
   std::tuple<std::string, float> rec_result;
-  this->recognizer->rec_image_shape[1] = 32;
-  this->recognizer->Predict(img, &rec_result);
+  if (!recognizer_->Predict(img, &rec_result)) {
+    FDERROR << "There's error while recognizing image in PPOCRSystem." << std::endl;
+    return false;
+  }
 
   result->text.push_back(std::get<0>(rec_result));
   result->rec_scores.push_back(std::get<1>(rec_result));
+  return true;
 }
 
-void PPOCRSystemv2::Classify(cv::Mat* img,
+bool PPOCRSystemv2::Classify(cv::Mat* img,
                              fastdeploy::vision::OCRResult* result) {
   std::tuple<int, float> cls_result;
 
-  this->classifier->Predict(img, &cls_result);
+  if (!classifier_->Predict(img, &cls_result)) {
+    FDERROR << "There's error while classifying image in PPOCRSystem." << std::endl;
+    return false;
+  }
 
   result->cls_labels.push_back(std::get<0>(cls_result));
   result->cls_scores.push_back(std::get<1>(cls_result));
+  return true;
 }
 
 bool PPOCRSystemv2::Predict(cv::Mat* img,
                             fastdeploy::vision::OCRResult* result) {
-  if (this->detector->initialized == 0) {  //没det
-    //输入单张“小图片”给分类器
-    if (this->classifier->initialized != 0) {
-      this->Classify(img, result);
-      //摆正单张图像
-      if ((result->cls_labels)[0] % 2 == 1 &&
-          (result->cls_scores)[0] > this->classifier->cls_thresh) {
-        cv::rotate(*img, *img, 1);
-      }
-    }
-    //输入单张“小图片”给识别器
-    if (this->recognizer->initialized != 0) {
-      this->Recognize(img, result);
-    }
-
-  } else {
-    //从DET模型开始
-    //一张图,会输出多个“小图片”，送给后续模型
-    this->Detect(img, result);
-    // crop image
-    std::vector<cv::Mat> img_list;
-
-    for (int j = 0; j < (result->boxes).size(); j++) {
-      cv::Mat crop_img;
-      crop_img =
-          fastdeploy::vision::ocr::GetRotateCropImage(*img, (result->boxes)[j]);
-      img_list.push_back(crop_img);
-    }
-    // cls
-    if (this->classifier->initialized != 0) {
-      for (int i = 0; i < img_list.size(); i++) {
-        this->Classify(&img_list[0], result);
-      }
-
-      for (int i = 0; i < img_list.size(); i++) {
-        if ((result->cls_labels)[i] % 2 == 1 &&
-            (result->cls_scores)[i] > this->classifier->cls_thresh) {
-          std::cout << "Rotate this image " << std::endl;
-          cv::rotate(img_list[i], img_list[i], 1);
-        }
-      }
-    }
-    // rec
-    if (this->recognizer->initialized != 0) {
-      for (int i = 0; i < img_list.size(); i++) {
-        this->Recognize(&img_list[i], result);
-      }
-    }
+  result->Clear();
+  if (nullptr != detector_ && !Detect(img, result)) {
+    FDERROR << "Failed to detect image." << std::endl;
+    return false;
   }
 
+  // Get croped images by detection result
+  std::vector<cv::Mat> image_list;
+  for (size_t i = 0; i < result->boxes.size(); ++i) {
+    auto crop_im = vision::ocr::GetRotateCropImage(*img, (result->boxes)[i]);
+    image_list.push_back(crop_im);
+  }
+  if (result->boxes.size() == 0) {
+    image_list.push_back(*img);
+  }
+
+  for (size_t i = 0; i < image_list.size(); ++i) {
+    if (nullptr != classifier_ && !Classify(&(image_list[i]), result)) {
+      FDERROR << "Failed to classify croped image of index " << i << "." << std::endl;
+      return false;
+    }
+    if (result->cls_labels[i] % 2 == 1 && result->cls_scores[i] > classifier_->cls_thresh) {
+      cv::rotate(image_list[i], image_list[i], 1);
+    }
+
+    if (nullptr != recognizer_ && !Recognize(&(image_list[i]), result)) {
+      FDERROR << "Failed to recgnize croped image of index " << i << "." << std::endl;
+      return false;
+    }
+  }
   return true;
 };
 
