33e9695dbfbd0821b471265a21d30df0ac67e4e0
Jack Zhou <zhoushunjie@baidu.com>
2022-09-13 19:03:06 +0800

    Add uie python api (#214)
    * add uie pybind
    
    * Add uie result pybind
    
    * Add uie python class
    
    * fix UIEModel pythonargs
    
    * Add schema node pybind
    
    * remove uie print
    
    * Fix cpp build ci

--- csrc/fastdeploy/text/uie/model.h
+++ csrc/fastdeploy/text/uie/model.h
@@ -37,6 +37,7 @@ struct FASTDEPLOY_DECL UIEResult {
   UIEResult() = default;
   UIEResult(size_t start, size_t end, double probability, std::string text)
       : start_(start), end_(end), probability_(probability), text_(text) {}
+  std::string Str() const;
 };
 
 FASTDEPLOY_DECL std::ostream& operator<<(std::ostream& os,
@@ -51,7 +52,8 @@ struct FASTDEPLOY_DECL SchemaNode {
   std::vector<std::vector<std::string>> prefix_;
   std::vector<std::vector<UIEResult*>> relations_;
   std::vector<SchemaNode> children_;
-
+  SchemaNode() = default;
+  SchemaNode(const SchemaNode&) = default;
   explicit SchemaNode(const std::string& name,
                       const std::vector<SchemaNode>& children = {})
       : name_(name), children_(children) {}
@@ -77,9 +79,9 @@ struct Schema {
   explicit Schema(const std::string& schema, const std::string& name = "root");
   explicit Schema(const std::vector<std::string>& schema_list,
                   const std::string& name = "root");
-  explicit Schema(const std::unordered_map<std::string,
-                                           std::vector<SchemaNode>>& schema_map,
+  explicit Schema(const std::vector<SchemaNode>& schema_list,
                   const std::string& name = "root");
+  explicit Schema(const SchemaNode& schema, const std::string& name = "root");
 
  private:
   void CreateRoot(const std::string& name);
@@ -99,13 +101,19 @@ struct FASTDEPLOY_DECL UIEModel {
   UIEModel(
       const std::string& model_file, const std::string& params_file,
       const std::string& vocab_file, float position_prob, size_t max_length,
-      const std::unordered_map<std::string, std::vector<SchemaNode>>& schema,
+      const SchemaNode& schema, const fastdeploy::RuntimeOption& custom_option =
+                                    fastdeploy::RuntimeOption(),
+      const fastdeploy::Frontend& model_format = fastdeploy::Frontend::PADDLE);
+  UIEModel(
+      const std::string& model_file, const std::string& params_file,
+      const std::string& vocab_file, float position_prob, size_t max_length,
+      const std::vector<SchemaNode>& schema,
       const fastdeploy::RuntimeOption& custom_option =
           fastdeploy::RuntimeOption(),
       const fastdeploy::Frontend& model_format = fastdeploy::Frontend::PADDLE);
   void SetSchema(const std::vector<std::string>& schema);
-  void SetSchema(
-      const std::unordered_map<std::string, std::vector<SchemaNode>>& schema);
+  void SetSchema(const std::vector<SchemaNode>& schema);
+  void SetSchema(const SchemaNode& schema);
 
   void ConstructTextsAndPrompts(
       const std::vector<std::string>& raw_texts, const std::string& node_name,
