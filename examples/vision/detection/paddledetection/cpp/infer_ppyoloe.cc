12353ec756fafba9b8c35a5e64ce1523c1b766b7
Jason <jiangjiajun@baidu.com>
2022-09-15 13:44:46 +0800

    Update infer_ppyoloe.cc

--- examples/vision/detection/paddledetection/cpp/infer_ppyoloe.cc
+++ examples/vision/detection/paddledetection/cpp/infer_ppyoloe.cc
@@ -85,7 +85,6 @@ void TrtInfer(const std::string& model_dir, const std::string& image_file) {
   option.UseTrtBackend();
   option.SetTrtInputShape("image", {1, 3, 640, 640});
   option.SetTrtInputShape("scale_factor", {1, 2});
-  option.EnablePaddleToTrt();
   auto model = fastdeploy::vision::detection::PPYOLOE(model_file, params_file,
                                                       config_file, option);
   if (!model.Initialized()) {
