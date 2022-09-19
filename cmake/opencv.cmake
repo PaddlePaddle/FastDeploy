c6e6a0f1ad4196b4804048a5cbe3aa41bc49eed1
zhoushunjie <zhoushunjie@baidu.com>
2022-09-19 22:22:22 +0800

    Use find_package for opencv in windows, mac and linux

--- cmake/opencv.cmake
+++ cmake/opencv.cmake
@@ -14,125 +14,36 @@
 
 
 set(OPENCV_INSTALL_DIR ${THIRD_PARTY_PATH}/install/)
+set(OPENCV_URL_PREFIX "https://bj.bcebos.com/paddle2onnx/libs")
 
+set(COMPRESSED_SUFFIX ".tgz")
 if(WIN32)
-    set(OPENCV_URL https://bj.bcebos.com/paddle2onnx/libs/opencv-win-x64-3.4.16.zip)
-    download_and_decompress(${OPENCV_URL} ${CMAKE_CURRENT_BINARY_DIR}/opencv-win-x64-3.4.16.zip ${THIRD_PARTY_PATH}/install/)
-    set(OpenCV_DIR ${THIRD_PARTY_PATH}/install/opencv-win-x64-3.4.16/build)
-    find_package(OpenCV REQUIRED PATHS ${OpenCV_DIR})
-    include_directories(${OpenCV_INCLUDE_DIRS})
-    list(APPEND DEPEND_LIBS ${OpenCV_LIBS})
-else()
-
-include(ExternalProject)
-
-set(OPENCV_PROJECT "extern_opencv")
-set(OPENCV_PREFIX_DIR ${THIRD_PARTY_PATH}/opencv)
-set(OPENCV_SOURCE_DIR
-    ${THIRD_PARTY_PATH}/opencv/src/${OPENCV_PROJECT})
-set(OPENCV_INSTALL_DIR ${THIRD_PARTY_PATH}/install/opencv)
-set(OPENCV_INC_DIR
-    "${OPENCV_INSTALL_DIR}/include/"
-    CACHE PATH "opencv include directory." FORCE)
-set(OPENCV_LIB_DIR
-    "${OPENCV_INSTALL_DIR}/lib"
-    CACHE PATH "opencv lib directory." FORCE)
-set(CMAKE_BUILD_RPATH "${CMAKE_BUILD_RPATH}" "${OPENCV_LIB_DIR}")
-
-if(WIN32)
-  message(FATAL_ERROR "NOT SUPPORT WINDOWS NOW, OPENCV")
+  set(OPENCV_LIB "opencv-win-x64-3.4.16")
+  set(COMPRESSED_SUFFIX ".zip")
 elseif(APPLE)
   if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "arm64")
-    set(OPENCV_URL "https://bj.bcebos.com/paddle2onnx/libs/opencv-osx-arm64-3.4.16.tgz")
+    set(OPENCV_LIB "opencv-osx-arm64-3.4.16")
   else()
-    set(OPENCV_URL "https://bj.bcebos.com/paddle2onnx/libs/opencv-osx-x86_64-3.4.16.tgz")
+    set(OPENCV_LIB "opencv-osx-x86_64-3.4.16")
   endif()
 else()
   if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "aarch64")
-    set(OPENCV_URL "https://bj.bcebos.com/paddle2onnx/libs/opencv-linux-aarch64-3.4.14.tgz")
+    set(OPENCV_LIB "opencv-linux-aarch64-3.4.14")
   else()
-    set(OPENCV_URL "https://bj.bcebos.com/paddle2onnx/libs/opencv-linux-x64-3.4.16.tgz") 
+    set(OPENCV_LIB "opencv-linux-x64-3.4.16")
   endif()
   if(ENABLE_OPENCV_CUDA)
     if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "aarch64")
       message(FATAL_ERROR "Cannot set ENABLE_OPENCV_CUDA=ON while in linux-aarch64 platform.")
     endif()
-    set(OPENCV_URL "https://bj.bcebos.com/paddle2onnx/libs/opencv-linux-x64-gpu-3.4.16.tgz")
+    set(OPENCV_LIB "opencv-linux-x64-gpu-3.4.16")
   endif()
 endif()
 
-include_directories(${OPENCV_INC_DIR}
-)# For OPENCV code to include internal headers.
+set(OPENCV_URL ${OPENCV_URL_PREFIX}/${OPENCV_LIB}${COMPRESSED_SUFFIX})
+download_and_decompress(${OPENCV_URL} ${CMAKE_CURRENT_BINARY_DIR}/${OPENCV_LIB}${COMPRESSED_SUFFIX} ${THIRD_PARTY_PATH}/install/)
 
-set(OPENCV_SOURCE_LIB ${OPENCV_SOURCE_DIR}/lib/)
-if(WIN32)
-  message(FATAL_ERROR "NOT SUPPORT WEINDOWS, OPENCV")
-elseif(APPLE) 
-  set(OPENCV_CORE_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_core.dylib) 
-  set(OPENCV_HIGHGUI_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_highgui.dylib)
-  set(OPENCV_IMGPROC_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_imgproc.dylib)
-  set(OPENCV_IMGCODESC_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_imgcodecs.dylib)
-else()
-  set(OPENCV_SOURCE_LIB ${OPENCV_SOURCE_DIR}/lib64)
-  if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "aarch64")
-    set(OPENCV_SOURCE_LIB ${OPENCV_SOURCE_DIR}/lib)
-  endif()
-  set(OPENCV_CORE_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_core.so) 
-  set(OPENCV_HIGHGUI_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_highgui.so)
-  set(OPENCV_IMGPROC_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_imgproc.so)
-  set(OPENCV_IMGCODESC_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_imgcodecs.so)
-  set(OPENCV_CUDAARITHM_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_cudaarithm.so)
-  set(OPENCV_CUDAIMGPROC_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_cudaimgproc.so)
-  set(OPENCV_CUDAWARPING_LIB ${OPENCV_INSTALL_DIR}/lib/libopencv_cudawarping.so)
-endif()
-
-if(WIN32)
-  message(FATAL_ERROR "NOT SUPPORT WINDOWS, OPENCV")
-else()
-  ExternalProject_Add(
-   ${OPENCV_PROJECT}
-   ${EXTERNAL_PROJECT_LOG_ARGS}
-   URL ${OPENCV_URL}
-   PREFIX ${OPENCV_PREFIX_DIR}
-   DOWNLOAD_NO_PROGRESS 1
-   CONFIGURE_COMMAND ""
-   BUILD_COMMAND ""
-   UPDATE_COMMAND ""
-   INSTALL_COMMAND
-     ${CMAKE_COMMAND} -E remove_directory ${OPENCV_INSTALL_DIR} &&
-     ${CMAKE_COMMAND} -E make_directory ${OPENCV_INSTALL_DIR} &&
-     ${CMAKE_COMMAND} -E rename ${OPENCV_SOURCE_LIB} ${OPENCV_INSTALL_DIR}/lib &&
-     ${CMAKE_COMMAND} -E copy_directory ${OPENCV_SOURCE_DIR}/include/
-     ${OPENCV_INC_DIR}
-   BUILD_BYPRODUCTS ${OPENCV_LIB})
-endif() 
-
-add_library(external_opencv_core  STATIC IMPORTED GLOBAL)
-set_property(TARGET external_opencv_core PROPERTY IMPORTED_LOCATION ${OPENCV_CORE_LIB})
-add_library(external_opencv_highgui  STATIC IMPORTED GLOBAL)
-set_property(TARGET external_opencv_highgui PROPERTY IMPORTED_LOCATION ${OPENCV_HIGHGUI_LIB})
-add_library(external_opencv_imgproc  STATIC IMPORTED GLOBAL)
-set_property(TARGET external_opencv_imgproc PROPERTY IMPORTED_LOCATION ${OPENCV_IMGPROC_LIB})
-add_library(external_opencv_imgcodesc  STATIC IMPORTED GLOBAL)
-set_property(TARGET external_opencv_imgcodesc PROPERTY IMPORTED_LOCATION ${OPENCV_IMGCODESC_LIB})
-
-add_dependencies(external_opencv_core ${OPENCV_PROJECT})
-add_dependencies(external_opencv_highgui ${OPENCV_PROJECT})
-add_dependencies(external_opencv_imgproc ${OPENCV_PROJECT})
-add_dependencies(external_opencv_imgcodesc ${OPENCV_PROJECT})
-
-list(APPEND DEPEND_LIBS external_opencv_core external_opencv_highgui external_opencv_imgproc external_opencv_imgcodesc)
-
-if(ENABLE_OPENCV_CUDA)
-  add_library(extern_opencv_cudawarping STATIC IMPORTED GLOBAL)
-  set_property(TARGET extern_opencv_cudawarping PROPERTY IMPORTED_LOCATION ${OPENCV_CUDAWARPING_LIB})
-  add_dependencies(extern_opencv_cudawarping ${OPENCV_PROJECT})
-  add_library(extern_opencv_cudaarithm STATIC IMPORTED GLOBAL)
-  set_property(TARGET extern_opencv_cudaarithm PROPERTY IMPORTED_LOCATION ${OPENCV_CUDAARITHM_LIB})
-  add_dependencies(extern_opencv_cudaarithm ${OPENCV_PROJECT})
-  add_library(extern_opencv_cudaimgproc STATIC IMPORTED GLOBAL)
-  set_property(TARGET extern_opencv_cudaimgproc PROPERTY IMPORTED_LOCATION ${OPENCV_CUDAIMGPROC_LIB})
-  add_dependencies(extern_opencv_cudaimgproc ${OPENCV_PROJECT})
-  list(APPEND DEPEND_LIBS extern_opencv_cudawarping extern_opencv_cudaarithm extern_opencv_cudaimgproc)
-endif()
-endif(WIN32)
+set(OpenCV_DIR ${THIRD_PARTY_PATH}/install/${OPENCV_LIB}/)
+find_package(OpenCV REQUIRED PATHS ${OpenCV_DIR})
+include_directories(${OpenCV_INCLUDE_DIRS})
+list(APPEND DEPEND_LIBS ${OpenCV_LIBS})
