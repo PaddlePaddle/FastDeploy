d2bb5db6c3c6ce9232624972e6e2246c5c6b7e74
yunyaoXYY <imylovex1@163.com>
2022-09-20 08:45:18 +0000

    Add PaddleInference-GPU for OCR Rec model

--- fastdeploy/vision/ocr/ppocr/recognizer.cc
+++ fastdeploy/vision/ocr/ppocr/recognizer.cc
@@ -47,9 +47,8 @@ Recognizer::Recognizer(const std::string& model_file,
     valid_cpu_backends = {Backend::ORT, Backend::OPENVINO};  // 指定可用的CPU后端
     valid_gpu_backends = {Backend::ORT, Backend::TRT};  // 指定可用的GPU后端
   } else {
-    // NOTE:此模型暂不支持paddle-inference-Gpu推理
-    valid_cpu_backends = {Backend::ORT, Backend::PDINFER, Backend::OPENVINO};
-    valid_gpu_backends = {Backend::ORT, Backend::TRT};
+    valid_cpu_backends = {Backend::PDINFER, Backend::ORT, Backend::OPENVINO};
+    valid_gpu_backends = {Backend::PDINFER, Backend::ORT, Backend::TRT};
   }
 
   runtime_option = custom_option;
