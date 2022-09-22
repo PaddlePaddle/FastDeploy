e364f0c8e91a79ff1d5ff03d96aa000bbcad0217
zhoushunjie <zhoushunjie@baidu.com>
2022-09-22 15:50:15 +0800

    Use find_package for tbb

--- cmake/openvino.cmake
+++ cmake/openvino.cmake
@@ -86,8 +86,7 @@ else()
   set(OPENVINO_LIB
       "${OPENVINO_INSTALL_DIR}/lib/libopenvino.so"
       CACHE FILEPATH "OPENVINO shared library." FORCE)
-  set(TBB_LIB "${OPENVINO_INSTALL_DIR}/3rdparty/tbb/lib/libtbb.so.2")
-  set(TBB_MALLOC_LIB "${OPENVINO_INSTALL_DIR}/3rdparty/tbb/lib/libtbbmalloc.so.2")
+  
   set(INSTALL_CMD ${CMAKE_COMMAND} -E remove_directory ${OPENVINO_INSTALL_DIR} &&
               ${CMAKE_COMMAND} -E make_directory ${OPENVINO_INSTALL_DIR} &&
               ${CMAKE_COMMAND} -E copy_directory ${OPENVINO_SOURCE_DIR}/lib/intel64 ${OPENVINO_INSTALL_DIR}/lib &&
@@ -118,15 +117,8 @@ add_library(external_openvino STATIC IMPORTED GLOBAL)
 set_property(TARGET external_openvino PROPERTY IMPORTED_LOCATION ${OPENVINO_LIB})
 add_dependencies(external_openvino ${OPENVINO_PROJECT})
 
-# # TBB is a multithread lib, can accelarate openvino
-add_library(tbb STATIC IMPORTED GLOBAL)
-set_property(TARGET tbb PROPERTY IMPORTED_LOCATION ${TBB_LIB})
-add_dependencies(tbb ${OPENVINO_PROJECT})
-
-add_library(tbbmalloc STATIC IMPORTED GLOBAL)
-set_property(TARGET tbbmalloc PROPERTY IMPORTED_LOCATION ${TBB_MALLOC_LIB})
-add_dependencies(tbbmalloc ${OPENVINO_PROJECT})
-
-set(OPENVINO_LIBS external_openvino tbb tbbmalloc)
+find_package(TBB REQUIRED PATHS "${OPENVINO_INSTALL_DIR}/3rdparty/tbb/")
+set(OPENVINO_LIBS external_openvino ${TBB_IMPORTED_TARGETS})
+message(STATUS "OPENVINO_LIBS = ${OPENVINO_LIBS}")
 
 list(APPEND DEPEND_LIBS ${OPENVINO_LIBS})
