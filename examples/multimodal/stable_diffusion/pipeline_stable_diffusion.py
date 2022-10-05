1acb0f22d6ed28d1c3577255fdc18fcb12b63ef5
zhoushunjie <zhoushunjie@baidu.com>
2022-10-05 11:02:02 +0000

    add hf license

--- examples/multimodal/stable_diffusion/pipeline_stable_diffusion.py
+++ examples/multimodal/stable_diffusion/pipeline_stable_diffusion.py
@@ -1,3 +1,5 @@
+# Copyright 2022 The HuggingFace Inc. team.
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
