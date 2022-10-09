cff22b888109c17473ce770f32aed2297a6f3cd8
ChaoII <849453582@qq.com>
2022-10-09 11:41:41 +0800

    delete some

---
+++ fastdeploy/vision/detection/ppdet/jde_track.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aichao on 2022/10/8.
+//
+
+#include "jde_track.h"
---
+++ fastdeploy/vision/detection/ppdet/jde_track.h
@@ -0,0 +1,16 @@
+//
+// Created by aichao on 2022/10/8.
+//
+
+#ifndef FASTDEPLOY_JDE_TRACK_H
+#define FASTDEPLOY_JDE_TRACK_H
+
+
+
+class jde_track {
+
+};
+
+
+
+#endif //FASTDEPLOY_JDE_TRACK_H
