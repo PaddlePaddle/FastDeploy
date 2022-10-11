27863f69f86d0d3ef15ae5652bee8c1e7cdb673e
huangjianhui <852142024@qq.com>
2022-10-11 20:16:07 +0800

    Update cpp_run.bat
    Update wget in bat

--- tests/release_task/cpp_run.bat
+++ tests/release_task/cpp_run.bat
@@ -24,9 +24,9 @@ if "%DEVICE%" == "gpu" (
 echo "CPP_FASTDEPLOY_PACKAGE: " %CPP_FASTDEPLOY_PACKAGE%
 echo "RUN_CASES" %RUN_CASES%
 
-wget -q https://bj.bcebos.com/paddlehub/fastdeploy/ppyoloe_crn_l_300e_coco.tgz
-wget -q https://gitee.com/paddlepaddle/PaddleDetection/raw/release/2.4/demo/000000014439.jpg
-wget -q https://bj.bcebos.com/paddlehub/fastdeploy/release_task_groud_truth_result.txt
+python -c "import wget; wget.download('https://bj.bcebos.com/paddlehub/fastdeploy/ppyoloe_crn_l_300e_coco.tgz')"
+python -c "import wget; wget.download('https://gitee.com/paddlepaddle/PaddleDetection/raw/release/2.4/demo/000000014439.jpg')"
+python -c "import wget; wget.download('https://bj.bcebos.com/paddlehub/fastdeploy/release_task_groud_truth_result.txt')"
 tar -xf ppyoloe_crn_l_300e_coco.tgz
 
 set IMAGE_PATH=%CURRENT_DIR%\000000014439.jpg
@@ -34,7 +34,7 @@ set MODEL_PATH=%CURRENT_DIR%\ppyoloe_crn_l_300e_coco
 set GROUND_TRUTH_PATH=%CURRENT_DIR%\release_task_groud_truth_result.txt
 set COMPARE_SHELL=%CURRENT_DIR%\compare_with_gt.py
 
-wget -q  https://fastdeploy.bj.bcebos.com/dev/cpp/$CPP_FASTDEPLOY_PACKAGE.zip
+python -c "import wget; wget.download('https://fastdeploy.bj.bcebos.com/dev/cpp/$CPP_FASTDEPLOY_PACKAGE.zip')"
 tar -xf %cd%\%CPP_FASTDEPLOY_PACKAGE%.zip
 
 mkdir build && cd build
