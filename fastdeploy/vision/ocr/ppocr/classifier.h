24317e1a14bd5da28fa82a28af95ccb5eecee8cb
yunyaoXYY <109218879+yunyaoXYY@users.noreply.github.com>
2022-10-19 17:21:48 +0800

    [Doc] Rename PPOCRSystem to PPOCR and update comments. (#395)
    * Imporve OCR Readme
    
    * Improve OCR Readme
    
    * Improve OCR Readme
    
    * Improve OCR Readme
    
    * Improve OCR Readme
    
    * Add Initialize function to PP-OCR
    
    * Add Initialize function to PP-OCR
    
    * Add Initialize function to PP-OCR
    
    * Make all the model links come from PaddleOCR
    
    * Improve OCR readme
    
    * Improve OCR readme
    
    * Improve OCR readme
    
    * Improve OCR readme
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add Readme for vision results
    
    * Add check for label file in postprocess of Rec model
    
    * Add check for label file in postprocess of Rec model
    
    * Add check for label file in postprocess of Rec model
    
    * Add check for label file in postprocess of Rec model
    
    * Add check for label file in postprocess of Rec model
    
    * Add check for label file in postprocess of Rec model
    
    * Add comments to create API docs
    
    * Improve OCR comments
    
    * Rename OCR and add comments
    
    * Make sure previous python example works
    
    * Make sure previous python example works
    
    Co-authored-by: Jason <jiangjiajun@baidu.com>

--- fastdeploy/vision/ocr/ppocr/classifier.h
+++ fastdeploy/vision/ocr/ppocr/classifier.h
@@ -20,20 +20,36 @@
 
 namespace fastdeploy {
 namespace vision {
+/** \brief All OCR series model APIs are defined inside this namespace
+ *
+ */
 namespace ocr {
-
+/*! @brief Classifier object is used to load the classification model provided by PaddleOCR.
+ */
 class FASTDEPLOY_DECL Classifier : public FastDeployModel {
  public:
   Classifier();
+  /** \brief Set path of model file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g ./ch_ppocr_mobile_v2.0_cls_infer/model.pdmodel.
+   * \param[in] params_file Path of parameter file, e.g ./ch_ppocr_mobile_v2.0_cls_infer/model.pdiparams, if the model format is ONNX, this parameter will be ignored.
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`.
+   * \param[in] model_format Model format of the loaded model, default is Paddle format.
+   */
   Classifier(const std::string& model_file, const std::string& params_file = "",
              const RuntimeOption& custom_option = RuntimeOption(),
              const ModelFormat& model_format = ModelFormat::PADDLE);
-
+  /// Get model's name
   std::string ModelName() const { return "ppocr/ocr_cls"; }
-
+  /** \brief Predict the input image and get OCR classification model result.
+   *
+   * \param[in] im The input image data, comes from cv::imread().
+   * \param[in] result The output of OCR classification model result will be writen to this structure.
+   * \return true if the prediction is successed, otherwise false.
+   */
   virtual bool Predict(cv::Mat* img, std::tuple<int, float>* result);
 
-  // pre & post parameters
+  // Pre & Post parameters
   float cls_thresh;
   std::vector<int> cls_image_shape;
   int cls_batch_num;
@@ -44,9 +60,9 @@ class FASTDEPLOY_DECL Classifier : public FastDeployModel {
 
  private:
   bool Initialize();
-
+  /// Preprocess the input data, and set the preprocessed results to `outputs`
   bool Preprocess(Mat* img, FDTensor* output);
-
+  /// Postprocess the inferenced results, and set the final result to `result`
   bool Postprocess(FDTensor& infer_result, std::tuple<int, float>* result);
 };
 
