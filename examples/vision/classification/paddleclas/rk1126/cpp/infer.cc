edb5d664437796bea2f561897f3eed8adbece2a3
yeliang2258 <1047690002@qq.com>
2022-11-09 13:06:06 +0000

    update code

--- examples/vision/classification/paddleclas/rk1126/cpp/infer.cc
+++ examples/vision/classification/paddleclas/rk1126/cpp/infer.cc
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-# include<string>
+#include <string>
 #include "fastdeploy/vision.h"
 #ifdef WIN32
 const char sep = '\\';
