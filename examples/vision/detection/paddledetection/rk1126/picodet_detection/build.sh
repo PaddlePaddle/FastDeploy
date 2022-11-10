e650b63dfaec72c77cb99fd6fe2df84ec483c956
yingshengBD <74164525+yingshengBD@users.noreply.github.com>
2022-11-10 16:21:37 +0800

    [ Model ][rk1126] add picodet (#557)
    [Example][rk1126] add picodet

---
+++ examples/vision/detection/paddledetection/rk1126/picodet_detection/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+USE_FULL_API=TRUE
+# configure
+TARGET_ARCH_ABI=armv8 # for RK3399, set to default arch abi
+#TARGET_ARCH_ABI=armv7hf # for Raspberry Pi 3B
+PADDLE_LITE_DIR=../Paddle-Lite
+THIRD_PARTY_DIR=./third_party
+if [ "x$1" != "x" ]; then
+    TARGET_ARCH_ABI=$1
+fi
+
+# build
+rm -rf build
+mkdir build
+cd build
+cmake -DPADDLE_LITE_DIR=${PADDLE_LITE_DIR} -DTARGET_ARCH_ABI=${TARGET_ARCH_ABI} -DTHIRD_PARTY_DIR=${THIRD_PARTY_DIR} ..
+make
