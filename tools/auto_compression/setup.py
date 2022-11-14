5e026eba313e87c0c2e937161b5a23a6af00d1fe
yunyaoXYY <imylovex1@163.com>
2022-11-14 07:56:39 +0000

    Remove extra requirements for fd-auto-compress package

--- tools/auto_compression/setup.py
+++ tools/auto_compression/setup.py
@@ -4,11 +4,9 @@ import fd_auto_compress
 long_description = "fd_auto_compress is a toolkit for model auto compression of FastDeploy.\n\n"
 long_description += "Usage: fastdeploy --auto_compress --config_path=./yolov7_tiny_qat_dis.yaml --method='QAT' --save_dir='../v7_qat_outmodel/' \n"
 
-with open("requirements.txt") as fin:
-    REQUIRED_PACKAGES = fin.read()
-
 setuptools.setup(
-    name="fd_auto_compress",  # name of package
+    name="fd_auto_compress",
+    version="0.0.1",
     description="A toolkit for model auto compression of FastDeploy.",
     long_description=long_description,
     long_description_content_type="text/plain",
@@ -16,7 +14,6 @@ setuptools.setup(
     author='fastdeploy',
     author_email='fastdeploy@baidu.com',
     url='https://github.com/PaddlePaddle/FastDeploy.git',
-    install_requires=REQUIRED_PACKAGES,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
