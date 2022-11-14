70e8774d5b56e46562d8ad548f45e7d85d0d38b3
jiangjiajun <jiangjiajun@baidu.com>
2022-11-14 09:51:06 +0000

    Fix preprocessor pybind api bug

--- fastdeploy/vision/classification/ppcls/ppcls_pybind.cc
+++ fastdeploy/vision/classification/ppcls/ppcls_pybind.cc
@@ -27,6 +27,9 @@ void BindPaddleClas(pybind11::module& m) {
         if (!self.Run(&images, &outputs)) {
           pybind11::eval("raise Exception('Failed to preprocess the input data in PaddleClasPreprocessor.')");
         }
+        for (size_t i = 0; i < outputs.size(); ++i) {
+          outputs[i].StopSharing();
+        }
         return outputs;
       });
 
