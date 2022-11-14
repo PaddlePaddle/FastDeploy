c84de29f94f9532c94054618369ff9c8396e7774
Wang Xinyu <wangxinyu_es@163.com>
2022-11-14 18:44:00 +0800

    [Backend] cuda normalize and permute, cuda concat, optimized ppcls, ppdet & ppseg (#546)
    * cuda normalize and permute, cuda concat
    
    * add use cuda option for preprocessor
    
    * ppyoloe use cuda normalize
    
    * ppseg use cuda normalize
    
    * add proclib cuda in processor base
    
    * ppcls add use cuda preprocess api
    
    * ppcls preprocessor set gpu id
    
    * fix pybind
    
    * refine ppcls preprocessing use gpu logic
    
    * fdtensor device id is -1 by default
    
    * refine assert message
    
    Co-authored-by: heliqi <1101791222@qq.com>

--- fastdeploy/vision/classification/ppcls/ppcls_pybind.cc
+++ fastdeploy/vision/classification/ppcls/ppcls_pybind.cc
@@ -28,6 +28,9 @@ void BindPaddleClas(pybind11::module& m) {
           pybind11::eval("raise Exception('Failed to preprocess the input data in PaddleClasPreprocessor.')");
         }
         return outputs;
+      })
+      .def("use_gpu", [](vision::classification::PaddleClasPreprocessor& self, int gpu_id = -1) {
+        self.UseGpu(gpu_id);
       });
 
   pybind11::class_<vision::classification::PaddleClasPostprocessor>(
