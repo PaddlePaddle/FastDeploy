4c36467341ff24516c54ead090c6a6dc8306ce38
Jason <jiangjiajun@baidu.com>
2022-11-23 10:51:43 +0800

    Update model.h

--- fastdeploy/vision/detection/ppdet/model.h
+++ fastdeploy/vision/detection/ppdet/model.h
@@ -86,7 +86,7 @@ class FASTDEPLOY_DECL PPYOLO : public PPDetBase {
           const ModelFormat& model_format = ModelFormat::PADDLE)
       : PPDetBase(model_file, params_file, config_file, custom_option,
                 model_format) {
-    valid_cpu_backends = {Backend::OPENVINO, Backend::PDINFER, Backend::LITE};
+    valid_cpu_backends = {Backend::PDINFER, Backend::LITE};
     valid_gpu_backends = {Backend::PDINFER};
     initialized = Initialize();
   }
