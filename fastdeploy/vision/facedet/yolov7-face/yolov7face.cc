401c23d61bc43a4644947762b49b2fb0eb5e4d9d
CoolKbh <hm13718556238@126.com>
2022-11-24 15:59:59 +0800

    fit yolov7Face.cc

--- fastdeploy/vision/facedet/yolov7-face/yolov7face.cc
+++ fastdeploy/vision/facedet/yolov7-face/yolov7face.cc
@@ -33,6 +33,7 @@ YOLOv7Face::YOLOv7Face(const std::string& model_file,
     valid_cpu_backends = {Backend::PDINFER, Backend::ORT};
     valid_gpu_backends = {Backend::PDINFER, Backend::ORT, Backend::TRT};
   }
+  runtime_option = custom_option;
   runtime_option.model_format = model_format;
   runtime_option.model_file = model_file;
   runtime_option.params_file = params_file;
