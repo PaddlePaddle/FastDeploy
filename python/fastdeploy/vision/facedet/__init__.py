520d005fce1d8fe612a7174aa430de52eba742d2
CoolKbh <hm13718556238@126.com>
2022-11-25 09:52:17 +0800

    feat __init__.py

--- python/fastdeploy/vision/facedet/__init__.py
+++ python/fastdeploy/vision/facedet/__init__.py
@@ -14,6 +14,7 @@
 
 from __future__ import absolute_import
 from .contrib.yolov5face import YOLOv5Face
+from .contrib.yolov5face import YOLOv7Face
 from .contrib.retinaface import RetinaFace
 from .contrib.scrfd import SCRFD
 from .contrib.ultraface import UltraFace
