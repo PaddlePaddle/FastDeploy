6ece05a920b277eec383518aae429167b9bf8a5a
totorolin <97090522+totorolin@users.noreply.github.com>
2022-11-28 19:13:02 +0800

    [Model] Support PaddleDetection SSD Model (#630)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    Co-authored-by: DefTruth <31974251+DefTruth@users.noreply.github.com>

--- python/fastdeploy/vision/detection/ppdet/__init__.py
+++ python/fastdeploy/vision/detection/ppdet/__init__.py
@@ -272,3 +272,28 @@ class MaskRCNN(PPYOLOE):
 
         raise Exception(
             "batch_predict is not supported for MaskRCNN model now.")
+
+
+class SSD(PPYOLOE):
+    def __init__(self,
+                 model_file,
+                 params_file,
+                 config_file,
+                 runtime_option=None,
+                 model_format=ModelFormat.PADDLE):
+        """Load a SSD model exported by PaddleDetection.
+
+        :param model_file: (str)Path of model file, e.g ssd/model.pdmodel
+        :param params_file: (str)Path of parameters file, e.g ssd/model.pdiparams, if the model_fomat is ModelFormat.ONNX, this param will be ignored, can be set as empty string
+        :param config_file: (str)Path of configuration file for deployment, e.g ppyoloe/infer_cfg.yml
+        :param runtime_option: (fastdeploy.RuntimeOption)RuntimeOption for inference this model, if it's None, will use the default backend on CPU
+        :param model_format: (fastdeploy.ModelForamt)Model format of the loaded model
+        """
+
+        super(PPYOLOE, self).__init__(runtime_option)
+
+        assert model_format == ModelFormat.PADDLE, "SSD model only support model format of ModelFormat.Paddle now."
+        self._model = C.vision.detection.SSD(
+            model_file, params_file, config_file, self._runtime_option,
+            model_format)
+        assert self.initialized, "SSD model initialize failed."
