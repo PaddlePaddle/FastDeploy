7b1e76ebfedcc5484ef12b62636451ba4feba483
yeliang2258 <1047690002@qq.com>
2022-12-02 08:51:43 +0000

    update cmake

--- cmake/xpu.cmake
+++
@@ -1,9 +0,0 @@
-if (NOT DEFINED TARGET_ABI)
-    message(WARNING "TARGET_ABI is DEFINED, will set to TARGET_ABI=amd64")
-    set(TARGET_ABI amd64)
-endif()
-
-if(NOT ${ENABLE_LITE_BACKEND})
-    message(WARNING "While compiling with -DWITH_XPU=ON, will force to set -DENABLE_LITE_BACKEND=ON")
-    set(ENABLE_LITE_BACKEND ON)
-endif()
