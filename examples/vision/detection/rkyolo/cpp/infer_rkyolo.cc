c057ef75700bf77bc32e77e45c46e15529337ffd
ZhengBicheng <a192161647@163.com>
2022-12-08 16:24:27 +0800

    update

--- examples/vision/detection/rkyolo/cpp/infer_rkyolo.cc
+++ examples/vision/detection/rkyolo/cpp/infer_rkyolo.cc
@@ -11,12 +11,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <iostream>
-#include <string>
 #include "fastdeploy/vision.h"
-#include <sys/time.h>
-double __get_us(struct timeval t) { return (t.tv_sec * 1000000 + t.tv_usec); }
-void InferRKYolo(const std::string& model_dir, const std::string& image_file);
+
+void InferRKYolo(const std::string& model_file, const std::string& image_file) {
+  struct timeval start_time, stop_time;
+
+  auto option = fastdeploy::RuntimeOption();
+  option.UseRKNPU2();
+
+  auto format = fastdeploy::ModelFormat::RKNN;
+
+  auto model = fastdeploy::vision::detection::RKYOLOV5(
+      model_file, option,format);
+
+  auto im = cv::imread(image_file);
+
+  fastdeploy::vision::DetectionResult res;
+  if (!model.Predict(im, &res)) {
+    std::cerr << "Failed to predict." << std::endl;
+    return;
+  }
+  std::cout << res.Str() << std::endl;
+  auto vis_im = fastdeploy::vision::VisDetection(im, res,0.5);
+  cv::imwrite("vis_result.jpg", vis_im);
+  std::cout << "Visualized result saved in ./vis_result.jpg" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 3) {
@@ -32,31 +51,3 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-void InferRKYolo(const std::string& model_dir, const std::string& image_file) {
-  struct timeval start_time, stop_time;
-  auto model_file = model_dir + "/yolov5s_relu_tk2_RK3588_i8.rknn";
-
-  auto option = fastdeploy::RuntimeOption();
-  option.UseRKNPU2();
-
-  auto format = fastdeploy::ModelFormat::RKNN;
-
-  auto model = fastdeploy::vision::detection::RKYOLOV5(
-      model_file, option,format);
-
-  auto im = cv::imread(image_file);
-
-  fastdeploy::vision::DetectionResult res;
-  gettimeofday(&start_time, NULL);
-  if (!model.Predict(im, &res)) {
-    std::cerr << "Failed to predict." << std::endl;
-    return;
-  }
-  gettimeofday(&stop_time, NULL);
-  printf("infer use %f ms\n", (__get_us(stop_time) - __get_us(start_time)) / 1000);
-
-  std::cout << res.Str() << std::endl;
-  auto vis_im = fastdeploy::vision::VisDetection(im, res,0.5);
-  cv::imwrite("rkyolo_result.jpg", vis_im);
-  std::cout << "Visualized result saved in ./rkyolo_result.jpg" << std::endl;
-}
