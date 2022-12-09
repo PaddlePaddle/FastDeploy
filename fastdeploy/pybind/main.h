295212e0b8eac857e6902ed61f2f438981bf1758
shentanyue <34421038+shentanyue@users.noreply.github.com>
2022-12-09 13:20:33 +0800

    [Paddle Lite] Support stable-diffusion model (#830)
    * support stable-diffusion model for paddlelite
    
    * update code

--- fastdeploy/pybind/main.h
+++ fastdeploy/pybind/main.h
@@ -67,9 +67,11 @@ FDDataType CTypeToFDDataType() {
     return FDDataType::FP32;
   } else if (std::is_same<T, double>::value) {
     return FDDataType::FP64;
+  } else if (std::is_same<T, int8_t>::value) {
+    return FDDataType::INT8;
   }
-  FDASSERT(false,
-           "CTypeToFDDataType only support int32/int64/float32/float64 now.");
+  FDASSERT(false, "CTypeToFDDataType only support "
+           "int8/int32/int64/float32/float64 now.");
   return FDDataType::FP32;
 }
 
