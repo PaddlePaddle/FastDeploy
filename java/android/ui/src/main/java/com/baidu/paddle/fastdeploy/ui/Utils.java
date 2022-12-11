09f4b8187349d884737c24313307411f102d1595
Winter <1558270516@qq.com>
2022-12-11 12:57:59 +0800

    [Android] add VoiceAssistant app example (#834)
    * [Android]add VoiceAssistant.
    
    * Create VoiceAssistantDemo
    
    * Update and rename VoiceAssistantDemo to VoiceAssistantDemo.md
    
    * Update VoiceAssistantDemo.md
    
    * Delete VoiceAssistantDemo.md
    
    * [Android]1.delete about core folder.
    2.build and configure bdasr_V3_20210628_cfe8c44.aar file.
    
    * change app/build.gradle etc.
    
    * Update build.gradle
    
    Co-authored-by: DefTruth <31974251+DefTruth@users.noreply.github.com>

--- java/android/ui/src/main/java/com/baidu/paddle/fastdeploy/ui/Utils.java
+++ java/android/ui/src/main/java/com/baidu/paddle/fastdeploy/ui/Utils.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.opengl.GLES20;
 import android.os.Environment;
@@ -309,5 +311,29 @@ public class Utils {
             }
         }
         return null;
+    }
+
+    public static boolean isNetworkAvailable(final Context context) {
+        boolean hasWifoCon = false;
+        boolean hasMobileCon = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfos = cm.getAllNetworkInfo();
+        for (NetworkInfo net : netInfos) {
+
+            String type = net.getTypeName();
+            if (type.equalsIgnoreCase("WIFI")) {
+                if (net.isConnected()) {
+                    hasWifoCon = true;
+                }
+            }
+
+            if (type.equalsIgnoreCase("MOBILE")) {
+                if (net.isConnected()) {
+                    hasMobileCon = true;
+                }
+            }
+        }
+        return hasWifoCon || hasMobileCon;
     }
 }
