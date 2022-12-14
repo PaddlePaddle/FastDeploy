22acdd90f961365213b8ff2b11494ea25303d6da
ZhengBicheng <a192161647@163.com>
2022-12-14 11:12:34 +0800

    更新scrfd python example代码

--- python/fastdeploy/vision/facedet/contrib/scrfd.py
+++ python/fastdeploy/vision/facedet/contrib/scrfd.py
@@ -52,10 +52,20 @@ class SCRFD(FastDeployModel):
                                    nms_iou_threshold)
 
     def disable_normalize_and_permute(self):
+        self.disable_permute()
+        self.disable_normalize()
+
+    def disable_normalize(self):
         """
-        This function will disable normalize and hwc2chw in preprocessing step.
+        This function will disable normalize in preprocessing step.
         """
-        self._model.disable_normalize_and_permute()
+        self._model.disable_normalize()
+
+    def disable_permute(self):
+        """
+        This function will disable hwc2chw in preprocessing step.
+        """
+        self._model.disable_permute()
 
     # 一些跟SCRFD模型有关的属性封装
     # 多数是预处理相关，可通过修改如model.size = [640, 640]改变预处理时resize的大小（前提是模型支持）
