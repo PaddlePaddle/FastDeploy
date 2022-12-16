599ff91b96ee503de01f91fc109b1a04e286eafa
felixhjh <852142024@qq.com>
2022-12-16 17:55:44 +0000

    Update pptinypose predict api with detection_result

--- python/fastdeploy/vision/keypointdetection/pptinypose/__init__.py
+++ python/fastdeploy/vision/keypointdetection/pptinypose/__init__.py
@@ -41,25 +41,18 @@ class PPTinyPose(FastDeployModel):
             model_format)
         assert self.initialized, "PPTinyPose model initialize failed."
 
-    def predict(self, input_image):
+    def predict(self, input_image, detection_result=None):
         """Detect keypoints in an input image
 
         :param im: (numpy.ndarray)The input image data, 3-D array with layout HWC, BGR format
+        :param detection_result: (DetectionResult)Pre-detected boxes result, default is None
         :return: KeyPointDetectionResult
         """
         assert input_image is not None, "The input image data is None."
-        return self._model.predict(input_image)
-
-    def predict_with_detection_result(self, input_image, detection_result):
-        """Detect keypoints in an input image with detection result
-
-        :param im: (numpy.ndarray)The input image data, 3-D array with layout HWC, BGR format
-        :param im: Fastdeploy DetectionResult
-        :return: KeyPointDetectionResult
-        """
-        assert input_image is not None, "The input image data is None."
-        assert detection_result is not None, "The detection_result data is None"
-        return self._model.predict(input_image, detection_result)
+        if detection_result:
+            return self._model.predict(input_image, detection_result)
+        else:
+            return self._model.predict(input_image)
 
     @property
     def use_dark(self):
