23c205857bfbd8cc41271655d3ef37792bba3970
ZhengBicheng <a192161647@163.com>
2022-12-18 12:22:34 +0800

    更新文档

--- fastdeploy/backends/rknpu/rknpu2/rknpu2_backend.cc
+++ fastdeploy/backends/rknpu/rknpu2/rknpu2_backend.cc
@@ -315,6 +315,7 @@ bool RKNPU2Backend::Infer(std::vector<FDTensor>& inputs,
         FDINFO << "The input model is not a quantitative model. "
                   "Close the normalize operation." << std::endl;
       }
+      DumpTensorAttr(input_attrs_[i]);
 
       input_mems_[i] = rknn_create_mem(ctx, inputs[i].Nbytes());
       if (input_mems_[i] == nullptr) {
@@ -340,6 +341,11 @@ bool RKNPU2Backend::Infer(std::vector<FDTensor>& inputs,
         FDERROR << "rknn_create_mem output_mems_ error." << std::endl;
         return false;
       }
+
+      // The data type of output data is changed to FP32
+      output_attrs_[i].type = RKNN_TENSOR_FLOAT32;
+      DumpTensorAttr(output_attrs_[i]);
+
       // default output type is depend on model, this requires float32 to compute top5
       ret = rknn_set_io_mem(ctx, output_mems_[i], &output_attrs_[i]);
 
