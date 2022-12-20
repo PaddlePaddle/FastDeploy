7fa045427533b817cdaee0ca0eea665d56761818
ZhengBicheng <a192161647@163.com>
2022-12-20 14:25:15 +0800

    重构insightface代码

---
+++ fastdeploy/vision/faceid/contrib/insightface_rec/model.h
@@ -0,0 +1,138 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.  //NOLINT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include "fastdeploy/vision/faceid/contrib/insightface_rec/base.h"
+namespace fastdeploy {
+namespace vision {
+namespace faceid {
+class FASTDEPLOY_DECL ArcFace : public InsightFaceRecognitionBase {
+ public:
+  /** \brief Set path of model file and configuration file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g picodet/model.pdmodel
+   * \param[in] params_file Path of parameter file, e.g picodet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
+   * \param[in] model_format Model format of the loaded model, default is Paddle format
+   */
+  ArcFace(const std::string& model_file, const std::string& params_file = "",
+          const RuntimeOption& custom_option = RuntimeOption(),
+          const ModelFormat& model_format = ModelFormat::ONNX)
+      : InsightFaceRecognitionBase(model_file, params_file, custom_option,
+                                   model_format) {
+    valid_cpu_backends = {Backend::OPENVINO, Backend::ORT, Backend::PDINFER,
+                          Backend::LITE};
+    valid_gpu_backends = {Backend::ORT, Backend::PDINFER, Backend::TRT};
+    valid_rknpu_backends = {Backend::RKNPU2};
+    initialized = Initialize();
+  }
+
+  virtual std::string ModelName() const { return "ArcFace"; }
+};
+class FASTDEPLOY_DECL AdaFace : public InsightFaceRecognitionBase {
+ public:
+  /** \brief Set path of model file and configuration file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g picodet/model.pdmodel
+   * \param[in] params_file Path of parameter file, e.g picodet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
+   * \param[in] model_format Model format of the loaded model, default is Paddle format
+   */
+  AdaFace(const std::string& model_file, const std::string& params_file = "",
+          const RuntimeOption& custom_option = RuntimeOption(),
+          const ModelFormat& model_format = ModelFormat::ONNX)
+      : InsightFaceRecognitionBase(model_file, params_file, custom_option,
+                                   model_format) {
+    valid_cpu_backends = {Backend::OPENVINO, Backend::ORT, Backend::PDINFER,
+                          Backend::LITE};
+    valid_gpu_backends = {Backend::ORT, Backend::PDINFER, Backend::TRT};
+    valid_rknpu_backends = {Backend::RKNPU2};
+    initialized = Initialize();
+  }
+
+  virtual std::string ModelName() const { return "AdaFace"; }
+};
+class FASTDEPLOY_DECL CosFace : public InsightFaceRecognitionBase {
+ public:
+  /** \brief Set path of model file and configuration file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g picodet/model.pdmodel
+   * \param[in] params_file Path of parameter file, e.g picodet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
+   * \param[in] model_format Model format of the loaded model, default is Paddle format
+   */
+  CosFace(const std::string& model_file, const std::string& params_file = "",
+          const RuntimeOption& custom_option = RuntimeOption(),
+          const ModelFormat& model_format = ModelFormat::ONNX)
+      : InsightFaceRecognitionBase(model_file, params_file, custom_option,
+                                   model_format) {
+    valid_cpu_backends = {Backend::OPENVINO, Backend::ORT, Backend::PDINFER,
+                          Backend::LITE};
+    valid_gpu_backends = {Backend::ORT, Backend::PDINFER, Backend::TRT};
+    valid_rknpu_backends = {Backend::RKNPU2};
+    initialized = Initialize();
+  }
+
+  virtual std::string ModelName() const { return "CosFace"; }
+};
+class FASTDEPLOY_DECL PartialFC : public InsightFaceRecognitionBase {
+ public:
+  /** \brief Set path of model file and configuration file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g picodet/model.pdmodel
+   * \param[in] params_file Path of parameter file, e.g picodet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
+   * \param[in] model_format Model format of the loaded model, default is Paddle format
+   */
+  PartialFC(const std::string& model_file, const std::string& params_file = "",
+          const RuntimeOption& custom_option = RuntimeOption(),
+          const ModelFormat& model_format = ModelFormat::ONNX)
+      : InsightFaceRecognitionBase(model_file, params_file, custom_option,
+                                   model_format) {
+    valid_cpu_backends = {Backend::OPENVINO, Backend::ORT, Backend::PDINFER,
+                          Backend::LITE};
+    valid_gpu_backends = {Backend::ORT, Backend::PDINFER, Backend::TRT};
+    valid_rknpu_backends = {Backend::RKNPU2};
+    initialized = Initialize();
+  }
+
+  virtual std::string ModelName() const { return "PartialFC"; }
+};
+class FASTDEPLOY_DECL VPL : public InsightFaceRecognitionBase {
+ public:
+  /** \brief Set path of model file and configuration file, and the configuration of runtime
+   *
+   * \param[in] model_file Path of model file, e.g picodet/model.pdmodel
+   * \param[in] params_file Path of parameter file, e.g picodet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+   * \param[in] custom_option RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
+   * \param[in] model_format Model format of the loaded model, default is Paddle format
+   */
+  VPL(const std::string& model_file, const std::string& params_file = "",
+            const RuntimeOption& custom_option = RuntimeOption(),
+            const ModelFormat& model_format = ModelFormat::ONNX)
+      : InsightFaceRecognitionBase(model_file, params_file, custom_option,
+                                   model_format) {
+    valid_cpu_backends = {Backend::OPENVINO, Backend::ORT, Backend::PDINFER,
+                          Backend::LITE};
+    valid_gpu_backends = {Backend::ORT, Backend::PDINFER, Backend::TRT};
+    valid_rknpu_backends = {Backend::RKNPU2};
+    initialized = Initialize();
+  }
+
+  virtual std::string ModelName() const { return "VPL"; }
+};
+
+}  // namespace faceid
+}  // namespace vision
+}  // namespace fastdeploy
---
+++ fastdeploy/vision/faceid/contrib/insightface_rec/preprocessor.cc
@@ -0,0 +1,78 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "fastdeploy/vision/faceid/contrib/insightface_rec/preprocessor.h"
+
+namespace fastdeploy {
+namespace vision {
+namespace faceid {
+
+InsightFaceRecognitionPreprocessor::InsightFaceRecognitionPreprocessor() {
+  // parameters for preprocess
+  size_ = {112, 112};
+  alpha_ = {1.f / 127.5f, 1.f / 127.5f, 1.f / 127.5f};
+  beta_ = {-1.f, -1.f, -1.f};  // RGB
+  swap_rb_ = true;
+}
+
+bool InsightFaceRecognitionPreprocessor::Preprocess(FDMat * mat, FDTensor* output) {
+
+  // face recognition model's preprocess steps in insightface
+  // reference: insightface/recognition/arcface_torch/inference.py
+  // 1. Resize
+  // 2. BGR2RGB
+  // 3. Convert(opencv style) or Normalize
+  // 4. HWC2CHW
+  int resize_w = size_[0];
+  int resize_h = size_[1];
+  if (resize_h != mat->Height() || resize_w != mat->Width()) {
+    Resize::Run(mat, resize_w, resize_h);
+  }
+  if (swap_rb_) {
+    BGR2RGB::Run(mat);
+  }
+
+  Convert::Run(mat, alpha_, beta_);
+  HWC2CHW::Run(mat);
+  Cast::Run(mat, "float");
+
+  mat->ShareWithTensor(output);
+  output->shape.insert(output->shape.begin(), 1);  // reshape to n, h, w, c
+  return true;
+}
+
+bool InsightFaceRecognitionPreprocessor::Run(std::vector<FDMat>* images,
+                                             std::vector<FDTensor>* outputs) {
+  if (images->empty()) {
+    FDERROR << "The size of input images should be greater than 0." << std::endl;
+    return false;
+  }
+  outputs->resize(1);
+  // Concat all the preprocessed data to a batch tensor
+  std::vector<FDTensor> tensors(images->size());
+  for (size_t i = 0; i < images->size(); ++i) {
+    if (!Preprocess(&(*images)[i], &tensors[i])) {
+      FDERROR << "Failed to preprocess input image." << std::endl;
+      return false;
+    }
+  }
+
+  if (tensors.size() == 1) {
+    (*outputs)[0] = std::move(tensors[0]);
+  }
+  return true;
+}
+}  // namespace faceid
+}  // namespace vision
+}  // namespace fastdeploy
