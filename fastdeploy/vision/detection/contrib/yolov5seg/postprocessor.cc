12075edfe87ca072624f80dd3527297f4f76a161
wjj19950828 <wjjisloser@163.com>
2022-12-29 09:41:28 +0000

    rm log info

--- fastdeploy/vision/detection/contrib/yolov5seg/postprocessor.cc
+++ fastdeploy/vision/detection/contrib/yolov5seg/postprocessor.cc
@@ -31,10 +31,7 @@ YOLOv5SegPostprocessor::YOLOv5SegPostprocessor() {
 bool YOLOv5SegPostprocessor::Run(const std::vector<FDTensor>& tensors, std::vector<DetectionResult>* results,
                               const std::vector<std::map<std::string, std::array<float, 2>>>& ims_info) {
   int batch = tensors[0].shape[0];
-  // test
-  std::cout << "1111111" << std::endl;
-  tensors[0].PrintInfo();
-  tensors[1].PrintInfo();
+
   results->resize(batch);
 
   for (size_t bs = 0; bs < batch; ++bs) {
@@ -104,57 +101,20 @@ bool YOLOv5SegPostprocessor::Run(const std::vector<FDTensor>& tensors, std::vect
     if ((*results)[bs].boxes.size() == 0) {
       return true;
     }
-    std::cout << "before NMS" << std::endl;
-    std::cout << (*results)[bs].yolo_masks.size() << std::endl;
-    std::cout << (*results)[bs].boxes.size() << std::endl;
-    std::cout << (*results)[bs].contain_masks << std::endl;
+
     utils::NMS(&((*results)[bs]), nms_threshold_);
-    std::cout << "after NMS" << std::endl;
-    std::cout << (*results)[bs].yolo_masks.size() << std::endl;
-    std::cout << (*results)[bs].boxes.size() << std::endl;
-    std::cout << (*results)[bs].contain_masks << std::endl;
 
     // deal with mask
-    std::cout << "2222222222" << std::endl;
     (*results)[bs].masks.resize((*results)[bs].boxes.size());
     const float* data_mask = reinterpret_cast<const float*>(tensors[1].Data()) + bs * tensors[1].shape[1] * tensors[1].shape[2] * tensors[1].shape[3];
     cv::Mat mask_proto = cv::Mat(tensors[1].shape[1], tensors[1].shape[2] * tensors[1].shape[3], CV_32FC(1), const_cast<float*>(data_mask));
-    std::cout << "333333333333" << std::endl;
-    std::cout << mask_proto.size() << std::endl;
-    std::cout << (*results)[bs].yolo_masks.size() << std::endl;
-    std::cout << (*results)[bs].boxes.size() << std::endl;
     // vector to cv::Mat for Matmul
     cv::Mat mask_proposals;
     for (size_t i = 0; i < (*results)[bs].yolo_masks.size(); ++i) {
-      std::string out;
-      out = out + std::to_string((*results)[bs].boxes[i][0]) + "," +
-          std::to_string((*results)[bs].boxes[i][1]) + ", " + std::to_string((*results)[bs].boxes[i][2]) +
-          ", " + std::to_string((*results)[bs].boxes[i][3]);
-      std::cout << out << std::endl;
-      std::cout << "test_wjj111111:" << std::endl;
-      for (size_t j = 0; j < 32; ++j) {
-        std::cout << (*results)[bs].yolo_masks[i][j] << std::endl;
-      }
-      std::cout << cv::Mat((*results)[bs].yolo_masks[i]).size() << std::endl;
       mask_proposals.push_back(cv::Mat((*results)[bs].yolo_masks[i]).t());
     }
-    std::cout << "444444444444" << std::endl;
-    std::cout << mask_proposals.size() << std::endl;
-    float* test_ptr_b = reinterpret_cast<float*>(mask_proposals.data);
-    for (size_t m = 0; m < 20; ++m) {
-      std::cout << test_ptr_b[m] << std::endl;
-    }
-    std::cout << "test_wjj_aaaaaaa" << std::endl;
     cv::Mat matmul_result = (mask_proposals * mask_proto).t();
-    float* test_ptr_a = reinterpret_cast<float*>(matmul_result.data);
-    for (size_t m = 0; m < 20; ++m) {
-      std::cout << test_ptr_a[m] << std::endl;
-    }
-    std::cout << "5555555555555" << std::endl;
-    std::cout << matmul_result.size() << std::endl;
     cv::Mat masks = matmul_result.reshape((*results)[bs].boxes.size(), {static_cast<int>(tensors[1].shape[2]), static_cast<int>(tensors[1].shape[3])});
-    std::cout << "6666666666666" << std::endl;
-    std::cout << masks.size() << std::endl;
 
     // split for boxes nums
 	  std::vector<cv::Mat> mask_channels;
@@ -195,48 +155,22 @@ bool YOLOv5SegPostprocessor::Run(const std::vector<FDTensor>& tensors, std::vect
       // sigmoid
       cv::exp(-mask_channels[i], dest);
 		  dest = 1.0 / (1.0 + dest);
-		  // crop mask feature map
+		  // crop mask for feature map
       int x1 = static_cast<int>(pad_w_mask);
       int y1 = static_cast<int>(pad_h_mask);
       int x2 = static_cast<int>(tensors[1].shape[3] - pad_w_mask);
       int y2 = static_cast<int>(tensors[1].shape[2] - pad_h_mask);
-      float* test_ptr_c = reinterpret_cast<float*>(dest.data);
-      for (size_t m = 0; m < 20; ++m) {
-        std::cout << test_ptr_c[m] << std::endl;
-      }
-      std::cout << "7777777777" << std::endl;
-      std::cout << dest.size() << std::endl;
-      std::cout << x1 << ", " << y1 << ", " << x2 << ", " << y2 << std::endl;
 		  cv::Rect roi(x1, y1, x2-x1, y2-y1);
-      std::cout << roi << std::endl;
 		  dest = dest(roi);
-      float* test_ptr0 = reinterpret_cast<float*>(dest.data);
-      for (size_t m = 0; m < 20; ++m) {
-        std::cout << test_ptr0[m] << std::endl;
-      }
       cv::resize(dest, mask, cv::Size(ipt_w, ipt_h), 0, 0, cv::INTER_LINEAR);
-      std::cout << "88888888888" << std::endl;
-      std::cout << mask.size() << std::endl;
 		  // crop mask for source img
       int x1_src = static_cast<int>((*results)[bs].boxes[i][0]);
       int y1_src = static_cast<int>((*results)[bs].boxes[i][1]);
       int x2_src = static_cast<int>((*results)[bs].boxes[i][2]);
       int y2_src = static_cast<int>((*results)[bs].boxes[i][3]);
       cv::Rect roi_src(x1_src, y1_src, x2_src-x1_src, y2_src-y1_src);
-      std::cout << roi_src << std::endl;
       mask = mask(roi_src);
-      std::cout << "99999999999" << std::endl;
-      std::cout << mask.size() << std::endl;
-      float* test_ptr1 = reinterpret_cast<float*>(mask.data);
-      for (size_t m = 0; m < 20; ++m) {
-        std::cout << test_ptr1[m] << std::endl;
-      }
 		  mask = mask > mask_threshold_;
-      // mask.convertTo(mask, CV_32SC1);
-      int32_t* test_ptr2 = reinterpret_cast<int32_t*>(mask.ptr());
-      for (size_t m = 0; m < 20; ++m) {
-        std::cout << test_ptr2[m] << std::endl;
-      }
       // save mask in DetectionResult
       int keep_mask_h = y2_src - y1_src;
       int keep_mask_w = x2_src - x1_src;
