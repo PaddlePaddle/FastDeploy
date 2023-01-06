a682763373ce5965d2e7dc375e48fc6d1116018a
Jason <jiangjiajun@baidu.com>
2023-01-06 13:44:05 +0800

    [Other] Upgrade runtime module (#1068)
    * Upgrade runtime module
    
    * Update option.h
    
    * Fix build error
    
    * Move enumerates
    
    * little modification
    
    * little modification
    
    * little modification:
    
    * Remove some useless flags

--- fastdeploy/backends/rknpu2/rknpu2_backend.cc
+++ fastdeploy/backends/rknpu2/rknpu2_backend.cc
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "fastdeploy/backends/rknpu/rknpu2/rknpu2_backend.h"
+#include "fastdeploy/backends/rknpu2/rknpu2_backend.h"
 #include "fastdeploy/utils/perf.h"
 namespace fastdeploy {
 RKNPU2Backend::~RKNPU2Backend() {
