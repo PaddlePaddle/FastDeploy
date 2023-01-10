9e9687e41f1fc79073479c58896182bbae148ab7
Jason <928090362@qq.com>
2023-01-10 13:29:52 +0800

    Update fastdeploy_init.sh

--- scripts/fastdeploy_init.sh
+++ scripts/fastdeploy_init.sh
@@ -28,13 +28,10 @@ done
 LIBS_DIRECOTRIES=($(awk -v RS=' ' '!a[$1]++' <<< ${LIBS_DIRECOTRIES[@]}))
 
 # Print the dynamic library location and output the configuration file
-output_file=$(dirname "${BASH_SOURCE[0]}")/fastdeploy_libs.conf
-rm $output_file
-IMPORT_PATH=""
+output_file=${FASTDEPLOY_LIBRARY_PATH}/fastdeploy_libs.conf
+rm -rf $output_file
 for LIB_DIR in ${LIBS_DIRECOTRIES[@]};do
-    echo "Find Library Directory: $LIB_DIR"
     echo "$LIB_DIR" >> $output_file
-    IMPORT_PATH=${LIB_DIR}":"$IMPORT_PATH
 done
 
 if [ -f "ascend_init.sh" ]
