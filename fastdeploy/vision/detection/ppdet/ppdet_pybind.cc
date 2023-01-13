b761a6225fb56c2a57f44df750b0578729014097
ZhengBicheng <a192161647@163.com>
2023-01-13 10:26:31 +0800

    尝试解决pybind问题

--- fastdeploy/vision/detection/ppdet/ppdet_pybind.cc
+++ fastdeploy/vision/detection/ppdet/ppdet_pybind.cc
@@ -18,52 +18,67 @@ void BindPPDet(pybind11::module& m) {
   pybind11::class_<vision::detection::PaddleDetPreprocessor>(
       m, "PaddleDetPreprocessor")
       .def(pybind11::init<std::string>())
-      .def("run", [](vision::detection::PaddleDetPreprocessor& self, std::vector<pybind11::array>& im_list) {
-        std::vector<vision::FDMat> images;
-        for (size_t i = 0; i < im_list.size(); ++i) {
-          images.push_back(vision::WrapMat(PyArrayToCvMat(im_list[i])));
-        }
-        std::vector<FDTensor> outputs;
-        if (!self.Run(&images, &outputs)) {
-          throw std::runtime_error("Failed to preprocess the input data in PaddleDetPreprocessor.");
-        }
-        for (size_t i = 0; i < outputs.size(); ++i) {
-          outputs[i].StopSharing();
-        }
-        return outputs;
-      })
-      .def("disable_normalize", [](vision::detection::PaddleDetPreprocessor& self) {
-        self.DisableNormalize();
-      })
-      .def("disable_permute", [](vision::detection::PaddleDetPreprocessor& self) {
-        self.DisablePermute();
-      });;
+      .def("run",
+           [](vision::detection::PaddleDetPreprocessor& self,
+              std::vector<pybind11::array>& im_list) {
+             std::vector<vision::FDMat> images;
+             for (size_t i = 0; i < im_list.size(); ++i) {
+               images.push_back(vision::WrapMat(PyArrayToCvMat(im_list[i])));
+             }
+             std::vector<FDTensor> outputs;
+             if (!self.Run(&images, &outputs)) {
+               throw std::runtime_error(
+                   "Failed to preprocess the input data in "
+                   "PaddleDetPreprocessor.");
+             }
+             for (size_t i = 0; i < outputs.size(); ++i) {
+               outputs[i].StopSharing();
+             }
+             return outputs;
+           })
+      .def("disable_normalize",
+           [](vision::detection::PaddleDetPreprocessor& self) {
+             self.DisableNormalize();
+           })
+      .def("disable_permute",
+           [](vision::detection::PaddleDetPreprocessor& self) {
+             self.DisablePermute();
+           });
+  ;
 
   pybind11::class_<vision::detection::PaddleDetPostprocessor>(
       m, "PaddleDetPostprocessor")
       .def(pybind11::init<>())
-      .def("run", [](vision::detection::PaddleDetPostprocessor& self, std::vector<FDTensor>& inputs) {
-        std::vector<vision::DetectionResult> results;
-        if (!self.Run(inputs, &results)) {
-          throw std::runtime_error("Failed to postprocess the runtime result in PaddleDetPostprocessor.");
-        }
-        return results;
-      })
+      .def("run",
+           [](vision::detection::PaddleDetPostprocessor& self,
+              std::vector<FDTensor>& inputs) {
+             std::vector<vision::DetectionResult> results;
+             if (!self.Run(inputs, &results)) {
+               throw std::runtime_error(
+                   "Failed to postprocess the runtime result in "
+                   "PaddleDetPostprocessor.");
+             }
+             return results;
+           })
       .def("apply_decode_and_nms",
-           [](vision::detection::PaddleDetPostprocessor& self){
+           [](vision::detection::PaddleDetPostprocessor& self) {
              self.ApplyDecodeAndNMS();
            })
-      .def("run", [](vision::detection::PaddleDetPostprocessor& self, std::vector<pybind11::array>& input_array) {
+      .def("run", [](vision::detection::PaddleDetPostprocessor& self,
+                     std::vector<pybind11::array>& input_array) {
         std::vector<vision::DetectionResult> results;
         std::vector<FDTensor> inputs;
         PyArrayToTensorList(input_array, &inputs, /*share_buffer=*/true);
         if (!self.Run(inputs, &results)) {
-          throw std::runtime_error("Failed to postprocess the runtime result in PaddleDetPostprocessor.");
+          throw std::runtime_error(
+              "Failed to postprocess the runtime result in "
+              "PaddleDetPostprocessor.");
         }
         return results;
       });
 
-  pybind11::class_<vision::detection::PPDetBase, FastDeployModel>(m, "PPDetBase")
+  pybind11::class_<vision::detection::PPDetBase, FastDeployModel>(m,
+                                                                  "PPDetBase")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>())
       .def("predict",
@@ -74,92 +89,116 @@ void BindPPDet(pybind11::module& m) {
              return res;
            })
       .def("batch_predict",
-           [](vision::detection::PPDetBase& self, std::vector<pybind11::array>& data) {
+           [](vision::detection::PPDetBase& self,
+              std::vector<pybind11::array>& data) {
              std::vector<cv::Mat> images;
              for (size_t i = 0; i < data.size(); ++i) {
-              images.push_back(PyArrayToCvMat(data[i]));
+               images.push_back(PyArrayToCvMat(data[i]));
              }
              std::vector<vision::DetectionResult> results;
              self.BatchPredict(images, &results);
              return results;
            })
-      .def("clone", [](vision::detection::PPDetBase& self) {
-        return self.Clone();
-      })
-      .def_property_readonly("preprocessor", &vision::detection::PPDetBase::GetPreprocessor)
-      .def_property_readonly("postprocessor", &vision::detection::PPDetBase::GetPostprocessor);
+      .def("clone",
+           [](vision::detection::PPDetBase& self) { return self.Clone(); })
+      .def_property_readonly("preprocessor",
+                             &vision::detection::PPDetBase::GetPreprocessor)
+      .def_property_readonly("postprocessor",
+                             &vision::detection::PPDetBase::GetPostprocessor);
 
+  pybind11::class_<vision::detection::PPDetDecode>(m, "PPDetDecode")
+      .def(pybind11::init<std::string>());
 
-  pybind11::class_<vision::detection::PPYOLO, vision::detection::PPDetBase>(m, "PPYOLO")
-      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
-                          ModelFormat>());
-
-  pybind11::class_<vision::detection::PPYOLOE, vision::detection::PPDetBase>(m, "PPYOLOE")
-      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
-                          ModelFormat>());
-
-  pybind11::class_<vision::detection::PicoDet, vision::detection::PPDetBase>(m, "PicoDet")
+  pybind11::class_<vision::detection::PPYOLO, vision::detection::PPDetBase>(
+      m, "PPYOLO")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::PaddleYOLOX, vision::detection::PPDetBase>(m, "PaddleYOLOX")
+  pybind11::class_<vision::detection::PPYOLOE, vision::detection::PPDetBase>(
+      m, "PPYOLOE")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::FasterRCNN, vision::detection::PPDetBase>(m, "FasterRCNN")
+  pybind11::class_<vision::detection::PicoDet, vision::detection::PPDetBase>(
+      m, "PicoDet")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::YOLOv3, vision::detection::PPDetBase>(m, "YOLOv3")
+  pybind11::class_<vision::detection::PaddleYOLOX,
+                   vision::detection::PPDetBase>(m, "PaddleYOLOX")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::MaskRCNN, vision::detection::PPDetBase>(m, "MaskRCNN")
+  pybind11::class_<vision::detection::FasterRCNN, vision::detection::PPDetBase>(
+      m, "FasterRCNN")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::SSD, vision::detection::PPDetBase>(m, "SSD")
+  pybind11::class_<vision::detection::YOLOv3, vision::detection::PPDetBase>(
+      m, "YOLOv3")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::PaddleYOLOv5, vision::detection::PPDetBase>(m, "PaddleYOLOv5")
+  pybind11::class_<vision::detection::MaskRCNN, vision::detection::PPDetBase>(
+      m, "MaskRCNN")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::PaddleYOLOv6, vision::detection::PPDetBase>(m, "PaddleYOLOv6")
+  pybind11::class_<vision::detection::SSD, vision::detection::PPDetBase>(m,
+                                                                         "SSD")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::PaddleYOLOv7, vision::detection::PPDetBase>(m, "PaddleYOLOv7")
+  pybind11::class_<vision::detection::PaddleYOLOv5,
+                   vision::detection::PPDetBase>(m, "PaddleYOLOv5")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
 
-  pybind11::class_<vision::detection::RTMDet, vision::detection::PPDetBase>(m, "RTMDet")
-      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
-                          ModelFormat>());    
-  pybind11::class_<vision::detection::CascadeRCNN, vision::detection::PPDetBase>(m, "CascadeRCNN")
-      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
-                          ModelFormat>());    
-  pybind11::class_<vision::detection::PSSDet, vision::detection::PPDetBase>(m, "PSSDet")
+  pybind11::class_<vision::detection::PaddleYOLOv6,
+                   vision::detection::PPDetBase>(m, "PaddleYOLOv6")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::RetinaNet, vision::detection::PPDetBase>(m, "RetinaNet")
+
+  pybind11::class_<vision::detection::PaddleYOLOv7,
+                   vision::detection::PPDetBase>(m, "PaddleYOLOv7")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::PPYOLOESOD, vision::detection::PPDetBase>(m, "PPYOLOESOD")
+
+  pybind11::class_<vision::detection::RTMDet, vision::detection::PPDetBase>(
+      m, "RTMDet")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::FCOS, vision::detection::PPDetBase>(m, "FCOS")
+  pybind11::class_<vision::detection::CascadeRCNN,
+                   vision::detection::PPDetBase>(m, "CascadeRCNN")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::TTFNet, vision::detection::PPDetBase>(m, "TTFNet")
+  pybind11::class_<vision::detection::PSSDet, vision::detection::PPDetBase>(
+      m, "PSSDet")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::TOOD, vision::detection::PPDetBase>(m, "TOOD")
+  pybind11::class_<vision::detection::RetinaNet, vision::detection::PPDetBase>(
+      m, "RetinaNet")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
                           ModelFormat>());
-  pybind11::class_<vision::detection::GFL, vision::detection::PPDetBase>(m, "GFL")
+  pybind11::class_<vision::detection::PPYOLOESOD, vision::detection::PPDetBase>(
+      m, "PPYOLOESOD")
       .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
-                          ModelFormat>());                                                                                                                                                                                              
+                          ModelFormat>());
+  pybind11::class_<vision::detection::FCOS, vision::detection::PPDetBase>(
+      m, "FCOS")
+      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
+                          ModelFormat>());
+  pybind11::class_<vision::detection::TTFNet, vision::detection::PPDetBase>(
+      m, "TTFNet")
+      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
+                          ModelFormat>());
+  pybind11::class_<vision::detection::TOOD, vision::detection::PPDetBase>(
+      m, "TOOD")
+      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
+                          ModelFormat>());
+  pybind11::class_<vision::detection::GFL, vision::detection::PPDetBase>(m,
+                                                                         "GFL")
+      .def(pybind11::init<std::string, std::string, std::string, RuntimeOption,
+                          ModelFormat>());
 }
 }  // namespace fastdeploy
