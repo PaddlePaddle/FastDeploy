c99ce0f9288e44ed06001e281e529c068ca0b1c1
Zheng-Bicheng <58363586+Zheng-Bicheng@users.noreply.github.com>
2023-02-08 19:46:11 +0800

    [Bug Fix] Update RKYOLO code format (#1254)
    * 更新rkyolo细节
    
    * 更新rkyolov5 format

--- fastdeploy/vision/detection/contrib/rknpu2/utils.h
+++ fastdeploy/vision/detection/contrib/rknpu2/utils.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #pragma once
-#include <stdint.h>
+#include <cstdint>
 #include <cmath>
 #include <vector>
 
@@ -20,11 +20,6 @@ namespace fastdeploy {
 namespace vision {
 namespace detection {
 float Clamp(float val, int min, int max);
-float Sigmoid(float x);
-float UnSigmoid(float y);
-inline static int32_t __clip(float val, float min, float max);
-int8_t QntF32ToAffine(float f32, int32_t zp, float scale);
-float DeqntAffineToF32(int8_t qnt, int32_t zp, float scale);
 int NMS(int valid_count, std::vector<float>& output_locations,
         std::vector<int>& class_id, std::vector<int>& order, float threshold,
         bool class_agnostic);
