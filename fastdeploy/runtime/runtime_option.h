6e4449fd9870c8117da0dd167b30c6c7af74ec92
wwbitejotunn <wang_bojun@outlook.com>
2023-02-09 12:53:08 +0000

    set stream by raw size_t

--- fastdeploy/runtime/runtime_option.h
+++ fastdeploy/runtime/runtime_option.h
@@ -111,6 +111,8 @@ struct FASTDEPLOY_DECL RuntimeOption {
 
   void SetExternalStream(void* external_stream);
 
+  void SetExternalRawStream(size_t external_stream);
+
   /*
    * @brief Set number of cpu threads while inference on CPU, by default it will decided by the different backends
    */
