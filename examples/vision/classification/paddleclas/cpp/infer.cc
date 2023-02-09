b982f45f7278641857151dc27ad3421c7889b15a
yunyaoXYY <imylovex1@163.com>
2023-02-09 20:27:11 +0800

    Support onnxruntime DirectML

--- examples/vision/classification/paddleclas/cpp/infer.cc
+++ examples/vision/classification/paddleclas/cpp/infer.cc
@@ -96,7 +96,8 @@ void IpuInfer(const std::string& model_dir, const std::string& image_file) {
   std::cout << res.Str() << std::endl;
 }
 
-void KunlunXinInfer(const std::string& model_dir, const std::string& image_file) {
+void KunlunXinInfer(const std::string& model_dir,
+                    const std::string& image_file) {
   auto model_file = model_dir + sep + "inference.pdmodel";
   auto params_file = model_dir + sep + "inference.pdiparams";
   auto config_file = model_dir + sep + "inference_cls.yaml";
@@ -152,7 +153,7 @@ void AscendInfer(const std::string& model_dir, const std::string& image_file) {
   auto model_file = model_dir + sep + "inference.pdmodel";
   auto params_file = model_dir + sep + "inference.pdiparams";
   auto config_file = model_dir + sep + "inference_cls.yaml";
-  
+
   auto option = fastdeploy::RuntimeOption();
   option.UseAscend();
 
@@ -172,6 +173,30 @@ void AscendInfer(const std::string& model_dir, const std::string& image_file) {
   std::cout << res.Str() << std::endl;
 }
 
+void DirectMLInfer(const std::string& model_dir,
+                   const std::string& image_file) {
+  auto model_file = model_dir + sep + "inference.pdmodel";
+  auto params_file = model_dir + sep + "inference.pdiparams";
+  auto config_file = model_dir + sep + "inference_cls.yaml";
+
+  auto option = fastdeploy::RuntimeOption();
+  option.UseDirectML();
+
+  auto model = fastdeploy::vision::classification::PaddleClasModel(
+      model_file, params_file, config_file, option);
+
+  assert(model.Initialized());
+
+  auto im = cv::imread(image_file);
+
+  fastdeploy::vision::ClassifyResult res;
+  if (!model.Predict(&im, &res)) {
+    std::cerr << "Failed to predict." << std::endl;
+    return;
+  }
+
+  std::cout << res.Str() << std::endl;
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 4) {
@@ -179,7 +204,8 @@ int main(int argc, char* argv[]) {
                  "e.g ./infer_demo ./ResNet50_vd ./test.jpeg 0"
               << std::endl;
     std::cout << "The data type of run_option is int, 0: run with cpu; 1: run "
-                 "with gpu; 2: run with gpu and use tensorrt backend; 3: run with ipu; 4: run with kunlunxin."
+                 "with gpu; 2: run with gpu and use tensorrt backend; 3: run "
+                 "with ipu; 4: run with kunlunxin."
               << std::endl;
     return -1;
   }
@@ -196,6 +222,8 @@ int main(int argc, char* argv[]) {
     KunlunXinInfer(argv[1], argv[2]);
   } else if (std::atoi(argv[3]) == 5) {
     AscendInfer(argv[1], argv[2]);
+  } else if (std::atoi(argv[3]) == 6) {
+    DirectMLInfer(argv[1], argv[2]);
   }
   return 0;
 }
