0c70989b40b58b1f8d672c7ad3f50f848db813f1
yunyaoXYY <imylovex1@163.com>
2023-02-13 11:08:34 +0800

    Support OnnxRuntime DirectML

--- fastdeploy/runtime/backends/ort/ort_backend.h
+++ fastdeploy/runtime/backends/ort/ort_backend.h
@@ -23,6 +23,7 @@
 #include "fastdeploy/runtime/backends/backend.h"
 #include "fastdeploy/runtime/backends/ort/option.h"
 #include "onnxruntime_cxx_api.h"  // NOLINT
+
 #ifdef WITH_DIRECTML
 #include "dml_provider_factory.h" // NOLINT
 #endif
