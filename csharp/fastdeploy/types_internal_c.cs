b7b68f8c40074be43bab8b2b5a10fc34d60d846b
chenjian <chenjian26@baidu.com>
2023-02-15 14:27:31 +0800

    [C# api] add c sharp api for fastdeploy (#1246)
    * add c sharp api for fastdeploy
    
    * update accroding to c apis
    
    * add cmakelist for c sharp api
    
    * add cmakelists for c sharp
    
    * fix cmakelists
    
    * fix cmakelists
    
    * add c sharp api for fastdeploy
    
    * add ppyoloe demo
    
    * add ppyoloe demo
    
    * modify demo namespace code
    
    * add readme
    
    * fix format
    
    * format code
    
    * fix doc
    
    ---------
    
    Co-authored-by: heliqi <1101791222@qq.com>

---
+++ csharp/fastdeploy/types_internal_c.cs
@@ -0,0 +1,125 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using fastdeploy.vision;
+
+namespace fastdeploy {
+namespace types_internal_c {
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArrayUint8 {
+  public nuint size;
+  public IntPtr data;  // byte[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArrayInt32 {
+  public nuint size;
+  public IntPtr data;  // int[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArraySize {
+  public nuint size;
+  public IntPtr data;  // nuint[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArrayInt64 {
+  public nuint size;
+  public IntPtr data;  // long[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArrayFloat {
+  public nuint size;
+  public IntPtr data;  // float[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_Cstr {
+  public nuint size;
+  public string data;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimArrayCstr {
+  public nuint size;
+  public IntPtr data;  // FD_Cstr[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_TwoDimArraySize {
+  public nuint size;
+  public IntPtr data;  // FD_OneDimArraySize[]
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_TwoDimArrayFloat {
+  public nuint size;
+  public IntPtr data;  // FD_OneDimArrayFloat[]
+}
+
+public enum FD_ResultType {
+  UNKNOWN_RESULT,
+  CLASSIFY,
+  DETECTION,
+  SEGMENTATION,
+  OCR,
+  MOT,
+  FACE_DETECTION,
+  FACE_ALIGNMENT,
+  FACE_RECOGNITION,
+  MATTING,
+  MASK,
+  KEYPOINT_DETECTION,
+  HEADPOSE
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_ClassifyResult {
+  public FD_OneDimArrayInt32 label_ids;
+  public FD_OneDimArrayFloat scores;
+  public FD_ResultType type;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_Mask {
+  public FD_OneDimArrayUint8 data;
+  public FD_OneDimArrayInt64 shape;
+  public FD_ResultType type;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_OneDimMask {
+  public nint size;
+  public IntPtr data;  // FD_Mask*
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct FD_DetectionResult {
+  public FD_TwoDimArrayFloat boxes;
+  public FD_OneDimArrayFloat scores;
+  public FD_OneDimArrayInt32 label_ids;
+  public FD_OneDimMask masks;
+  [MarshalAs(UnmanagedType.U1)]
+  public bool contain_masks;
+  public FD_ResultType type;
+}
+
+}
+}
