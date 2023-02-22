443b7c9003ccc14f0520f962af9d4e90477e5d97
chenjian <chenjian26@baidu.com>
2023-02-22 07:46:16 +0000

    fix bug

--- c_api/fastdeploy_capi/vision/segmentation/ppseg/model.h
+++ c_api/fastdeploy_capi/vision/segmentation/ppseg/model.h
@@ -19,70 +19,70 @@
 #include "fastdeploy_capi/runtime_option.h"
 #include "fastdeploy_capi/vision/result.h"
 
-typedef struct FD_C_PaddleClasModelWrapper FD_C_PaddleClasModelWrapper;
+typedef struct FD_C_PaddleSegModelWrapper FD_C_PaddleSegModelWrapper;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** \brief Create a new FD_C_PaddleClasModelWrapper object
+/** \brief Create a new FD_C_PaddleSegModelWrapper object
  *
- * \param[in] model_file Path of model file, e.g resnet/model.pdmodel
- * \param[in] params_file Path of parameter file, e.g resnet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
- * \param[in] config_file Path of configuration file for deployment, e.g resnet/infer_cfg.yml
+ * \param[in] model_file Path of model file, e.g net/model.pdmodel
+ * \param[in] params_file Path of parameter file, e.g unet/model.pdiparams, if the model format is ONNX, this parameter will be ignored
+ * \param[in] config_file Path of configuration file for deployment, e.g unet/deploy.yml
  * \param[in] fd_c_runtime_option_wrapper RuntimeOption for inference, the default will use cpu, and choose the backend defined in `valid_cpu_backends`
  * \param[in] model_format Model format of the loaded model, default is Paddle format
  *
- * \return Return a pointer to FD_C_PaddleClasModelWrapper object
+ * \return Return a pointer to FD_C_PaddleSegModelWrapper object
  */
 
-FASTDEPLOY_CAPI_EXPORT extern __fd_give FD_C_PaddleClasModelWrapper*
-FD_C_CreatePaddleClasModelWrapper(
+FASTDEPLOY_CAPI_EXPORT extern __fd_give FD_C_PaddleSegModelWrapper*
+FD_C_CreatePaddleSegModelWrapper(
     const char* model_file, const char* params_file, const char* config_file,
     FD_C_RuntimeOptionWrapper* fd_c_runtime_option_wrapper,
     const FD_C_ModelFormat model_format);
 
-/** \brief Destroy a FD_C_PaddleClasModelWrapper object
+/** \brief Destroy a FD_C_PaddleSegModelWrapper object
  *
- * \param[in] fd_c_paddleclas_model_wrapper pointer to FD_C_PaddleClasModelWrapper object
+ * \param[in] fd_c_paddleseg_model_wrapper pointer to FD_C_PaddleSegModelWrapper object
  */
 
-FASTDEPLOY_CAPI_EXPORT extern void FD_C_DestroyPaddleClasModelWrapper(
-    __fd_take FD_C_PaddleClasModelWrapper* fd_c_paddleclas_model_wrapper);
+FASTDEPLOY_CAPI_EXPORT extern void FD_C_DestroyPaddleSegModelWrapper(
+    __fd_take FD_C_PaddleSegModelWrapper* fd_c_paddleseg_model_wrapper);
 
-/** \brief Predict the classification result for an input image
+/** \brief Predict the segmentation result for an input image
  *
- * \param[in] fd_c_paddleclas_model_wrapper pointer to FD_C_PaddleClasModelWrapper object
+ * \param[in] fd_c_paddleseg_model_wrapper pointer to FD_C_PaddleSegModelWrapper object
  * \param[in] img pointer to cv::Mat image
- * \param[in] fd_c_classify_result pointer to FD_C_ClassifyResult object, which stores the result.
+ * \param[in] fd_c_segmentation_result pointer to FD_C_SegmentationResult object, which stores the result.
  */
 
-FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleClasModelWrapperPredict(
-    __fd_keep FD_C_PaddleClasModelWrapper* fd_c_paddleclas_model_wrapper,
-    FD_C_Mat img, FD_C_ClassifyResult* fd_c_classify_result_wrapper);
+FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleSegModelWrapperPredict(
+    __fd_keep FD_C_PaddleSegModelWrapper* fd_c_paddleseg_model_wrapper,
+    FD_C_Mat img, FD_C_SegmentationResult* fd_c_segmentation_result);
 
 /** \brief Check if the model is initialized successfully
  *
- * \param[in] fd_c_paddleclas_model_wrapper pointer to FD_C_PaddleClasModelWrapper object
+ * \param[in] fd_c_paddleseg_model_wrapper pointer to FD_C_PaddleSegModelWrapper object
  *
  * \return Return a bool of value true if initialized successfully
  */
 
-FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleClasModelWrapperInitialized(
-    __fd_keep FD_C_PaddleClasModelWrapper* fd_c_paddleclas_model_wrapper);
+FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleSegModelWrapperInitialized(
+    __fd_keep FD_C_PaddleSegModelWrapper* fd_c_paddleseg_model_wrapper);
 
 
-/** \brief Predict the classification results for a batch of input images
+/** \brief Predict the segmentation results for a batch of input images
    *
-   * \param[in] fd_c_paddleclas_model_wrapper pointer to FD_C_PaddleClasModelWrapper object
+   * \param[in] fd_c_paddleseg_model_wrapper pointer to FD_C_PaddleSegModelWrapper object
    * \param[in] imgs The input image list, each element comes from cv::imread()
-   * \param[in] results The output classification result list
+   * \param[in] results The output segmentation result list
    * \return true if the prediction successed, otherwise false
    */
-FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleClasModelWrapperBatchPredict(
-            __fd_keep FD_C_PaddleClasModelWrapper* fd_c_paddleclas_model_wrapper,
+FASTDEPLOY_CAPI_EXPORT extern FD_C_Bool FD_C_PaddleSegModelWrapperBatchPredict(
+            __fd_keep FD_C_PaddleSegModelWrapper* fd_c_paddleseg_model_wrapper,
             FD_C_OneDimMat imgs,
-            FD_C_OneDimClassifyResult* results);
+            FD_C_OneDimSegmentationResult* results);
 
 
 #ifdef __cplusplus
