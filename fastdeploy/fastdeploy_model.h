ee89553f1a013ff5b3d9829f6c629bf7734032a3
DefTruth <31974251+DefTruth@users.noreply.github.com>
2023-02-28 09:57:31 +0800

    [Bug Fix] move enable_valid_backend_check option -> runtime_option (#1450)
    * [Model] Add skip valid backend check option -> FastDeployModel
    
    * [Model] Add skip valid backend check option -> FastDeployModel
    
    * [Model] Add skip valid backend check option -> FastDeployModel
    
    * [FastDeploy] move enable_valid_backend_check option -> runtime_option

--- fastdeploy/fastdeploy_model.h
+++ fastdeploy/fastdeploy_model.h
@@ -121,16 +121,6 @@ class FASTDEPLOY_DECL FastDeployModel {
   virtual double GetProfileTime() {
     return runtime_->GetProfileTime();
   }
-  /** \brief Enable to check if current backend set by user can be found at valid_xxx_backend.
-   */
-  virtual void EnableValidBackendCheck() {
-    enable_valid_backend_check_ = true;
-  }
-  /** \brief Disable to check if current backend set by user can be found at valid_xxx_backend.
-   */
-  virtual void DisableValidBackendCheck() {
-    enable_valid_backend_check_ = false;
-  }
   /** \brief Release reused input/output buffers
   */
   virtual void ReleaseReusedBuffer() {
@@ -179,8 +169,6 @@ class FASTDEPLOY_DECL FastDeployModel {
   // whether to record inference time
   bool enable_record_time_of_runtime_ = false;
   std::vector<double> time_of_runtime_;
-  // enable the check for valid backend, default true.
-  bool enable_valid_backend_check_ = true;
 };
 
 }  // namespace fastdeploy
