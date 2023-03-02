99b1bc5d6e04bda309fa1f27c8277812853d48a3
WJJ1995 <wjjisloser@163.com>
2023-03-02 19:11:21 +0800

    [Benchmark] Add run_benchmark_cpu.sh (#1465)
    * add GPL lisence
    
    * add GPL-3.0 lisence
    
    * add GPL-3.0 lisence
    
    * add GPL-3.0 lisence
    
    * support yolov8
    
    * add pybind for yolov8
    
    * add yolov8 readme
    
    * add cpp benchmark
    
    * add cpu and gpu mem
    
    * public part split
    
    * add runtime mode
    
    * fixed bugs
    
    * add cpu_thread_nums
    
    * deal with comments
    
    * deal with comments
    
    * deal with comments
    
    * rm useless code
    
    * add FASTDEPLOY_DECL
    
    * add FASTDEPLOY_DECL
    
    * fixed for windows
    
    * mv rss to pss
    
    * mv rss to pss
    
    * Update utils.cc
    
    * use thread to collect mem
    
    * Add ResourceUsageMonitor
    
    * rm useless code
    
    * fixed bug
    
    * fixed typo
    
    * update ResourceUsageMonitor
    
    * fixed bug
    
    * fixed bug
    
    * add note for ResourceUsageMonitor
    
    * deal with comments
    
    * add macros
    
    * deal with comments
    
    * deal with comments
    
    * deal with comments
    
    * re-lint
    
    * rm pmap and use mem api
    
    * rm pmap and use mem api
    
    * add mem api
    
    * Add PrintBenchmarkInfo func
    
    * Add PrintBenchmarkInfo func
    
    * Add PrintBenchmarkInfo func
    
    * deal with comments
    
    * fixed enable_paddle_to_trt
    
    * add log for paddle_trt
    
    * support ppcls benchmark
    
    * use new trt option api
    
    * update benchmark info
    
    * simplify benchmark.cc
    
    * simplify benchmark.cc
    
    * deal with comments
    
    * Add ppseg && ppocr benchmark
    
    * add OCR rec img
    
    * add ocr benchmark
    
    * fixed trt shape
    
    * add trt shape
    
    * resolve conflict
    
    * add ENABLE_BENCHMARK define
    
    * Add ClassifyDiff
    
    * Add Resize for ClassifyResult
    
    * deal with comments
    
    * add convert info script
    
    * resolve conflict
    
    * Add SaveBenchmarkResult func
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * add config.txt for option
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * add benchmark.sh
    
    * mv thread_nums from 8 to 1
    
    * deal with comments
    
    * deal with comments
    
    * fixed readme
    
    * deal with comments
    
    ---------
    
    Co-authored-by: DefTruth <31974251+DefTruth@users.noreply.github.com>

--- benchmark/cpp/flags.h
+++ benchmark/cpp/flags.h
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <unordered_map>
 #include "gflags/gflags.h"
 #include "fastdeploy/benchmark/utils.h"
 
@@ -25,39 +26,19 @@ static const char sep = '/';
 
 DEFINE_string(model, "", "Directory of the inference model.");
 DEFINE_string(image, "", "Path of the image file.");
-DEFINE_string(device, "cpu",
-              "Type of inference device, support 'cpu/gpu/xpu'.");
-DEFINE_int32(device_id, 0, "device(gpu/xpu/...) id.");
-DEFINE_int32(warmup, 200, "Number of warmup for profiling.");
-DEFINE_int32(repeat, 1000, "Number of repeats for profiling.");
-DEFINE_string(profile_mode, "runtime", "runtime or end2end.");
-DEFINE_string(backend, "default",
-              "The inference runtime backend, support: ['default', 'ort', "
-              "'paddle', 'ov', 'trt', 'paddle_trt', 'lite']");
-DEFINE_int32(cpu_thread_nums, 8, "Set numbers of cpu thread.");
-DEFINE_bool(
-    include_h2d_d2h, false, "Whether run profiling with h2d and d2h.");
-DEFINE_bool(
-    use_fp16, false,
-    "Whether to use FP16 mode, only support 'trt', 'paddle_trt' "
-    "and 'lite' backend");
-DEFINE_bool(
-    collect_memory_info, false, "Whether to collect memory info");
-DEFINE_int32(sampling_interval, 50, "How often to collect memory info(ms).");
-DEFINE_string(result_path, "benchmark.txt", "Path of benchmark result file.");
+DEFINE_string(config_path, "config.txt", "Path of benchmark config.");
 
 static void PrintUsage() {
-  std::cout << "Usage: infer_demo --model model_path --image img_path --device "
-               "[cpu|gpu|xpu] --backend "
-               "[default|ort|paddle|ov|trt|paddle_trt|lite] "
-               "--use_fp16 false"
+  std::cout << "Usage: infer_demo --model model_path --image img_path "
+               "--config_path config.txt[Path of benchmark config.] "
             << std::endl;
   std::cout << "Default value of device: cpu" << std::endl;
   std::cout << "Default value of backend: default" << std::endl;
   std::cout << "Default value of use_fp16: false" << std::endl;
 }
 
-static void PrintBenchmarkInfo() {
+static void PrintBenchmarkInfo(std::unordered_map<std::string,
+                               std::string> config_info) {
 #if defined(ENABLE_BENCHMARK) && defined(ENABLE_VISION)
   // Get model name
   std::vector<std::string> model_names;
@@ -71,31 +52,32 @@ static void PrintBenchmarkInfo() {
   ss.precision(3);
   ss << "\n======= Model Info =======\n";
   ss << "model_name: " << model_names[model_names.size() - 1] << std::endl;
-  ss << "profile_mode: " << FLAGS_profile_mode << std::endl;
-  if (FLAGS_profile_mode == "runtime") {
-    ss << "include_h2d_d2h: " << FLAGS_include_h2d_d2h << std::endl;
+  ss << "profile_mode: " << config_info["profile_mode"] << std::endl;
+  if (config_info["profile_mode"] == "runtime") {
+    ss << "include_h2d_d2h: " << config_info["include_h2d_d2h"] << std::endl;
   }
   ss << "\n======= Backend Info =======\n";
-  ss << "warmup: " << FLAGS_warmup << std::endl;
-  ss << "repeats: " << FLAGS_repeat << std::endl;
-  ss << "device: " << FLAGS_device << std::endl;
-  if (FLAGS_device == "gpu") {
-    ss << "device_id: " << FLAGS_device_id << std::endl;
+  ss << "warmup: " << config_info["warmup"] << std::endl;
+  ss << "repeats: " << config_info["repeat"] << std::endl;
+  ss << "device: " << config_info["device"] << std::endl;
+  if (config_info["device"] == "gpu") {
+    ss << "device_id: " << config_info["device_id"] << std::endl;
+    ss << "use_fp16: " << config_info["use_fp16"] << std::endl;
   }
-  ss << "backend: " << FLAGS_backend << std::endl;
-  if (FLAGS_device == "cpu") {
-    ss << "cpu_thread_nums: " << FLAGS_cpu_thread_nums << std::endl;
+  ss << "backend: " << config_info["backend"] << std::endl;
+  if (config_info["device"] == "cpu") {
+    ss << "cpu_thread_nums: " << config_info["cpu_thread_nums"] << std::endl;
   }
-  ss << "use_fp16: " << FLAGS_use_fp16 << std::endl;
-  ss << "collect_memory_info: " << FLAGS_collect_memory_info << std::endl;
-  if (FLAGS_collect_memory_info) {
-    ss << "sampling_interval: " << std::to_string(FLAGS_sampling_interval)
+  ss << "collect_memory_info: "
+     << config_info["collect_memory_info"] << std::endl;
+  if (config_info["collect_memory_info"] == "true") {
+    ss << "sampling_interval: " << config_info["sampling_interval"]
        << "ms" << std::endl;
   }
   std::cout << ss.str() << std::endl;
   // Save benchmark info
   fastdeploy::benchmark::ResultManager::SaveBenchmarkResult(ss.str(),
-                                                  FLAGS_result_path);
+                                        config_info["result_path"]);
 #endif
   return;
 }
