f42332ec9b81e8b025448339274e67bee5a82a2e
WJJ1995 <wjjisloser@163.com>
2023-03-06 15:32:55 +0800

    [Benchmark] Add x86/Arm/XPU/GPU benchmark scripts (#1522)
    * add GPL lisence
    
    * add GPL-3.0 lisence
    
    * add GPL-3.0 lisence
    
    * add GPL-3.0 lisence
    
    * support yolov8
    
    * add pybind for yolov8
    
    * add yolov8 readme
    
    * add cpp benchmark
    
    * add cpu and gpu mem
    
    * public part split
    
    * add runtime mode
    
    * fixed bugs
    
    * add cpu_thread_nums
    
    * deal with comments
    
    * deal with comments
    
    * deal with comments
    
    * rm useless code
    
    * add FASTDEPLOY_DECL
    
    * add FASTDEPLOY_DECL
    
    * fixed for windows
    
    * mv rss to pss
    
    * mv rss to pss
    
    * Update utils.cc
    
    * use thread to collect mem
    
    * Add ResourceUsageMonitor
    
    * rm useless code
    
    * fixed bug
    
    * fixed typo
    
    * update ResourceUsageMonitor
    
    * fixed bug
    
    * fixed bug
    
    * add note for ResourceUsageMonitor
    
    * deal with comments
    
    * add macros
    
    * deal with comments
    
    * deal with comments
    
    * deal with comments
    
    * re-lint
    
    * rm pmap and use mem api
    
    * rm pmap and use mem api
    
    * add mem api
    
    * Add PrintBenchmarkInfo func
    
    * Add PrintBenchmarkInfo func
    
    * Add PrintBenchmarkInfo func
    
    * deal with comments
    
    * fixed enable_paddle_to_trt
    
    * add log for paddle_trt
    
    * support ppcls benchmark
    
    * use new trt option api
    
    * update benchmark info
    
    * simplify benchmark.cc
    
    * simplify benchmark.cc
    
    * deal with comments
    
    * Add ppseg && ppocr benchmark
    
    * add OCR rec img
    
    * add ocr benchmark
    
    * fixed trt shape
    
    * add trt shape
    
    * resolve conflict
    
    * add ENABLE_BENCHMARK define
    
    * Add ClassifyDiff
    
    * Add Resize for ClassifyResult
    
    * deal with comments
    
    * add convert info script
    
    * resolve conflict
    
    * Add SaveBenchmarkResult func
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * add config.txt for option
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * add benchmark.sh
    
    * mv thread_nums from 8 to 1
    
    * deal with comments
    
    * deal with comments
    
    * fixed readme
    
    * deal with comments
    
    * add all platform shell
    
    * Update config.arm.txt
    
    * Update config.gpu.txt
    
    * Update config.x86.txt
    
    ---------
    
    Co-authored-by: DefTruth <31974251+DefTruth@users.noreply.github.com>

--- benchmark/cpp/benchmark.sh
+++
@@ -1,9 +0,0 @@
-# Run all models specify hardware and specify backend
-./benchmark_ppseg --model PP_HumanSegV2_Lite_192x192_with_argmax_infer --image portrait_heng.jpg --config_path config.txt
-./benchmark_ppseg --model PP_HumanSegV2_Mobile_192x192_with_argmax_infer --image portrait_heng.jpg --config_path config.txt
-./benchmark_ppcls --model MobileNetV2_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.txt
-./benchmark_ppocr_det --model ch_PP-OCRv3_det_infer --image 12.jpg --config_path config.txt
-./benchmark_ppocr_cls --model ch_ppocr_mobile_v2.0_cls_infer --image rec_img.jpg --config_path config.txt
-./benchmark_ppocr_rec --model ch_PP-OCRv3_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path config.txt
-./benchmark_ppyolov8 --model yolov8_s_500e_coco --image 000000014439.jpg --config_path config.txt
-./benchmark_yolov5 --model yolov5s.onnx --image 000000014439.jpg --config_path config.txt
---
+++ benchmark/cpp/benchmark_x86.sh
@@ -0,0 +1,28 @@
+# Run all models specify hardware and specify backend
+
+# PaddleDetection
+./benchmark_ppyolov8 --model yolov8_s_500e_coco_no_nms --image 000000014439.jpg --config_path config.x86.txt --no_nms
+
+# PaddleSeg
+./benchmark_ppseg --model Portrait_PP_HumanSegV2_Lite_256x144_with_argmax_infer --image portrait_heng.jpg --config_path config.x86.txt
+./benchmark_ppseg --model PP_HumanSegV2_Lite_192x192_with_argmax_infer --image portrait_heng.jpg --config_path config.x86.txt
+./benchmark_ppseg --model PP_HumanSegV1_Lite_infer --image portrait_heng.jpg --config_path config.x86.txt
+./benchmark_ppseg --model Deeplabv3_ResNet101_OS8_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path config.x86.txt
+./benchmark_ppseg --model PP_LiteSeg_B_STDC2_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path config.x86.txt
+./benchmark_ppseg --model SegFormer_B0-cityscapes-with-argmax --image cityscapes_demo.png --config_path config.x86.txt
+
+# PaddleClas
+./benchmark_ppcls --model PPLCNet_x1_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model PPLCNetV2_base_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model MobileNetV1_x0_25_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model MobileNetV1_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model MobileNetV3_large_x1_0_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model ShuffleNetV2_x2_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model ResNet50_vd_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model EfficientNetB0_small_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+./benchmark_ppcls --model PPHGNet_tiny_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path config.x86.txt
+
+# PaddleOCR
+./benchmark_ppocr_det --model ch_PP-OCRv3_det_infer --image 12.jpg --config_path config.x86.txt
+./benchmark_ppocr_cls --model ch_ppocr_mobile_v2.0_cls_infer --image rec_img.jpg --config_path config.x86.txt
+./benchmark_ppocr_rec --model ch_PP-OCRv3_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path config.x86.txt
