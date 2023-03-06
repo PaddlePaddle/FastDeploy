d0cb7a7731d266079e190264dad38ac5c2cacbd2
root <guxukai365@gmail.com>
2023-03-06 08:57:09 +0000

    remove Apply() bind and fix

--- fastdeploy/vision/common/processors/manager_pybind.cc
+++ fastdeploy/vision/common/processors/manager_pybind.cc
@@ -52,6 +52,6 @@ void BindProcessorManager(pybind11::module& m) {
       .def("post_apply", &vision::ProcessorManager::PostApply)
       .def("use_cuda",
            [](vision::ProcessorManager& self, bool enable_cv_cuda = false,
-              int gpu_id = -1) { self.UseCuda(enable_cv_cuda, gpu_id); })
+              int gpu_id = -1) { self.UseCuda(enable_cv_cuda, gpu_id); });
 }
 }  // namespace fastdeploy
