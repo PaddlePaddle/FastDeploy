a83edf8a0285c7b81c6d8038491fa63adc4884b9
DefTruth <qiustudent_r@163.com>
2023-03-10 15:17:33 +0800

    [Benchmark] Update get_models.sh

--- benchmark/cpp/get_models.sh
+++ benchmark/cpp/get_models.sh
@@ -283,13 +283,13 @@ download_common_model_xvf https://paddleocr.bj.bcebos.com/PP-OCRv2/chinese/ch_PP
 
 # download images
 download_common_file https://bj.bcebos.com/paddlehub/fastdeploy/rec_img.jpg rec_img.jpg
-download_common_file https://paddleseg.bj.bcebos.com/dygraph/demo/cityscapes_demo.png cityscapes_demo.png
-download_common_file https://bj.bcebos.com/fastdeploy/test/portrait_heng.jpg portrait_heng.jpg
+download_common_file https://bj.bcebos.com/paddlehub/fastdeploy/cityscapes_demo.png cityscapes_demo.png
+download_common_file https://bj.bcebos.com/paddlehub/fastdeploy/portrait_heng.jpg portrait_heng.jpg
 download_common_file https://bj.bcebos.com/paddlehub/fastdeploy/matting_input.jpg matting_input.jpg
 download_common_file https://github.com/paddlepaddle/PaddleOCR/raw/release/2.6/doc/imgs/12.jpg 12.jpg
 download_common_file https://github.com/paddlepaddle/PaddleClas/raw/release/2.4/deploy/images/ImageNet/ILSVRC2012_val_00000010.jpeg ILSVRC2012_val_00000010.jpeg
 download_common_file https://github.com/paddlepaddle/PaddleDetection/raw/release/2.4/demo/000000014439.jpg 000000014439.jpg
-download_common_file https://gitee.com/paddlepaddle/PaddleOCR/raw/release/2.6/ppocr/utils/ppocr_keys_v1.txt ppocr_keys_v1.txt
+download_common_file https://github.com/paddlepaddle/PaddleOCR/raw/release/2.6/ppocr/utils/ppocr_keys_v1.txt ppocr_keys_v1.txt
 
 # covert models -> onnx/mnn/tnn/ncnn
 if [ "$CONVERT_FLAG" = "convert" ]; then
