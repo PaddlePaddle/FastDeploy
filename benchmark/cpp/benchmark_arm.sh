5acb41e8e1ce669615dbef408c81136facf5f1d8
DefTruth <qiustudent_r@163.com>
2023-03-12 21:02:24 +0800

    [Benchmark] Optimize benchmark_arm.sh

--- benchmark/cpp/benchmark_arm.sh
+++ benchmark/cpp/benchmark_arm.sh
@@ -1,99 +1,60 @@
 # Run all models specify hardware and specify backend
-sleep_interval=10
-sleep_seconds() {
-  echo "[INFO][SLEEP] --- sleep $sleep_interval seconds for Arm CPU mobile ..."
-  sleep $sleep_interval
-}
-config_path="config.arm.txt"
-profile_tag="partial"
+export LD_LIBRARY_PATH=${PWD}:$LD_LIBRARY_PATH
+
+CONFIG_PATH="config.arm.txt"
 if [ ! "$1" = "config.arm.txt" ]; then
   if [ -f "$1" ]; then
-    config_path="$1"
+    CONFIG_PATH="$1"
   fi
 fi
-if [ "$2" = "all" ]; then
-  profile_tag="all"
-fi
-
-benchmark_arm_partial() {
-  # PaddleDetection: +1
-  ./benchmark_ppyolov8 --model yolov8_s_500e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-
-  # PaddleClas: +9
-  ./benchmark_ppcls --model PPLCNet_x1_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model PPLCNetV2_base_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV1_x0_25_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV1_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV3_large_x1_0_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model ShuffleNetV2_x2_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model ResNet50_vd_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model EfficientNetB0_small_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model PPHGNet_tiny_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-
-  # PaddleOCR: +3
-  ./benchmark_ppocr_det --model ch_PP-OCRv3_det_infer --image 12.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_cls --model ch_ppocr_mobile_v2.0_cls_infer --image rec_img.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_rec --model ch_PP-OCRv3_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path $config_path && sleep_seconds
-
-  # PaddleSeg: +9
-  ./benchmark_ppseg --model PP_LiteSeg_B_STDC2_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model Portrait_PP_HumanSegV2_Lite_256x144_with_argmax_infer --image portrait_heng.jpg --config_path $config_path  && sleep_seconds
-  ./benchmark_ppseg --model PP_HumanSegV2_Lite_192x192_with_argmax_infer --image portrait_heng.jpg --config_path $config_path  && sleep_seconds
-  ./benchmark_ppseg --model PP_HumanSegV1_Lite_infer --image portrait_heng.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model Deeplabv3_ResNet101_OS8_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model SegFormer_B0-cityscapes-with-argmax --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-
-  # num: 19
+DEFAULT_INTERLVAL=180
+sleep_seconds() {
+  sleep_interval=$DEFAULT_INTERLVAL
+  if [ ! "$1" = "" ]; then
+    sleep_interval=$1
+  fi
+  echo "[INFO][SLEEP] --- Sleep $sleep_interval seconds for Arm CPU mobile to prevent the phone from overheating ..."
+  sleep $sleep_interval
 }
 
-benchmark_arm_all() {
-  # PaddleDetection: +8
-  ./benchmark_ppyolov5 --model yolov5_s_300e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyolov6 --model yolov6_s_300e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyolov7 --model yolov7_l_300e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyolov8 --model yolov8_s_500e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyolox --model yolox_s_300e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyoloe --model ppyoloe_crn_l_300e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_ppyoloe --model ppyoloe_plus_crn_m_80e_coco_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
-  ./benchmark_picodet --model picodet_l_640_coco_lcnet_no_nms --image 000000014439.jpg --config_path $config_path --no_nms && sleep_seconds
+# PaddleDetection: +8
+./benchmark_ppyolov5 --model yolov5_s_300e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyolov6 --model yolov6_s_300e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyolov7 --model yolov7_l_300e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyolov8 --model yolov8_s_500e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyolox --model yolox_s_300e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyoloe --model ppyoloe_crn_l_300e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_ppyoloe --model ppyoloe_plus_crn_m_80e_coco_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
+./benchmark_picodet --model picodet_l_640_coco_lcnet_no_nms --image 000000014439.jpg --config_path $CONFIG_PATH --no_nms && sleep_seconds
 
-  # PaddleClas: +9
-  ./benchmark_ppcls --model PPLCNet_x1_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model PPLCNetV2_base_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV1_x0_25_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV1_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model MobileNetV3_large_x1_0_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model ShuffleNetV2_x2_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model ResNet50_vd_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model EfficientNetB0_small_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
-  ./benchmark_ppcls --model PPHGNet_tiny_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $config_path && sleep_seconds
+# PaddleClas: +9
+./benchmark_ppcls --model PPLCNet_x1_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model PPLCNetV2_base_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model MobileNetV1_x0_25_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model MobileNetV1_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model MobileNetV3_large_x1_0_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model ShuffleNetV2_x2_0_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model ResNet50_vd_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model EfficientNetB0_small_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppcls --model PPHGNet_tiny_ssld_infer --image ILSVRC2012_val_00000010.jpeg --config_path $CONFIG_PATH && sleep_seconds
 
-  # PaddleOCR: +5
-  ./benchmark_ppocr_det --model ch_PP-OCRv3_det_infer --image 12.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_cls --model ch_ppocr_mobile_v2.0_cls_infer --image rec_img.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_rec --model ch_PP-OCRv3_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_det --model ch_PP-OCRv2_det_infer --image 12.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppocr_rec --model ch_PP-OCRv2_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path $config_path && sleep_seconds
+# PaddleOCR: +5
+./benchmark_ppocr_det --model ch_PP-OCRv3_det_infer --image 12.jpg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppocr_cls --model ch_ppocr_mobile_v2.0_cls_infer --image rec_img.jpg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppocr_rec --model ch_PP-OCRv3_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppocr_det --model ch_PP-OCRv2_det_infer --image 12.jpg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppocr_rec --model ch_PP-OCRv2_rec_infer --image rec_img.jpg --rec_label_file ppocr_keys_v1.txt --config_path $CONFIG_PATH && sleep_seconds
 
-  # PaddleSeg: +9
-  ./benchmark_ppseg --model PP_LiteSeg_B_STDC2_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model Portrait_PP_HumanSegV2_Lite_256x144_with_argmax_infer --image portrait_heng.jpg --config_path $config_path  && sleep_seconds
-  ./benchmark_ppseg --model PP_HumanSegV2_Lite_192x192_with_argmax_infer --image portrait_heng.jpg --config_path $config_path  && sleep_seconds
-  ./benchmark_ppseg --model PP_HumanSegV1_Lite_infer --image portrait_heng.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model Deeplabv3_ResNet101_OS8_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-  ./benchmark_ppseg --model SegFormer_B0-cityscapes-with-argmax --image cityscapes_demo.png --config_path $config_path && sleep_seconds
-  ./benchmark_ppmatting --model PP-Matting-512 --image matting_input.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppmatting --model PPHumanMatting --image matting_input.jpg --config_path $config_path && sleep_seconds
-  ./benchmark_ppmatting --model PPModnet_MobileNetV2 --image matting_input.jpg --config_path $config_path && sleep_seconds
-
-  # num: 31
-}
-
-if [ "$profile_tag" = "all" ]; then
-  benchmark_arm_all
-else
-  benchmark_arm_partial
-fi
+# PaddleSeg: +9
+./benchmark_ppseg --model PP_LiteSeg_B_STDC2_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppseg --model Portrait_PP_HumanSegV2_Lite_256x144_with_argmax_infer --image portrait_heng.jpg --config_path $CONFIG_PATH  && sleep_seconds
+./benchmark_ppseg --model PP_HumanSegV2_Lite_192x192_with_argmax_infer --image portrait_heng.jpg --config_path $CONFIG_PATH  && sleep_seconds
+./benchmark_ppseg --model PP_HumanSegV1_Lite_infer --image portrait_heng.jpg --config_path $CONFIG_PATH && sleep_seconds
+./benchmark_ppseg --model Deeplabv3_ResNet101_OS8_cityscapes_with_argmax_infer --image cityscapes_demo.png --config_path $CONFIG_PATH && sleep_seconds 240
+./benchmark_ppseg --model SegFormer_B0-cityscapes-with-argmax --image cityscapes_demo.png --config_path $CONFIG_PATH && sleep_seconds 240
+./benchmark_ppmatting --model PP-Matting-512 --image matting_input.jpg --config_path $CONFIG_PATH && sleep_seconds 240
+./benchmark_ppmatting --model PPHumanMatting --image matting_input.jpg --config_path $CONFIG_PATH && sleep_seconds 240
+./benchmark_ppmatting --model PPModnet_MobileNetV2 --image matting_input.jpg --config_path $CONFIG_PATH && sleep_seconds 240
 
 # usage:
 # ./benchmark_arm.sh
@@ -101,7 +62,7 @@ fi
 # ./benchmark_arm.sh config.arm.mnn.txt
 # ./benchmark_arm.sh config.arm.tnn.txt
 # ./benchmark_arm.sh config.arm.ncnn.txt
-# ./benchmark_arm.sh config.arm.lite.txt all
-# ./benchmark_arm.sh config.arm.mnn.txt all
-# ./benchmark_arm.sh config.arm.tnn.txt all
-# ./benchmark_arm.sh config.arm.ncnn.txt all
+# ./benchmark_arm.sh config.arm.lite.fp16.txt
+# ./benchmark_arm.sh config.arm.mnn.fp16.txt
+# ./benchmark_arm.sh config.arm.tnn.fp16.txt
+# ./benchmark_arm.sh config.arm.ncnn.fp16.txt
