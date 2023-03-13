8b69aa3628229cc5541424ee0210a4657ffba1e1
root <guxukai365@gmail.com>
2023-03-13 08:15:30 +0000

    add comments

--- python/fastdeploy/vision/common/processors.py
+++ python/fastdeploy/vision/common/processors.py
@@ -70,11 +70,19 @@ class NormalizeAndPermute(Processor):
 class Cast(Processor):
     def __init__(self, dtype="float"):
         self.processor = C.vision.processors.Cast(dtype)
+        """Creat a new cast opereaton with given dtype
+
+        :param dtype dtype of the output
+        """
 
 
 class HWC2CHW(Processor):
     def __init__(self):
         self.processor = C.vision.processors.HWC2CHW()
+        """Creat a new hwc2chw processor with default dtype.
+
+        :return An instance of processor `HWC2CHW`
+        """
 
 
 class Normalize(Processor):
@@ -87,11 +95,25 @@ class Normalize(Processor):
                  swap_rb=False):
         self.processor = C.vision.processors.Normalize(mean, std, is_scale,
                                                        min, max, swap_rb)
+        """Creat a new normalize opereator with given paremeters.
+
+        :param mean     A list containing the mean of each channel
+        :param std      A list containing the standard deviation of each channel
+        :param is_scale Specifies if the image are being scaled or not
+        :param min      A list containing the minimum value of each channel
+        :param max      A list containing the maximum value of each channel
+        """
 
 
 class PadToSize(Processor):
     def __init__(self, width, height, value=[]):
         self.processor = C.vision.processors.PadToSize(width, height, value)
+        """Create a new PadToSize opereator with given parameters.
+
+        :param width     Desired width of the output image
+        :param height    Desired height of the output image
+        :param value     values to pad with
+        """
 
 
 class Resize(Processor):
@@ -104,8 +126,22 @@ class Resize(Processor):
                  use_scale=False):
         self.processor = C.vision.processors.Resize(width, height, scale_w,
                                                     scale_h, interp, use_scale)
+        """Create a Resize operation with the given parameters.
+
+        :param width   Desired width of the output image
+        :param height  Desired height of the output image
+        :param scale_w Scales the width in x-direction
+        :param scale_h Scales the height in y-direction
+        :param interp: optionally, the interpolation mode for resizing image
+        :param use_scale: optionally, whether to scale image
+        """
 
 
 class StridePad(Processor):
     def __init__(self, stride, value=[]):
         self.processor = C.vision.processors.StridePad(stride, value)
+        """Create a StridePad processor with given parameters.
+
+        :param stride Stride of the processor
+        :param value  values to pad with
+        """
