16a4ef29564f77a759b1921805b4951c36046374
yeliang2258 <1047690002@qq.com>
2023-03-15 06:18:11 +0000

    add mobilenetv1 test

--- tests/timvx/run_test.sh
+++ tests/timvx/run_test.sh
@@ -1,6 +1,7 @@
 export LD_LIBRARY_PATH=${PWD}/lib
 
-./test_clas models/resnet50_vd_ptq/ images/ILSVRC2012_val_00000010.jpeg results/resnet50_clas.txt
+./test_clas models/mobilenetv1_ssld_ptq images/ILSVRC2012_val_00000010.jpeg results/mobilenetv1_cls.txt
+./test_clas models/resnet50_vd_ptq/ images/ILSVRC2012_val_00000010.jpeg results/resnet50_cls.txt
 ./test_yolov5 models/yolov5s_ptq_model/ images/000000014439.jpg results/yolov5_result.txt
 ./test_ppyoloe models/ppyoloe_noshare_qat/ images/000000014439.jpg results/ppyoloe_result.txt
 ./test_ppliteseg models/ppliteseg images/cityscapes_demo.png results/ppliteseg_result.txt
