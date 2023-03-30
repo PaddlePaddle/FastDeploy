42151576aa4d03546ef875e5c982e565e33a6d78
wjj19950828 <wjjisloser@163.com>
2023-03-30 03:31:22 +0000

    test

--- benchmark/cpp/run_gpu.sh
+++ benchmark/cpp/run_gpu.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
-bash benchmark_gpu.sh config/config.gpu.ort.fp32.e2e.mem.txt
-bash benchmark_gpu.sh config/config.gpu.ort.fp32.e2e.txt
-bash benchmark_gpu.sh config/config.gpu.ort.fp32.txt
+# bash benchmark_gpu.sh config/config.gpu.ort.fp32.e2e.mem.txt
+# bash benchmark_gpu.sh config/config.gpu.ort.fp32.e2e.txt
+# bash benchmark_gpu.sh config/config.gpu.ort.fp32.txt
 
-bash benchmark_gpu.sh config/config.gpu.paddle.fp32.e2e.mem.txt
-bash benchmark_gpu.sh config/config.gpu.paddle.fp32.e2e.txt
-bash benchmark_gpu.sh config/config.gpu.paddle.fp32.txt
+# bash benchmark_gpu.sh config/config.gpu.paddle.fp32.e2e.mem.txt
+# bash benchmark_gpu.sh config/config.gpu.paddle.fp32.e2e.txt
+# bash benchmark_gpu.sh config/config.gpu.paddle.fp32.txt
+# rm all paddle_trt/trt cache
+# find . -name "trt_serialized*" | xargs rm -rf
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.e2e.mem.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.e2e.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.h2d.txt
 # rm all paddle_trt/trt cache
 find . -name "trt_serialized*" | xargs rm -rf
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.e2e.mem.txt
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.e2e.txt
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.txt
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp16.h2d.txt
-# rm all paddle_trt/trt cache
-find . -name "trt_serialized*" | xargs rm -rf
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.e2e.mem.txt
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.e2e.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.e2e.mem.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.e2e.txt
 bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.txt
-bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.h2d.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.paddle_trt.fp32.h2d.txt
 # rm all paddle_trt/trt cache
-find . -name "trt_serialized*" | xargs rm -rf
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.e2e.mem.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.e2e.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.h2d.txt
+# find . -name "trt_serialized*" | xargs rm -rf
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.e2e.mem.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.e2e.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp16.h2d.txt
 # rm all paddle_trt/trt cache
-find . -name "trt_serialized*" | xargs rm -rf
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.e2e.mem.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.e2e.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.txt
-bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.h2d.txt
+# find . -name "trt_serialized*" | xargs rm -rf
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.e2e.mem.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.e2e.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.txt
+# bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.h2d.txt
 # rm all paddle_trt/trt cache
 find . -name "trt_serialized*" | xargs rm -rf
