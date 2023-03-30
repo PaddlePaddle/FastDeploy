f4a700873298ae099d6ae750d2c9f3a15ca2a606
CoolCola <49013063+CoolKbh@users.noreply.github.com>
2023-03-30 14:22:46 +0800

    [Streamer] Build python wheel and python API (#1702)
    * add setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * add scripts
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup.py
    
    * add pybind interface
    
    * update CMakeLists
    
    * fix CMakeLists
    
    * fix setup.py
    
    * fix setup.py
    
    * fix setup
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix pybind
    
    * fix pybind
    
    * fix pybind
    
    * fix pybind
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists.txt
    
    * fix CMakeLists.txt
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix CMakeLists
    
    * fix process_libraries
    
    * test setup
    
    * fix setup
    
    * fix py wheel build
    
    * add ppyoloe py streamer
    
    * delete annotation, fix form
    
    ---------
    
    Co-authored-by: Wang Xinyu <shaywxy@gmail.com>

--- streamer/python/streamer/c_lib_wrap.py
+++ streamer/python/streamer/c_lib_wrap.py
@@ -4,15 +4,21 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import absolute_import
+import logging
+import os
+import sys
 
-file(GLOB_RECURSE ALL_SRCS ${PROJECT_SOURCE_DIR}/src/gstreamer/plugin/fdtracker/*.cc ${PROJECT_SOURCE_DIR}/src/gstreamer/plugin/fdtracker/*.cpp)
 
-add_library(gstfdtracker SHARED ${ALL_SRCS} )
-target_link_libraries(gstfdtracker ${DEPEND_LIBS} ${FASTDEPLOY_LIBS})
+try:
+    from .libs.streamer_main import *
+except:
+    raise RuntimeError("FastDeploy Streamer initalized failed!")
+
