d52192616eb378da107914d0668212379e6430b2
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-04-13 23:49:27 +0800

    update for tvm backend

--- examples/vision/detection/paddledetection/tvm/cpp/convert_model.py
+++ examples/vision/detection/paddledetection/tvm/cpp/convert_model.py
@@ -3,6 +3,21 @@ import tvm
 from tvm import relay
 from tvm.contrib import graph_executor
 import os
+import argparse
+
+
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path", default="./picodet_l_320_coco_lcnet/model")
+    parser.add_argument(
+        "--shape_dict",
+        default={"image": [1, 3, 320, 320],
+                 "scale_factor": [1, 2]})
+    parser.add_argument("--tvm_save_name", default="tvm_model")
+    parser.add_argument("--tvm_save_path", default="./tvm_save")
+    args = parser.parse_args()
+    return args
 
 
 def read_model(model_path):
@@ -13,6 +28,8 @@ def paddle_to_tvm(paddle_model,
                   shape_dict,
                   tvm_save_name="tvm_model",
                   tvm_save_path="./tvm_save"):
+    if isinstance(shape_dict, str):
+        shape_dict = eval(shape_dict)
     mod, params = relay.frontend.from_paddle(paddle_model, shape_dict)
     # 这里首先在PC的CPU上进行测试 所以使用LLVM进行导出
     target = tvm.target.Target("llvm", host="llvm")
@@ -30,10 +47,12 @@ def paddle_to_tvm(paddle_model,
             fo.write(relay.save_param_dict(base_lib.get_params()))
         module = graph_executor.GraphModule(base_lib['default'](dev))
         module.load_params(relay.save_param_dict(base_lib.get_params()))
-        print(module.get_input_info()[0])
+        print("export success")
 
 
 if __name__ == "__main__":
-    paddle_model = read_model("./picodet_l_320_coco_lcnet/model")
-    shape_dict = {"image": [1, 3, 320, 320], "scale_factor": [1, 2]}
-    paddle_to_tvm(paddle_model, shape_dict)
+    config = get_config()
+    paddle_model = read_model(config.model_path)
+    shape_dict = config.shape_dict
+    paddle_to_tvm(paddle_model, shape_dict, config.tvm_save_name,
+                  config.tvm_save_path)
