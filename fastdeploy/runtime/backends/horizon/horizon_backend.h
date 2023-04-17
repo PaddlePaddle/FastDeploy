308e26bee9e4098e2c124a03f482fd2831c27050
seyosum <xu_hao_98@163.com>
2023-04-17 15:51:36 +0800

    更改horizon头文件编码规范

--- fastdeploy/runtime/backends/horizon/horizon_backend.h
+++ fastdeploy/runtime/backends/horizon/horizon_backend.h
@@ -65,5 +65,5 @@ class HorizonBackend : public BaseBackend {
     static FDDataType HorizonTensorTypeToFDDataType(int32_t type);
     static hbDNNDataType FDDataTypeToHorizonTensorType(FDDataType type);
 };
-} // namespace fastdeploy
+}  // namespace fastdeploy
 
