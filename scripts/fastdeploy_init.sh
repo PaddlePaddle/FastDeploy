3cb9a9fc6bafddb438fc0dd85682eabf01cb128f
Zheng-Bicheng <58363586+Zheng-Bicheng@users.noreply.github.com>
2023-05-19 17:30:12 +0800

    [Shell] support fastdeploy_init.sh for MacOS (#1967)
    * 兼容zsh
    
    * 使用脚本所在的绝对路径, 确保与执行位置无关
    
    * * Fixed the naming error of the LIBS_DIRECOTRIES variable.
    * Fixed the error of using '=' for string comparison.
    
    * * Troubleshooting failed environment variable addition on macOS system
    
    ---------
    
    Co-authored-by: Edward Zhou <edwardzhou@EdwarddeMacBook-Pro-2.local>
    Co-authored-by: Edward Zhou <edward.zhq@gmail.com>
    Co-authored-by: DefTruth <31974251+DefTruth@users.noreply.github.com>

--- scripts/fastdeploy_init.sh
+++ scripts/fastdeploy_init.sh
@@ -2,7 +2,7 @@
 
 PLATFORM=`uname`
 FASTDEPLOY_LIBRARY_PATH=${BASH_SOURCE:-$0}
-if [[ "$PLATFORM" == "Linux" ]];then
+if [[ "$PLATFORM" = "Linux" ]];then
     FASTDEPLOY_LIBRARY_PATH=`readlink -f ${FASTDEPLOY_LIBRARY_PATH}`
 fi
 FASTDEPLOY_LIBRARY_PATH=$(cd `dirname ${FASTDEPLOY_LIBRARY_PATH}`; pwd)
@@ -22,23 +22,23 @@ else
 fi
 
 for SO_FILE in $ALL_SO_FILES;do
-    LIBS_DIRECOTRIES+=(${SO_FILE%/*})
+    LIBS_DIRECTORIES+=(${SO_FILE%/*})
 done
 
 # Find all the .dylib files' path
 # ALL_DYLIB_FILES=(`find $FASTDEPLOY_LIBRARY_PATH -name "*.dylib*"`)
 for DYLIB_FILE in $ALL_DYLIB_FILES;do
-    LIBS_DIRECOTRIES+=(${DYLIB_FILE%/*})
+    LIBS_DIRECTORIES+=(${DYLIB_FILE%/*})
 done
 
 # Remove the dumplicate directories
-LIBS_DIRECOTRIES=($(awk -v RS=' ' '!a[$1]++' <<< ${LIBS_DIRECOTRIES[@]}))
+LIBS_DIRECTORIES=($(awk -v RS=' ' '!a[$1]++' <<< ${LIBS_DIRECTORIES[@]}))
 
 # Print the dynamic library location and output the configuration file
 IMPORT_PATH=""
 output_file=${FASTDEPLOY_LIBRARY_PATH}/fastdeploy_libs.conf
 rm -rf $output_file
-for LIB_DIR in ${LIBS_DIRECOTRIES[@]};do
+for LIB_DIR in ${LIBS_DIRECTORIES[@]};do
     echo "Find Library Directory: $LIB_DIR"
     echo "$LIB_DIR" >> $output_file
     IMPORT_PATH=${LIB_DIR}":"$IMPORT_PATH
@@ -50,6 +50,12 @@ then
 fi
 
 echo "[Execute] Will try to export all the library directories to environments, if not work, please try to export these path by your self."
-NEW_LIB_PATH=$(tr ":" "\n" <<< "${IMPORT_PATH}:$LD_LIBRARY_PATH" | sort | uniq | tr "\n" ":")
-
-export LD_LIBRARY_PATH=$NEW_LIB_PATH
+PLATFORM=`uname`
+if [[ "$PLATFORM" = "Linux" ]];then
+  NEW_LIB_PATH=$(tr ":" "\n" <<< "${IMPORT_PATH}:$LD_LIBRARY_PATH" | sort | uniq | tr "\n" ":")
+  export LD_LIBRARY_PATH=$NEW_LIB_PATH
+fi
+if [[ "$PLATFORM" = "Darwin" ]];then
+  NEW_LIB_PATH=$(tr ":" "\n" <<< "${IMPORT_PATH}:$DYLD_LIBRARY_PATH" | sort | uniq | tr "\n" ":")
+  export DYLD_LIBRARY_PATH=$NEW_LIB_PATH
+fi
