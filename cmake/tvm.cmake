779e35c7988274c20d5615877d6d60fb122e8ecc
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-05-22 13:42:54 +0800

    update

--- cmake/tvm.cmake
+++ cmake/tvm.cmake
@@ -15,23 +15,31 @@ elseif (${CMAKE_SYSTEM} MATCHES "Linux")
 else ()
     error("TVM only support MacOS in Arm64 or linux in x86")
 endif ()
-set(TVM_FILE "tvm-${TVM_SYSTEM}-${TVM_VERSION}")
-set(TVM_URL "${TVM_URL_BASE}${TVM_FILE}.tgz")
+set(TVM_FILE "tvm-${TVM_SYSTEM}-${TVM_VERSION}.tgz")
+set(TVM_URL "${TVM_URL_BASE}${TVM_FILE}")
 
-
-download_and_decompress(${TVM_URL} ${CMAKE_CURRENT_BINARY_DIR}/tvm ${THIRD_PARTY_PATH}/install/)
 set(TVM_RUNTIME_PATH "${THIRD_PARTY_PATH}/install/tvm")
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory "${TVM_RUNTIME_PATH}")
+download_and_decompress(${TVM_URL}
+        "${CMAKE_CURRENT_BINARY_DIR}/${TVM_FILE}"
+        "${THIRD_PARTY_PATH}/install/")
+include_directories(${TVM_RUNTIME_PATH}/include)
 
 # copy dlpack to third_party
+set(DLPACK_PATH "${THIRD_PARTY_PATH}/install/dlpack")
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory "${DLPACK_PATH}")
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${PROJECT_SOURCE_DIR}/third_party/dlpack/include"
-        "${THIRD_PARTY_PATH}/install/tvm/include")
+        "${PROJECT_SOURCE_DIR}/third_party/dlpack"
+        "${THIRD_PARTY_PATH}/install/")
+include_directories(${DLPACK_PATH}/include)
 
-# copy dmlc-core to third_party
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${PROJECT_SOURCE_DIR}/third_party/dmlc-core/include"
-        "${THIRD_PARTY_PATH}/install/tvm/include")
+set(DMLC_CORE_PATH "${THIRD_PARTY_PATH}/install/dmlc-core")
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory "${DMLC_CORE_PATH}")
+set(DMLC_CORE_URL https://bj.bcebos.com/fastdeploy/third_libs/dmlc-core.tgz)
+download_and_decompress(${DMLC_CORE_URL}
+        "${CMAKE_CURRENT_BINARY_DIR}/dmlc-core.tgz"
+        "${THIRD_PARTY_PATH}/install/")
+include_directories(${DMLC_CORE_PATH}/include)
 
 # include lib
 if (EXISTS ${TVM_RUNTIME_PATH})
@@ -40,7 +48,6 @@ if (EXISTS ${TVM_RUNTIME_PATH})
     elseif (${CMAKE_SYSTEM} MATCHES "Linux")
         set(TVM_RUNTIME_LIB ${TVM_RUNTIME_PATH}/lib/libtvm_runtime.so)
     endif ()
-    include_directories(${TVM_RUNTIME_PATH}/include)
     include(${TVM_RUNTIME_PATH}/lib/cmake/tvm/tvmConfig.cmake)
     add_definitions(-DDMLC_USE_LOGGING_LIBRARY=<tvm/runtime/logging.h>)
 else ()
