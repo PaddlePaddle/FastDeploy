0ae34ce31533796b736fb2fcc78ba54b14f0f400
DefTruth <31974251+DefTruth@users.noreply.github.com>
2023-06-22 18:30:01 +0800

    [benchmark] update paddlex benchmark scripts (#2060)
    * [benchmark] fixed paddlex benchmark for picodet 320
    
    * [Bug Fix] fixed paddlex ppseg pp-trt infer error
    
    * [Bug Fix] fixed paddlex dino benchmark trt shapes
    
    * [benchmark] support paddlex ppyoloe pptrt benchmark
    
    * [benchmark] adjust paddlex dino trt shapes
    
    * [benchmark] add max_workspace_size flags for tensorrt/pptrt backend
    
    * [benchmark] add max_workspace_size flags for tensorrt/pptrt backend
    
    * [benchmark] add max_workspace_size flags for tensorrt/pptrt backend
    
    * [benchmark] add ort/paddle h2d gpu configs for paddlex
    
    * [benchmark] update paddlex benchmark scripts
    
    * [benchmark] update paddlex benchmark scripts
    
    ---------
    
    Co-authored-by: qiuyanjun <qiuyanjun@baidu.com>

--- benchmark/paddlex/run_gpu.sh
+++ benchmark/paddlex/run_gpu.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -x
+
 export CUDA_VISIBLE_DEVICES='0'
 bash benchmark_gpu.sh config/config.gpu.ort.fp32.e2e.txt
 bash benchmark_gpu.sh config/config.gpu.ort.fp32.txt
@@ -30,3 +32,5 @@ bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.txt
 bash benchmark_gpu_trt.sh config/config.gpu.trt.fp32.h2d.txt
 # rm all paddle_trt/trt cache
 find . -name "trt_serialized*" | xargs rm -rf
+
+set +x
--- benchmark/paddlex/run_x86.sh
+++ benchmark/paddlex/run_x86.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 bash benchmark_x86.sh config/config.x86.ort.fp32.e2e.txt
 bash benchmark_x86.sh config/config.x86.ort.fp32.txt
@@ -8,3 +9,5 @@ bash benchmark_x86.sh config/config.x86.ov.fp32.txt
 
 bash benchmark_x86.sh config/config.x86.paddle.fp32.e2e.txt
 bash benchmark_x86.sh config/config.x86.paddle.fp32.txt
+
+set +x
