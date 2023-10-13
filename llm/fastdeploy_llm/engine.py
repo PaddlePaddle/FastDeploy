eecd3fdb3b308cc5ddc6acf85010500fdda22c8c
chenjian <chenjian26@baidu.com>
2023-10-13 16:18:19 +0800

    support bloom prefix

--- llm/fastdeploy_llm/engine.py
+++ llm/fastdeploy_llm/engine.py
@@ -438,6 +438,10 @@ def dy_input_preprocess(inputs):
                         "input_ids"].shape[1]] = paddle.arange(inputs["input_ids"]
                                                             .shape[1])
                     if "bloom" in args.architecture:
+                        tgt_generation_mask[i, :, 0, :max_prefix_len +
+                                            length] = paddle.ones(
+                                                shape=[1, max_prefix_len + length],
+                                                dtype=args.dtype)
                         arange_tensor_encoder[i, :, :length + max_prefix_len] = paddle.arange(
                             length + max_prefix_len).astype(args.dtype)
                         inputs["tgt_pos"] = inputs["tgt_pos"] + max_prefix_len
