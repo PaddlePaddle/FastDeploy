7b657959cd93fd7a778917d6546eee00abdfbab3
chenjian <chenjian26@baidu.com>
2023-12-13 20:09:05 +0800

    Improve robust for llm service

--- llm_ic/main.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-from fastdeploy_ic.server.launcher import serve
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Inference load balance controller launcher")
-    parser.add_argument("--grpc-port", type=int, default=9000)
-    args = parser.parse_args()
-    serve(args)
