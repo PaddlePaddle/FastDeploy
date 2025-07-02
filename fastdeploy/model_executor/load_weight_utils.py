277248e20da97a76e3224bad5c4d3f076538f6fc
Jiang-Jia-Jun <jiangjiajun@baidu.com>
2025-07-02 17:27:16 +0800

    Add new code files

---
+++ fastdeploy/model_executor/load_weight_utils.py
@@ -0,0 +1,289 @@
+"""
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+import json
+import os
+
+import paddle
+import paddle.distributed as dist
+from fastsafetensors import SafeTensorsFileLoader, SingleGroup
+from paddleformers.transformers import PretrainedModel
+from paddleformers.transformers.model_utils import load_tp_checkpoint
+from safetensors import safe_open
+from tqdm import tqdm
+
+from fastdeploy.config import FDConfig, ModelConfig
+from fastdeploy.model_executor.models.tp_utils import \
+    check_tensor_parallel_prerequisites
+from fastdeploy.platforms import current_platform
+
+
+def load_ep_checkpoint(model_path: str,
+                       config: ModelConfig,
+                       return_numpy: bool = False):
+    """
+    load ep checkpoint
+    """
+    with open(os.path.join(model_path, "model.safetensors.index.json"),
+              "r") as f:
+        weight_list = json.load(f)["weight_map"]
+    filtered_map = {k: v for k, v in weight_list.items() if "experts" not in k}
+    num_local_ffn_keys = []
+
+    for i in range(config.moe_layer_start_index, config.num_layers):
+        for j in range(
+                config.num_experts_start_offset,
+                config.num_experts_start_offset + config.num_experts_per_rank,
+        ):
+            ffn1_key = f"ernie.layers.{i}.mlp.experts.{j}.up_gate_proj.weight"
+            ffn2_key = (f"ernie.layers.{i}.mlp.experts.{j}.down_proj.weight")
+
+            ffn1_quant_key = f"ernie.layers.{i}.mlp.experts.{j}.up_gate_proj.quant_weight"
+            ffn2_quant_key = (
+                f"ernie.layers.{i}.mlp.experts.{j}.down_proj.quant_weight")
+
+            ffn1_scale_key = f"ernie.layers.{i}.mlp.experts.{j}.up_gate_proj.weight_scale"
+            ffn2_scale_key = (
+                f"ernie.layers.{i}.mlp.experts.{j}.down_proj.weight_scale")
+            num_local_ffn_keys.append(ffn1_key)
+            num_local_ffn_keys.append(ffn2_key)
+            num_local_ffn_keys.append(ffn1_quant_key)
+            num_local_ffn_keys.append(ffn2_quant_key)
+            num_local_ffn_keys.append(ffn1_scale_key)
+            num_local_ffn_keys.append(ffn2_scale_key)
+
+    for k in num_local_ffn_keys:
+        if k in weight_list:
+            filtered_map[k] = weight_list[k]
+
+    state_dict = {}
+    # Get all safetensor file paths that need to be opened
+    safetensor_paths = set(filtered_map.values())
+
+    # Open each safetensor file sequentially with progress bar
+    for safetensor_path in tqdm(safetensor_paths,
+                                desc="Loading safetensor files",
+                                unit="file"):
+        with safe_open(os.path.join(model_path, safetensor_path),
+                       framework="np",
+                       device="cpu") as f:
+            # Check if this file contains keys from filtered_map
+            for k in filtered_map:
+                if filtered_map[k] == safetensor_path and k in f.keys():
+                    weight = f.get_tensor(k)
+                    if not return_numpy:
+                        weight = paddle.Tensor(weight, zero_copy=True)
+                        weight = weight._copy_to(
+                            paddle.framework._current_expected_place(), False)
+                    state_dict[k] = weight
+    return state_dict
+
+
+def safetensors_weights_iterator(safe_tensor_list: list[str], ):
+    """
+    safetensors_weights_iterator
+    """
+    for st_file in tqdm(
+            safe_tensor_list,
+            desc="Loading safetensors checkpoint shards",
+    ):
+        with safe_open(st_file, framework="np") as f:
+            for name in f.keys():
+                param = f.get_tensor(name)
+                yield name, param
+
+
+def fastsafetensors_weights_iterator(safetensor_list: list[str], ):
+    """
+    Return an iterator over tensors on GPU from a given safetensor_list.
+    """
+    world_size = dist.get_world_size()
+    if world_size > 1:
+        pg = dist.get_group()
+        device = f"gpu:{pg.rank}" if paddle.is_compiled_with_cuda() else "cpu"
+    else:
+        pg = SingleGroup()
+        device = f"gpu:{pg.rank()}" if paddle.is_compiled_with_cuda(
+        ) else "cpu"
+
+    safetensor_files_sub_lists = [
+        safetensor_list[i:i + world_size]
+        for i in range(0, len(safetensor_list), world_size)
+    ]
+
+    for st_file in tqdm(
+            safetensor_files_sub_lists,
+            desc="Loading fastsafetensors checkpoint shards",
+    ):
+        loader = SafeTensorsFileLoader(pg,
+                                       device,
+                                       nogds=True,
+                                       debug_log=False,
+                                       framework="paddle")
+        rank_file_map = {i: [f] for i, f in enumerate(st_file)}
+        loader.add_filenames(rank_file_map)
+        try:
+            fb = loader.copy_files_to_device()
+            try:
+                keys = list(fb.key_to_rank_lidx.keys())
+                for k in keys:
+                    t = fb.get_tensor(k)
+                    yield k, t
+            finally:
+                fb.close()
+        finally:
+            loader.close()
+
+
+def load_pre_sharded_checkpoint(model_path: str,
+                                local_rank: int,
+                                use_fastsafetensor: bool = False):
+    """
+    load_pre_sharded_checkpoint
+    """
+    state_dict = {}
+    _, safetensor_files = get_all_safetensors(
+        os.path.join(model_path, f"rank{local_rank}"))
+    weights_iterator = safetensors_weights_iterator(safetensor_files)
+    for name, weight in weights_iterator:
+        state_dict[name] = weight
+    return state_dict
+
+
+def get_all_safetensors(model_path: str):
+    """
+    get_all_safetensors
+    """
+    safe_model_path = os.path.join(model_path, "model.safetensors")
+    if os.path.exists(safe_model_path):
+        safetensor_list = [safe_model_path]
+        with safe_open(safe_model_path, framework="np", device="cpu") as f:
+            key_name_list = f.keys()
+        return key_name_list, safetensor_list
+    else:
+        with open(os.path.join(model_path, "model.safetensors.index.json"),
+                  "r") as f:
+            weight_map = json.load(f)["weight_map"]
+        weight_files_in_index = set()
+        for weight_name in weight_map:
+            weight_files_in_index.add(
+                os.path.join(model_path, weight_map[weight_name]))
+        key_name_list = list(set(weight_map.keys()))
+        safetensor_list = list(weight_files_in_index)
+        safetensor_list.sort()
+    return key_name_list, safetensor_list
+
+
+def load_tp_checkpoint_v1(
+    model_path: str,
+    cls: PretrainedModel,
+    fd_config: FDConfig,
+    use_fastsafetensor: bool = True,
+):
+    """
+    load_tp_checkpoint_v1
+    """
+
+    safetensor_keys, safetensor_files = get_all_safetensors(model_path)
+
+    if use_fastsafetensor:
+        weights_iterator = fastsafetensors_weights_iterator(safetensor_files)
+    else:
+        weights_iterator = safetensors_weights_iterator(safetensor_files)
+
+    tensor_parallel_filtered_map = {}
+    check_tensor_parallel_prerequisites(
+        fd_config,
+        cls,
+        tensor_parallel_filtered_map,
+        safetensor_keys,
+    )
+    need_tp = True if tensor_parallel_filtered_map else False
+    state_dict = {}
+    for key, weight in weights_iterator:
+        paddle.device.cuda.synchronize()
+        if need_tp and key in tensor_parallel_filtered_map:
+            action = tensor_parallel_filtered_map.pop(key)
+            tensor = action(weight).clone()
+        else:
+            tensor = weight.clone()
+        state_dict[key] = tensor
+        weight.value().get_tensor()._clear()
+    return state_dict
+
+
+def deal_state_dict(state_dict):
+    """deal_state_dict"""
+    device = paddle.CUDAPinnedPlace()
+    for name, src in state_dict.items():
+        if src._is_initialized() and not isinstance(src.place,
+                                                    paddle.CUDAPinnedPlace):
+            dst = src._copy_to(device, True)
+            dst_tensor = dst.value().get_tensor()
+            src_tensor = src.value().get_tensor()
+            src_tensor._clear()
+            src_tensor._share_data_with(dst_tensor)
+
+
+def load_composite_checkpoint(
+    model_path: str,
+    cls: PretrainedModel,
+    fd_config: FDConfig,
+    return_numpy=True,
+):
+    """
+    # This method supports loading model weights under three parallelism strategies:
+    # 1. Expert Parallel (EP)
+    # 2. Tensor Parallel (TP)
+    # 3. Pre-sharded (pre-split)
+    """
+    if fd_config.parallel_config.use_ep:
+        state_dict = load_ep_checkpoint(model_path,
+                                        fd_config.model_config,
+                                        return_numpy=True)
+    else:
+        rank_dirs = [
+            f for f in os.listdir(model_path) if f.startswith("rank")
+            and os.path.isdir(os.path.join(model_path, f))
+        ]
+        if len(rank_dirs) > 1:
+            if fd_config.parallel_config.tensor_parallel_degree != len(
+                    rank_dirs):
+                raise ValueError(
+                    f"Your model only supports loading with tp{len(rank_dirs)}"
+                )
+            state_dict = load_pre_sharded_checkpoint(
+                model_path,
+                fd_config.parallel_config.tensor_parallel_rank,
+                use_fastsafetensor=False,
+            )
+        else:
+            if fd_config.load_config.use_fastsafetensor and (
+                    current_platform.available()
+                    and current_platform.is_cuda()):
+                state_dict = load_tp_checkpoint_v1(model_path,
+                                                   cls,
+                                                   fd_config,
+                                                   use_fastsafetensor=True)
+                deal_state_dict(state_dict)
+            else:
+                state_dict = load_tp_checkpoint(model_path,
+                                                cls,
+                                                fd_config.model_config,
+                                                return_numpy=return_numpy)
+    if not state_dict:
+        raise ValueError("weight not found in state_dict !")
+    return state_dict
---
+++ fastdeploy/model_executor/models/tp_utils.py
@@ -0,0 +1,405 @@
+"""
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+import re
+from enum import Enum
+from functools import partial
+from typing import Dict, List
+
+import numpy as np
+import paddle
+from paddleformers.transformers import PretrainedModel
+from paddleformers.transformers.conversion_utils import split_or_merge_func
+from paddleformers.utils.log import logger
+
+from fastdeploy.config import FDConfig
+from fastdeploy.model_executor.models.utils import LayerIdPlaceholder
+
+
+def check_tensor_parallel_prerequisites(
+    fd_config: FDConfig,
+    cls: PretrainedModel,
+    tensor_parallel_filtered_map: Dict[str, partial],
+    safetensor_keys: List[str],
+) -> None:
+    """check_tensor_parallel_prerequisites"""
+    if fd_config.parallel_config.tensor_parallel_degree > 1:
+        tensor_parallel_map = cls._get_tensor_parallel_mappings(
+            fd_config.model_config, is_split=True)
+        if not tensor_parallel_map:
+            logger.error("filtered_quant_map should not be empty. \
+                parallel splitting required, but _get_tensor_parallel_mappings is not implemented."
+                         )
+        filtered_tp_keys = cls._resolve_prefix_keys(tensor_parallel_map.keys(),
+                                                    safetensor_keys)
+        for k, v in filtered_tp_keys.items():
+            tensor_parallel_filtered_map[v] = tensor_parallel_map.pop(k)
+        if not tensor_parallel_filtered_map:
+            logger.error("tensor_parallel_filtered_map should not be empty. \
+                The weights required for tensor parallel splitting are inconsistent with the model's weights."
+                         )
+
+
+def extract_prefix(weight_name: str) -> str:
+    """extract_prefix"""
+    if weight_name.startswith("."):
+        return ""
+    parts = weight_name.split(".", 1)
+    return parts[0] if len(parts) > 1 else ""
+
+
+def has_prefix(prefix_name: str, weight_name: str):
+    """has_prefix"""
+    return prefix_name == extract_prefix(weight_name)
+
+
+class TensorSplitMode(Enum):
+    """TensorSplitMode"""
+
+    GQA = "is_gqa"
+    TRANSPOSE = "transpose"
+    QKV = "is_old_qkv"
+    PairFused = "is_naive_2fuse"
+    TripletFused = "is_naive_3fuse"
+
+
+def extract_placeholders(template: str):
+    """extract_placeholders"""
+    return set(re.findall(r"{(\w+)}", template))
+
+
+class SafeDict(dict):
+    """SafeDict"""
+
+    def __missing__(self, key):
+        return "{" + key + "}"
+
+
+def has_placeholders(placeholders):
+    """has_placeholders"""
+    return len(placeholders) > 0
+
+
+def update_final_actions(params, final_actions, key, action):
+    """update_final_actions"""
+    new_key = key.format_map(SafeDict(params))
+    final_actions[new_key] = action
+
+
+def build_expanded_keys(num_layers, num_experts, start_layer, base_actions):
+    """build_expanded_keys"""
+    final_actions = {}
+    for key, action in base_actions.items():
+        placeholders = extract_placeholders(key)
+        if not has_placeholders(placeholders):
+            final_actions[key] = action
+        else:
+            if LayerIdPlaceholder.LAYER_ID.value in placeholders:
+                for layer_id in range(num_layers):
+                    update_final_actions(
+                        {LayerIdPlaceholder.LAYER_ID.value: layer_id},
+                        final_actions,
+                        key,
+                        action,
+                    )
+            elif LayerIdPlaceholder.FFN_LAYER_ID.value in placeholders:
+                for layer_id in range(start_layer):
+                    update_final_actions(
+                        {LayerIdPlaceholder.FFN_LAYER_ID.value: layer_id},
+                        final_actions,
+                        key,
+                        action,
+                    )
+            elif (LayerIdPlaceholder.MOE_LAYER_ID.value in placeholders
+                  and LayerIdPlaceholder.EXPERT_ID.value in placeholders):
+                for layer_id in range(start_layer, num_layers):
+                    for export_id in range(num_experts):
+                        update_final_actions(
+                            {
+                                LayerIdPlaceholder.MOE_LAYER_ID.value:
+                                layer_id,
+                                LayerIdPlaceholder.EXPERT_ID.value: export_id,
+                            },
+                            final_actions,
+                            key,
+                            action,
+                        )
+            elif (LayerIdPlaceholder.MOE_LAYER_ID.value in placeholders
+                  and len(placeholders) == 1):
+                for layer_id in range(start_layer, num_layers):
+                    update_final_actions(
+                        {LayerIdPlaceholder.MOE_LAYER_ID.value: layer_id},
+                        final_actions,
+                        key,
+                        action,
+                    )
+            else:
+                logger.error(f"{key} does not match any case.")
+    return final_actions
+
+
+def gqa_qkv_split_func(
+    tensor_parallel_degree,
+    tensor_parallel_rank,
+    num_attention_heads,
+    num_key_value_heads,
+    head_dim,
+):
+    """
+    gqa_qkv_split_func
+    """
+
+    def fn(x, is_column=True):
+        """fucn"""
+
+        def get_shape(tensor):
+            """get_shape"""
+            return tensor.get_shape() if hasattr(tensor,
+                                                 "get_shape") else tensor.shape
+
+        def slice_tensor(tensor, start, end):
+            """slice_tensor"""
+            shape = get_shape(tensor)
+            if len(shape) == 1:
+                return tensor[start:end]
+            elif is_column:
+                return tensor[..., start:end]
+            else:
+                return tensor[start:end, ...]
+
+        q_end = num_attention_heads * head_dim
+        k_end = q_end + num_key_value_heads * head_dim
+        v_end = k_end + num_key_value_heads * head_dim
+
+        q = slice_tensor(x, 0, q_end)
+        k = slice_tensor(x, q_end, k_end)
+        v = slice_tensor(x, k_end, v_end)
+
+        def split_tensor(tensor, degree):
+            """
+            split_tensor
+            """
+            shape = get_shape(tensor)
+            size = shape[-1] if is_column else shape[0]
+            block_size = size // degree
+            if hasattr(tensor, "get_shape"):
+                return [
+                    slice_tensor(tensor, i * block_size, (i + 1) * block_size)
+                    for i in range(degree)
+                ]
+            else:
+                if isinstance(x, paddle.Tensor):
+                    if is_column:
+                        return paddle.split(tensor, degree, axis=-1)
+                    else:
+                        return paddle.split(tensor, degree, axis=0)
+                else:
+                    if is_column:
+                        return np.split(tensor, degree, axis=-1)
+                    else:
+                        return np.split(tensor, degree, axis=0)
+
+        q_list = split_tensor(q, tensor_parallel_degree)
+        k_list = split_tensor(k, tensor_parallel_degree)
+        v_list = split_tensor(v, tensor_parallel_degree)
+
+        if tensor_parallel_rank is None:
+            res = []
+            for q_i, k_i, v_i in zip(q_list, k_list, v_list):
+                if is_column:
+                    if isinstance(x, paddle.Tensor):
+                        res.append(paddle.concat([q_i, k_i, v_i], axis=-1))
+                    else:
+                        res.append(np.concatenate([q_i, k_i, v_i], axis=-1))
+                else:
+                    if isinstance(x, paddle.Tensor):
+                        res.append(paddle.concat([q_i, k_i, v_i], axis=0))
+                    else:
+                        res.append(np.concatenate([q_i, k_i, v_i], axis=0))
+            return res
+        else:
+            if isinstance(x, paddle.Tensor):
+                if is_column:
+                    return paddle.concat(
+                        [
+                            q_list[tensor_parallel_rank],
+                            k_list[tensor_parallel_rank],
+                            v_list[tensor_parallel_rank],
+                        ],
+                        axis=-1,
+                    )
+                else:
+                    return paddle.concat(
+                        [
+                            q_list[tensor_parallel_rank],
+                            k_list[tensor_parallel_rank],
+                            v_list[tensor_parallel_rank],
+                        ],
+                        axis=0,
+                    )
+            else:
+                if is_column:
+                    return np.concatenate(
+                        [
+                            q_list[tensor_parallel_rank],
+                            k_list[tensor_parallel_rank],
+                            v_list[tensor_parallel_rank],
+                        ],
+                        axis=-1,
+                    )
+                else:
+                    return np.concatenate(
+                        [
+                            q_list[tensor_parallel_rank],
+                            k_list[tensor_parallel_rank],
+                            v_list[tensor_parallel_rank],
+                        ],
+                        axis=0,
+                    )
+
+    return fn
+
+
+def gqa_qkv_merge_func(num_attention_heads, num_key_value_heads, head_dim):
+    """
+    gqa_qkv_merge_func
+    """
+
+    def fn(weight_list, is_column=True):
+        """fn"""
+        tensor_parallel_degree = len(weight_list)
+        num_attention_heads = num_attention_heads // tensor_parallel_degree
+        num_key_value_heads = num_key_value_heads // tensor_parallel_degree
+
+        is_paddle_tensor = not isinstance(weight_list[0], np.ndarray)
+
+        def get_shape(tensor):
+            """
+            get_shape
+            """
+            return tensor.get_shape() if hasattr(tensor,
+                                                 "get_shape") else tensor.shape
+
+        def slice_tensor(tensor, start, end):
+            """
+            slice_tensor
+            """
+            if len(get_shape(tensor)) == 1:
+                return tensor[start:end]
+            elif is_column:
+                return tensor[..., start:end]
+            else:
+                return tensor[start:end, ...]
+
+        q_list, k_list, v_list = [], [], []
+
+        for weight in weight_list:
+            q_end = num_attention_heads * head_dim
+            k_end = q_end + num_key_value_heads * head_dim
+            v_end = k_end + num_key_value_heads * head_dim
+
+            q = slice_tensor(weight, 0, q_end)
+            k = slice_tensor(weight, q_end, k_end)
+            v = slice_tensor(weight, k_end, v_end)
+
+            q_list.append(q)
+            k_list.append(k)
+            v_list.append(v)
+
+        merged = q_list + k_list + v_list
+
+        if is_paddle_tensor:
+            if is_column:
+                tensor = paddle.concat(merged, axis=-1)
+            else:
+                tensor = paddle.concat(merged, axis=0)
+            if tensor.place.is_gpu_place():
+                tensor = tensor._copy_to(paddle.CUDAPinnedPlace(), False)
+            return tensor
+        else:
+            if is_column:
+                return np.concatenate(merged, axis=-1)
+            else:
+                return np.concatenate(merged, axis=0)
+
+    return fn
+
+
+def split_or_merge_qkv_func(
+    is_split,
+    tensor_parallel_degree,
+    tensor_parallel_rank,
+    num_attention_heads,
+    num_key_value_heads,
+    head_dim,
+):
+    """
+    split_or_merge_qkv_func
+    """
+    if is_split:
+        return gqa_qkv_split_func(
+            tensor_parallel_degree=tensor_parallel_degree,
+            tensor_parallel_rank=tensor_parallel_rank,
+            num_attention_heads=num_attention_heads,
+            num_key_value_heads=num_key_value_heads,
+            head_dim=head_dim,
+        )
+    else:
+        return gqa_qkv_merge_func(
+            num_attention_heads=num_attention_heads,
+            num_key_value_heads=num_key_value_heads,
+            head_dim=head_dim,
+        )
+
+
+def split_or_merge_func_v1(
+    is_split,
+    tensor_parallel_degree,
+    tensor_parallel_rank,
+    num_attention_heads=None,
+    num_key_value_heads=None,
+    head_dim=None,
+):
+    """
+    split_or_merge_func_v1
+    """
+
+    def fn(x, **kwargs):
+        """func"""
+        is_gqa = kwargs.pop("is_gqa", False)
+        if is_gqa:
+            func = split_or_merge_qkv_func(
+                is_split=is_split,
+                tensor_parallel_degree=tensor_parallel_degree,
+                tensor_parallel_rank=tensor_parallel_rank,
+                num_attention_heads=num_attention_heads,
+                num_key_value_heads=num_key_value_heads,
+                head_dim=head_dim,
+            )
+            is_column = kwargs.pop("is_column", True)
+            return func(x, is_column=is_column)
+        else:
+            func = split_or_merge_func(
+                is_split=is_split,
+                tensor_parallel_degree=tensor_parallel_degree,
+                tensor_parallel_rank=tensor_parallel_rank,
+                num_attention_heads=num_attention_heads,
+            )
+            is_column = kwargs.pop("is_column", True)
+            is_naive_2fuse = kwargs.pop("is_naive_2fuse", False)
+            return func(x, is_column=is_column, is_naive_2fuse=is_naive_2fuse)
+
+    return fn
