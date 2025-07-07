c022ea5c27fb848490c526c04441a3db33179e7b
vivienfanghua <vivienfanghua@163.com>
2025-07-07 20:20:56 +0800

    draft change rejectionsampling topk=40

--- custom_ops/gpu_ops/sample_kernels/sampling.cuh
+++ custom_ops/gpu_ops/sample_kernels/sampling.cuh
@@ -287,7 +287,7 @@ __global__ void TopKTopPSamplingFromProbKernel(DType* probs, IdType* output, flo
   curandStatePhilox4_32_10_t state;
   curand_init(philox_seed, bx, philox_offset, &state);
   const uint32_t row_idx = bx;
-  const uint32_t k = top_p_arr[row_idx] == 0 ? 1 : 20;
+  const uint32_t k = top_p_arr[row_idx] == 0 ? 1 : 40;
   const float p = top_p_arr[row_idx] == 0 ? 1e-6 : top_p_arr[row_idx];
 
   extern __shared__ __align__(
