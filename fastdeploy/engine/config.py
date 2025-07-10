1054e35353b5d909074e3cf3939e30363b0ab91e
yulangz <1301481108@qq.com>
2025-07-10 07:37:18 +0000

    update

--- fastdeploy/engine/config.py
+++ fastdeploy/engine/config.py
@@ -685,7 +685,8 @@ class Config:
         self.engine_worker_queue_port = engine_worker_queue_port
         self.device_ids = ",".join([str(i) for i in range(self.worker_num_per_node)])
         self.device_ids = os.getenv("CUDA_VISIBLE_DEVICES", self.device_ids)
-        self.device_ids = os.getenv("XPU_VISIBLE_DEVICES", self.device_ids)
+        if current_platform.is_xpu():
+            self.device_ids = os.getenv("XPU_VISIBLE_DEVICES", self.device_ids)
 
         self.read_from_config()
         self.postprocess()
