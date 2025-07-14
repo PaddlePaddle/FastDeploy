63db1cb2fb56541b36b7ded89082f133a84d49c5
YuanRisheng <yuanrisheng@baidu.com>
2025-07-14 09:44:48 +0000

    fix qwen moe

--- fastdeploy/model_executor/models/qwen3moe.py
+++ fastdeploy/model_executor/models/qwen3moe.py
@@ -121,7 +121,7 @@ class Qwen3DecoderLayer(nn.Layer):
                                 moe_intermediate_size=fd_config.model_config.
                                 moe_intermediate_size,
                                 num_experts=fd_config.model_config.moe_num_experts,
-                                top_k=fd_config.model_config.num_experts_per_tok,
+                                top_k=fd_config.model_config.top_k,
                                 layer_idx=layer_id,
                                 weight_key_map=weight_key_map)
         else:
