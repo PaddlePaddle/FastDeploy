c85eaa9163c7df9a256f62bde8efce4f329d7525
xiaoxiaohehe001 <hiteezsf@163.com>
2025-07-16 02:16:00 +0800

    fix_45t_mm

--- fastdeploy/model_executor/load_weight_utils.py
+++ fastdeploy/model_executor/load_weight_utils.py
@@ -43,11 +43,21 @@ def load_ep_checkpoint(model_path: str,
     filtered_map = {k: v for k, v in weight_list.items() if "experts" not in k}
     num_local_ffn_keys = []
 
-    for i in range(config.moe_layer_start_index, config.num_hidden_layers):
-        for j in range(
-                config.num_experts_start_offset,
-                config.num_experts_start_offset + config.num_experts_per_rank,
-        ):
+    from itertools import chain
+    def get_expert_ranges(config):
+        """
+        """
+        base_range = range(
+            config.num_experts_start_offset,
+            config.num_experts_start_offset + config.num_experts_per_rank
+        )
+        if isinstance(config.moe_num_experts, list):
+            return chain(base_range,
+                        range(base_range.start + config.moe_num_experts[0], base_range.stop + config.moe_num_experts[0]))
+        return base_range
+
+    for i in range(config.moe_layer_start_index, config.num_layers):
+        for j in get_expert_ranges(config):
             ffn1_key = f"ernie.layers.{i}.mlp.experts.{j}.up_gate_proj.weight"
             ffn2_key = (f"ernie.layers.{i}.mlp.experts.{j}.down_proj.weight")
 
