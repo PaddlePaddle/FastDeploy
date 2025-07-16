feec7673ec97e573002a842dad4d171bd248d753
shige <1021937542@qq.com>
2025-07-16 14:04:16 +0800

    add opentelemetry on dequeue

--- fastdeploy/engine/engine.py
+++ fastdeploy/engine/engine.py
@@ -57,6 +57,7 @@ class LLMEngine(object):
     Attributes:
         cfg (Config): Configuration object containing all the parameters.
         cached_generated_tokens (queue.Queue): Queue to store generated tokens.
+        cached_generated_tokens (queue.Queue): Queue to store generated tokens.
         scheduler (LocalScheduler or GlobalScheduler): Scheduling tasks.
         input_processor (InputPreprocessor): Preprocessor for input data.
         resource_manager (ResourceManager): Manager for resource allocation.
@@ -711,7 +712,7 @@ class LLMEngine(object):
         Insert tasks to engine.
         """
         for task in tasks:
-                start_span_request("DEQUEUE", task, trace.SpanKind.CONSUMER) 
+            start_span_request("DEQUEUE", task, trace.SpanKind.CONSUMER) 
         # TODO 返回至 scheduler
         if allocated:
             current_tasks = []
