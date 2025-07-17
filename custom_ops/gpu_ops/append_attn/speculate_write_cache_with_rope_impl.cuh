6c248db776edd68bc1970015cf2de007740a060f
zhoutianzi666 <17801055074@163.com>
2025-07-17 12:09:51 +0800

    remove padding_offsets from atten

--- custom_ops/gpu_ops/append_attn/speculate_write_cache_with_rope_impl.cuh
+++ custom_ops/gpu_ops/append_attn/speculate_write_cache_with_rope_impl.cuh
@@ -214,7 +214,7 @@ __global__ void append_speculate_cache_rope_kernel(
        linear_index < elem_cnt;
        linear_index += step) {
     const int token_id = linear_index / hidden_size;
-    const int ori_bi = (token_id + batch_id_per_token[token_id]) / max_seq_len;
+    const int ori_bi = batch_id_per_token[token_id];
     if (seq_lens_decoder[ori_bi] == 0) continue;
     const int bias = linear_index % hidden_size;
     const int hi = bias / head_size;  // q + k + v
@@ -347,7 +347,7 @@ __global__ void append_speculate_cache_neox_rope_kernel(
        linear_index < elem_cnt;
        linear_index += step) {
     const int token_id = linear_index / half_hidden_size;
-    const int ori_bi = (token_id + batch_id_per_token[token_id]) / max_seq_len;
+    const int ori_bi = batch_id_per_token[token_id];
     if (seq_lens_decoder[ori_bi] == 0) continue;
     const int bias = linear_index % half_hidden_size;
     const int hi = bias / half_head_size;  // q + k + v
