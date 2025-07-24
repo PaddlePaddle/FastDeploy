e414faed2ba969f0cc4b9a8587bfb1e056331d23
Wanglongzhi2001 <583087864@qq.com>
2025-07-24 16:23:24 +0800

    update

--- fastdeploy/model_executor/layers/moe/ep.py
+++ fastdeploy/model_executor/layers/moe/ep.py
@@ -194,7 +194,21 @@ class DeepEPEngine:
         Return:
             combined_hidden_states: [num_tokens, hidden]
         """
-
+        # TODO(@wufeisheng): Delete them when deepep in PaddlePaddle is fixed 
+        (
+            src_info,
+            layout_range,
+            num_max_dispatch_tokens_per_rank,
+            num_experts,
+        ) = handle
+        handle = (
+            src_info,
+            layout_range,
+            num_max_dispatch_tokens_per_rank,
+            None,
+            num_experts,
+        )
+        
         combined_hidden_states, _, combine_hook = self.decode_deepep_engine.low_latency_combine(
             hidden_states,
             topk_idx,
