950b01b03f3edd0d5fcb239abaa6463ff6b64cd8
XieYunshen <1084314248@qq.com>
2025-07-28 20:06:47 +0800

    Refactored ci pipeline

--- scripts/coverage_run.sh
+++ scripts/coverage_run.sh
@@ -6,7 +6,23 @@ run_path="$DIR/../test/"
 cd ${run_path}
 ls
 
-dirs=("layers" "operators" "worker" "utils")
+exclude=("ci_use")
+for d in */ ; do
+  dir_name="${d%/}"
+  if [[ -d "$dir_name" ]]; then
+    skip=false
+    for ex in "${exclude[@]}"; do
+      if [[ "$dir_name" == "$ex" ]]; then
+        skip=true
+        break
+      fi
+    done
+    if ! $skip; then
+      dirs+=("$dir_name")
+    fi
+  fi
+done
+
 failed_tests_file="failed_tests.log"
 > "$failed_tests_file"
 disabled_tests=(
--- scripts/run_pre_ce.sh
+++ scripts/run_pre_ce.sh
@@ -3,10 +3,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "$DIR"
 
 python -m pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
-python -m pip install --pre paddlepaddle-gpu -i https://www.paddlepaddle.org.cn/packages/nightly/cu126/
 python -m pip install -r requirements.txt
 python -m pip install jsonschema aistudio_sdk==0.3.5
-bash build.sh || exit 1
 
 failed_files=()
 run_path="$DIR/../test/ci_use/"
