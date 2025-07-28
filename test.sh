ded97b792975ad3f56967d666815ab5ed5918809
ZoengHN <1290971635@qq.com>
2025-07-28 19:18:04 +0800

    解决权重导入问题

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 pkill python
 rm -rf log
-python -m fastdeploy.entrypoints.openai.api_server --model baidu/ERNIE-4.5-0.3B-Paddle --port 8180 --metrics-port 8181 --engine-worker-queue-port 8183 --quantization wint4 --max-model-len 32768 --max-num-seqs 32
+# python -m fastdeploy.entrypoints.openai.api_server --model baidu/ERNIE-4.5-0.3B-Paddle --port 8180 --metrics-port 8181 --engine-worker-queue-port 8183 --quantization wint4 --max-model-len 32768 --max-num-seqs 32
+
+python -m fastdeploy.entrypoints.openai.api_server \
+       --model /root/.paddlenlp/models/Qwen/Qwen2.5-VL-7B-Instruct \
+       --port 8180 \
+       --metrics-port 8181 \
+       --engine-worker-queue-port 8183 \
+       --quantization wint4 \
+       --max-model-len 32768 \
+       --max-num-seqs 32 \
+       --reasoning-parser qwen-25-vl \
+       --enable-mm
