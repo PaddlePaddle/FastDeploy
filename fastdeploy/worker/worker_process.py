38b1efe181f95ec190c3f81eb4931c1751d6d7e6
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-07-29 11:41:37 +0800

    Update worker_process.py

--- fastdeploy/worker/worker_process.py
+++ fastdeploy/worker/worker_process.py
@@ -287,7 +287,7 @@ class PaddleDisWorkerProc:
                 if self.task_queue.num_tasks() > 0:
                     # VL only support 1 batch to prefill
                     if not self.fd_config.model_config.enable_mm or not self.worker.prefill_finished():
-                        if self.nnode > 1 and self.parallel_config.tensor_parallel_size > 1
+                        if self.nnode > 1 and self.parallel_config.tensor_parallel_size > 1:
                             self.task_queue.read_finish_flag.set(1)
                         else:
                             self.exist_task_signal.value[self.fd_config.parallel_config.expert_parallel_rank] = 1
