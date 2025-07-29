7e99db49169400c5fe22b79ddb379020aa60a72c
gongshaotian <gstian5555@outlook.com>
2025-07-29 22:10:07 +0800

    update flas attn backend

--- fastdeploy/model_executor/layers/attention/append_attn_backend.py
+++ fastdeploy/model_executor/layers/attention/append_attn_backend.py
@@ -83,8 +83,8 @@ class AppendAttentionBackend(AttentionBackend):
         kv_num_heads: int,
         num_heads: int,
         head_dim: int,
-        encoder_block_shape_q: int,
-        decoder_block_shape_q: int,
+        encoder_block_shape_q: int = -1,
+        decoder_block_shape_q: int = -1,
     ) -> None:
         """
         AppendAttentionBackend __init__
