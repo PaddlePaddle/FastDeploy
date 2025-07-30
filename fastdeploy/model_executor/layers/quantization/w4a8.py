bae23dc09458f212998be114ab0e1263afa36d52
carryyu <569782149@qq.com>
2025-07-30 19:27:57 +0800

    Revert "support model loading for w4a8 offline quant (#3064)"
    This reverts commit 3214fb5393f711c6d5579288fda58ab39ac1d75e.

--- fastdeploy/model_executor/layers/quantization/w4a8.py
+++ fastdeploy/model_executor/layers/quantization/w4a8.py
@@ -25,17 +25,15 @@ class W4A8Config(QuantConfigBase):
     quantization config for weight 4bits and activation 8bits
     """
 
-    def __init__(self, is_permuted) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.is_permuted = is_permuted
 
     def name(self) -> str:
         return "w4a8"
 
     @classmethod
     def from_config(cls, config: dict) -> "W4A8Config":
-        is_permuted = getattr(config, "is_permuted", False)
-        return cls(is_permuted)
+        return cls()
 
     def get_quant_method(self, layer) -> Optional[QuantMethodBase]:
         if isinstance(layer, FusedMoE):
