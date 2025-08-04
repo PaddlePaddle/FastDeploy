9143d64973889300a87c11fb4f3476b873c5435e
EmmonsCurse <1577972691@qq.com>
2025-08-04 17:10:12 +0800

    [CI] add test_compare_top_logprobs

--- tools/codestyle/pre_commit.sh
+++ tools/codestyle/pre_commit.sh
@@ -29,7 +29,8 @@ if ! [[ $(python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1$2}') -ge 36 
     exit 1
 fi
 
-diff_files=$(git diff --name-only --diff-filter=ACMR ${BRANCH})
+# Exclude any files under the 'test/ce/server/' directory from code style checks.
+diff_files=$(git diff --name-only --diff-filter=ACMR ${BRANCH} | grep -v '^test/ce/server/')
 num_diff_files=$(echo "$diff_files" | wc -l)
 echo -e "diff files between pr and ${BRANCH}:\n${diff_files}"
 
