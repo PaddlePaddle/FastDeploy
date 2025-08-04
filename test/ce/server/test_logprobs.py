c1cc4150271c904c1873f8a271cd949f46991db6
ZhangYulongg <1272816783@qq.com>
2025-08-04 17:01:14 +0800

    [ci] add CI case

--- test/ce/server/test_logprobs.py
+++ test/ce/server/test_logprobs.py
@@ -1,6 +1,6 @@
-from core import *
-import requests
 import json
+
+from core import *
 
 
 def test_unstream_with_logprobs():
@@ -36,7 +36,7 @@ def test_unstream_with_logprobs():
         "prompt_tokens": 22,
         "total_tokens": 25,
         "completion_tokens": 3,
-        "prompt_tokens_details": {"cached_tokens": 0}
+        "prompt_tokens_details": {"cached_tokens": 0},
     }
 
 
