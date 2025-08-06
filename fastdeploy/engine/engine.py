0951d60ef0cd18dc3c1cb8987594cd17e446d288
liyonghua0910 <lyhhh37@outlook.com>
2025-08-06 05:12:36 +0000

    [fix] fix metrics num_requests_waiting and num_requests_running

--- fastdeploy/engine/engine.py
+++ fastdeploy/engine/engine.py
@@ -425,6 +425,8 @@ class LLMEngine:
                     get_request_pool.submit(_fetch_request)
                 # 2. Schedule requests
                 tasks = self.resource_manager.schedule()
+                main_process_metrics.num_requests_waiting.dec(len(tasks))
+                main_process_metrics.num_requests_running.inc(len(tasks))
                 # 3. Send to engine
                 if tasks:
                     self.resource_manager.get_real_bsz()
