740be61057b92e01af17eaf4e89e399d7b43344a
chenjian <1435317881@qq.com>
2025-08-10 13:55:38 +0800

    [Bug Fix] Fix scheduler bug in develop (#3292)
    * Fix scheduler bug in develop
    
    * Fix scheduler bug in develop
    
    * Fix scheduler bug in develop

--- fastdeploy/output/token_processor.py
+++ fastdeploy/output/token_processor.py
@@ -431,8 +431,13 @@ class TokenProcessor:
         else:
             batch = self.output_tokens[1, 0]
             tokens = tokens[2 : batch + 2]
-
+        
         batch_result = list()
+        if envs.ENABLE_V1_KVCACHE_SCHEDULER:
+            need_to_be_reschedule_req_ids = list(self.resource_manager.to_be_rescheduled_request_id_set)
+            for request_id in need_to_be_reschedule_req_ids:
+                if self.resource_manager.requests[request_id].idx >= (batch - 1):  # No more token generated for preempted request
+                    self.resource_manager.reschedule_preempt_task(request_id)
         for i in range(batch):
             if self.resource_manager.stop_flags[i]:
                 continue
@@ -459,6 +464,8 @@ class TokenProcessor:
                 if recovery_stop:
                     llm_logger.info(f"recovery stop signal found at task {task_id}")
                 if not recovery_stop and token_id < 0:
+                    if task_id in self.resource_manager.to_be_rescheduled_request_id_set:
+                        self.resource_manager.reschedule_preempt_task(task_id)
                     continue
 
             if task.get("prefill_chunk_info", None) is not None:
