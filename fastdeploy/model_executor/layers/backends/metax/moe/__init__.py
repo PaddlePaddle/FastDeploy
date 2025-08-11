27889245b10614dade16dd65694541f4a6ba99e9
Kane2011 <86709049+Kane2011@users.noreply.github.com>
2025-08-11 13:05:06 +0800

    Update __init__.py
    1. remove metax's key word comment;
    2. add fused_moe_kernel_paddle import

--- fastdeploy/model_executor/layers/backends/metax/moe/__init__.py
+++ fastdeploy/model_executor/layers/backends/metax/moe/__init__.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" "
-metax gpu moe
-"""
+
+from .triton_moe_kernels import fused_moe_kernel_paddle
 
 __all__ = [
     "fused_moe_kernel_paddle",
