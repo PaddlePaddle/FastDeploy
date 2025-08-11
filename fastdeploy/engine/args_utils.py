22193a5e10207b9f70f00e2e70ea1b7405cbec76
rainyfly <1435317881@qq.com>
2025-08-11 13:49:22 +0800

    fix block num setting in scheduler v1

--- fastdeploy/engine/args_utils.py
+++ fastdeploy/engine/args_utils.py
@@ -884,7 +884,7 @@ class EngineArgs:
             if self.enable_chunked_prefill:
                 self.max_num_batched_tokens = 2048
             else:
-                self.max_num_batched_tokens = self.max_model_len
+                self.max_num_batched_tokens = 8192 # if set to max_model_len, it's easy to be OOM
 
         all_dict = asdict(self)
         all_dict["model_cfg"] = model_cfg
