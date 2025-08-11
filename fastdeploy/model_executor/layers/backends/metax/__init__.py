752829fce978ac87620e486311ee526ed75bfadd
Kane2011 <86709049+Kane2011@users.noreply.github.com>
2025-08-11 13:01:39 +0800

    Update __init__.py
    1. remove metax's key work comment

--- fastdeploy/model_executor/layers/backends/metax/__init__.py
+++ fastdeploy/model_executor/layers/backends/metax/__init__.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-metax gpu backend methods
-"""
-
 from .attention.flash_attn_backend import FlashAttentionBackend
 from .moe.fused_moe_triton_metax_backend import MetaxTritonWeightOnlyMoEMethod
 
