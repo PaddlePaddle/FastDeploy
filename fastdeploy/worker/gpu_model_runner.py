25f9fb536f94fde7d8552cc0611ce0fb3de3ffdd
gongshaotian <gstian5555@outlook.com>
2025-08-12 20:51:56 +0800

    1.fix dynamic_weight_manager bug 2.add clear api for CasualLM

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -836,6 +836,7 @@ class GPUModelRunner(ModelRunnerBase):
         # 1. Load original model
         model_loader = get_model_loader(load_config=self.fd_config.load_config)
         self.model = model_loader.load_model(fd_config=self.fd_config)
+
         # 1.1 Load RL dynamic model
         if self.fd_config.load_config.dynamic_load_weight:
             from fastdeploy.rl.dynamic_weight_manager import DynamicWeightManager
@@ -1558,7 +1559,7 @@ class GPUModelRunner(ModelRunnerBase):
 
         # Clear CudaGraph
         if self.use_cudagraph:
-            self.model.clear_grpah_opt_backend(fd_config=self.fd_config)
+            self.model.clear_grpah_opt_backend()
 
         self.dynamic_weight_manager._log_memory("dynamic weight manager clear all memory")
 
