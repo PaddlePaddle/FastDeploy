145711ba95851204ca8e44339759fb2dd8225501
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2025-08-13 11:38:07 +0800

    Update test_EB_Lite_serving_mtp.py

--- test/ci_use/EB_Lite_mtp/test_EB_Lite_serving_mtp.py
+++ test/ci_use/EB_Lite_mtp/test_EB_Lite_serving_mtp.py
@@ -350,7 +350,7 @@ def test_streaming(openai_client, capsys):
 def test_profile_reset_block_num():
     """测试profile reset_block_num功能，与baseline diff不能超过5%"""
     log_file = "./log/config.log"
-    baseline = 6058
+    baseline = 30065
 
     if not os.path.exists(log_file):
         pytest.fail(f"Log file not found: {log_file}")
