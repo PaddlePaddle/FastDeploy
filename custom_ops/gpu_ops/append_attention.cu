233d133d48ca8b8f1cc326e446d1d35c848245c9
liumengyuan <liumengyuan@mail.ustc.edu.cn>
2025-08-14 22:13:11 +0800

    add mask_offset para to align with develop

--- custom_ops/gpu_ops/append_attention.cu
+++ custom_ops/gpu_ops/append_attention.cu
@@ -624,6 +624,7 @@ void AppendAttentionWithOutput(
     const paddle::optional<paddle::Tensor>& cache_v_zp,
     const paddle::optional<paddle::Tensor>& out_linear_shifts,
     const paddle::optional<paddle::Tensor>& out_linear_smooths,
+    const paddle::optional<paddle::Tensor>& mask_offset,
     const paddle::optional<paddle::Tensor>& kv_signal_data,
     const paddle::optional<paddle::Tensor>& q_norm_weight,
     const paddle::optional<paddle::Tensor>& k_norm_weight,
@@ -661,6 +662,10 @@ void AppendAttentionWithOutput(
   meta_data.max_blocks_per_seq = block_tables.dims()[1];
   meta_data.block_size = key_cache.dims()[2];
   meta_data.batch_size = seq_lens_this_time.dims()[0];
+
+  if (mask_offset) {
+    meta_data.mask_offset = mask_offset.get().data<int>();
+  }
 
   auto dispatch_by_template = [&](auto temp_args) -> void {
       AppendAttentionKernel<type2value<decltype(temp_args)>::value>(
@@ -932,6 +937,7 @@ std::vector<std::vector<int64_t>> AppendAttentionWithOutputInferShape(
     const paddle::optional<std::vector<int64_t>>& cache_v_zp_shape,
     const paddle::optional<std::vector<int64_t>>& out_linear_shifts_shape,
     const paddle::optional<std::vector<int64_t>>& out_linear_smooths_shape,
+    const paddle::optional<std::vector<int64_t>>& mask_offset_shape,
     const paddle::optional<std::vector<int64_t>>& kv_signal_data_shape,
     const paddle::optional<std::vector<int64_t>>& q_norm_weight_shape,
     const paddle::optional<std::vector<int64_t>>& k_norm_weight_shape,
@@ -988,6 +994,7 @@ std::vector<paddle::DataType> AppendAttentionWithOutputInferDtype(
     const paddle::optional<paddle::DataType>& cache_v_zp_dtype,
     const paddle::optional<paddle::DataType>& out_linear_shifts_dtype,
     const paddle::optional<paddle::DataType>& out_linear_smooths_dtype,
+    const paddle::optional<paddle::DataType>& mask_offset_dtype,
     const paddle::optional<paddle::DataType>& kv_signal_data_dtype,
     const paddle::optional<paddle::DataType>& q_norm_weight_dtype,
     const paddle::optional<paddle::DataType>& k_norm_weight_dtype,
@@ -1107,9 +1114,10 @@ PD_BUILD_STATIC_OP(append_attention_with_output)
              paddle::Optional("cache_v_zp"),
              paddle::Optional("out_linear_shifts"),
              paddle::Optional("out_linear_smooths"),
+             paddle::Optional("mask_offset"),
              paddle::Optional("kv_signal_data"),
-              paddle::Optional("q_norm_weight"),
-              paddle::Optional("k_norm_weight")})
+             paddle::Optional("q_norm_weight"),
+             paddle::Optional("k_norm_weight")})
     .Outputs({"fmha_out_out", "qkv_out", "key_cache_out", "value_cache_out"})
     .SetInplaceMap({{"fmha_out", "fmha_out_out"},
                     {"key_cache", "key_cache_out"},
--- custom_ops/gpu_ops/cpp_extensions.cc
+++ custom_ops/gpu_ops/cpp_extensions.cc
@@ -120,6 +120,7 @@ void AppendAttentionWithOutput(
     const paddle::optional<paddle::Tensor> &cache_v_zp,
     const paddle::optional<paddle::Tensor> &out_linear_shifts,
     const paddle::optional<paddle::Tensor> &out_linear_smooths,
+    const paddle::optional<paddle::Tensor> &mask_offset,
     const paddle::optional<paddle::Tensor> &kv_signal_data,
     const paddle::optional<paddle::Tensor>& q_norm_weight,
     const paddle::optional<paddle::Tensor>& k_norm_weight,
