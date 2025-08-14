b9c4f159ae7f63a5dbe6c887ecfa02875f8d4432
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2025-08-14 19:36:06 +0800

    Update test_Qwen3-MoE_serving.py

--- test/ci_use/Qwen3-MoE/test_Qwen3-MoE_serving.py
+++ test/ci_use/Qwen3-MoE/test_Qwen3-MoE_serving.py
@@ -303,7 +303,7 @@ def test_non_thinking_prompt(api_url, headers):
 def test_profile_reset_block_num():
     """测试profile reset_block_num功能，与baseline diff不能超过5%"""
     log_file = "./log/config.log"
-    baseline = 3253
+    baseline = 17864
 
     if not os.path.exists(log_file):
         pytest.fail(f"Log file not found: {log_file}")
