137d66cfa6436068a12e0dc9e7ed3204048b3d70
luukunn <981429396@qq.com>
2025-08-20 12:56:11 +0800

    add x1 enable_thinking

--- fastdeploy/input/ernie_processor.py
+++ fastdeploy/input/ernie_processor.py
@@ -195,6 +195,8 @@ class ErnieProcessor(BaseDataProcessor):
             request["temperature"] = 1
         if request.get("top_p") < _SAMPLING_EPS:
             request["top_p"] = _SAMPLING_EPS
+        if self.reasoning_parser and self.reasoning_parser.__class__.__name__ == "ErnieX1ReasoningParser":
+            request["enable_thinking"] = True
         data_processor_logger.info(f"Processed request {request}")
 
         return request
