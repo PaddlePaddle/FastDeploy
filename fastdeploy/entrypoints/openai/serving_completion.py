684885160bc3003942b616e72ce7cbcbbae2a8b1
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-08-22 11:34:57 +0800

    Update serving_completion.py

--- fastdeploy/entrypoints/openai/serving_completion.py
+++ fastdeploy/entrypoints/openai/serving_completion.py
@@ -109,20 +109,21 @@ class OpenAIServingCompletion:
         except Exception:
             return ErrorResponse(code=408, message=f"Request queued time exceed {self.max_waiting_time}")
         try:
-            for idx, prompt in enumerate(request_prompts):
-                request_id_idx = f"{request_id}-{idx}"
-                current_req_dict = request.to_dict_for_infer(request_id_idx, prompt)
-                try:
+            try:
+                for idx, prompt in enumerate(request_prompts):
+                    request_id_idx = f"{request_id}-{idx}"
+                    current_req_dict = request.to_dict_for_infer(request_id_idx, prompt)
                     current_req_dict["arrival_time"] = time.time()
                     prompt_token_ids = self.engine_client.format_and_add_data(current_req_dict)
                     if isinstance(prompt_token_ids, np.ndarray):
                         prompt_token_ids = prompt_token_ids.tolist()
                     text_after_process_list.append(current_req_dict.get("text_after_process"))
                     prompt_batched_token_ids.append(prompt_token_ids)
-                except Exception as e:
-                    return ErrorResponse(message=str(e), code=400)
+            except Exception as e:
+                self.engine_client.semaphore.release()
+                return ErrorResponse(message=str(e), code=400)
 
-                del current_req_dict
+            del current_req_dict
 
             if request.stream:
                 return self.completion_stream_generator(
