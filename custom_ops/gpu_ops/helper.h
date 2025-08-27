265eb75966b2a8098733b9f4959def6c33fb1e80
baoqiwen <baoqiwen@baidu.com>
2025-08-27 07:44:20 +0000

    Resolve conflicts introduced after updating the code.

--- custom_ops/gpu_ops/helper.h
+++ custom_ops/gpu_ops/helper.h
@@ -182,12 +182,6 @@ public:
   typedef paddle::bfloat16 data_t;
 };
 
-template <> class PDTraits<paddle::DataType::FLOAT8_E4M3FN> {
-public:
-  using DataType = cutlass::float_e4m3_t;             
-  using data_t = paddle::float8_e4m3fn;       
-};
-
 template <> class PDTraits<paddle::DataType::INT8> {
 public:
   typedef int8_t DataType;
@@ -202,9 +196,15 @@ public:
 
 template <> class PDTraits<paddle::DataType::FLOAT8_E4M3FN> {
 public:
-  typedef __nv_fp8_e4m3 DataType;
-  typedef paddle::float8_e4m3fn data_t;
+  using DataType = cutlass::float_e4m3_t;             
+  using data_t = paddle::float8_e4m3fn;       
 };
+
+// template <> class PDTraits<paddle::DataType::FLOAT8_E4M3FN> {
+// public:
+//   typedef __nv_fp8_e4m3 DataType;
+//   typedef paddle::float8_e4m3fn data_t;
+// };
 
 template <typename T, int Size> struct alignas(sizeof(T) * Size) AlignedVector {
   T val[Size];
