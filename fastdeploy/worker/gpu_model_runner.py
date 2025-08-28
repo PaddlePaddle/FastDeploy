545055c2eeab04221195cf4286ba02dfe94821f8
lizexu123 <2694294196@qq.com>
2025-08-28 04:12:08 +0000

    fix

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -260,7 +260,10 @@ class GPUModelRunner(ModelRunnerBase):
                     self.share_inputs["need_think_end"][idx : idx + 1, :] = 0
                     self.share_inputs["reasoning_index"][idx : idx + 1, :] = 0
 
-                input_ids = request.prompt_token_ids + request.output_token_ids
+                if len(request.output_token_ids) == 0:
+                    input_ids = request.prompt_token_ids
+                else:
+                    input_ids = request.prompt_token_ids + request.output_token_ids
                 logger.debug(
                     f"Handle prefill request {request} at idx {idx} prefill_start_index {prefill_start_index} prefill_end_index {prefill_end_index} need_prefilled_token_num {len(input_ids)}"
                 )
