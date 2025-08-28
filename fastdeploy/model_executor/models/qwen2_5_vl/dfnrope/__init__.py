d24508eead9be39176c2b220535dbeb2c3589e37
wangyafeng <wangyafeng@baidu.com>
2025-08-28 11:43:04 +0800

    adapt qwen2_5_vl add init.py v2

--- fastdeploy/model_executor/models/qwen2_5_vl/dfnrope/__init__.py
+++ fastdeploy/model_executor/models/qwen2_5_vl/dfnrope/__init__.py
@@ -1,7 +1,7 @@
 """
-# Copyright (c) 2025  PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -13,3 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
+
+from .configuration import DFNRopeVisionTransformerConfig
+from .modeling import DFNRopeVisionTransformerPretrainedModel
+
+__all__ = [
+    "DFNRopeVisionTransformerConfig",
+    "DFNRopeVisionTransformerPretrainedModel",
+]
