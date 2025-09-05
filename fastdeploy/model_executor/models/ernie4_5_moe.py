45c0ae2eee1512f63c2dd6128f51bad9e56bdff8
ckl117 <ckl117@163.com>
2025-09-05 14:47:44 +0800

    check

--- fastdeploy/model_executor/models/ernie4_5_moe.py
+++ fastdeploy/model_executor/models/ernie4_5_moe.py
@@ -637,7 +637,7 @@ class Ernie4_5_ForCausalLM(Ernie4_5_MoeForCausalLM):
         return "Ernie4_5_ForCausalLM"
 
 
-class Ernie4_5ForCausalLM(Ernie4_5_MoeForCausalLM):
+class Ernie4_5ForCausalLM(Ernie4_5_ForCausalLM):
     """
     Ernie4_5ForCausalLM 0.3B-PT
     """
@@ -801,3 +801,16 @@ class Ernie4_5_PretrainedModel(Ernie4_5_MoePretrainedModel):
         Model Architecture Name
         """
         return "Ernie4_5_ForCausalLM"
+
+
+class Ernie4_5PretrainedModel(Ernie4_5_PretrainedModel):
+    """
+    Ernie4_5PretrainedModel 0.3B-PT
+    """
+
+    @classmethod
+    def arch_name(self):
+        """
+        Model Architecture Name
+        """
+        return "Ernie4_5ForCausalLM"
