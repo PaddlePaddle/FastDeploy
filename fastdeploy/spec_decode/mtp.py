8f8011dc6db107dfc63a9b1c1cf5e342dfa87866
littledgg <1658565283@qq.com>
2025-09-05 11:21:18 +0800

    solve mtp

--- fastdeploy/spec_decode/mtp.py
+++ fastdeploy/spec_decode/mtp.py
@@ -212,19 +212,21 @@ class MTPProposer(Proposer):
             self.target_model_inputs["max_len_tensor_cpu"]
         ).cpu()
 
-        self.model_inputs["encoder_batch_ids"] = paddle.zeros_like(self.main_model_inputs["encoder_batch_ids"])
+        self.model_inputs["encoder_batch_ids"] = paddle.zeros_like(self.target_model_inputs["encoder_batch_ids"])
         self.model_inputs["encoder_tile_ids_per_batch"] = paddle.zeros_like(
-            self.main_model_inputs["encoder_tile_ids_per_batch"]
+            self.target_model_inputs["encoder_tile_ids_per_batch"]
         )
         self.model_inputs["encoder_num_blocks_x_cpu"] = paddle.zeros_like(
-            self.main_model_inputs["encoder_num_blocks_x_cpu"]
+            self.target_model_inputs["encoder_num_blocks_x_cpu"]
         ).cpu()
-        self.model_inputs["kv_batch_ids"] = paddle.zeros_like(self.main_model_inputs["kv_batch_ids"])
-        self.model_inputs["kv_tile_ids_per_batch"] = paddle.zeros_like(self.main_model_inputs["kv_tile_ids_per_batch"])
+        self.model_inputs["kv_batch_ids"] = paddle.zeros_like(self.target_model_inputs["kv_batch_ids"])
+        self.model_inputs["kv_tile_ids_per_batch"] = paddle.zeros_like(
+            self.target_model_inputs["kv_tile_ids_per_batch"]
+        )
         self.model_inputs["kv_num_blocks_x_cpu"] = paddle.zeros_like(
-            self.main_model_inputs["kv_num_blocks_x_cpu"]
+            self.target_model_inputs["kv_num_blocks_x_cpu"]
         ).cpu()
-        self.model_inputs["max_len_kv_cpu"] = paddle.zeros_like(self.main_model_inputs["max_len_kv_cpu"]).cpu()
+        self.model_inputs["max_len_kv_cpu"] = paddle.zeros_like(self.target_model_inputs["max_len_kv_cpu"]).cpu()
 
         # Get the attention backend
         attn_cls = get_attention_backend()
