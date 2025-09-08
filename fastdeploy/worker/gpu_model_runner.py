ea9f49ef86a2cb921a611ea4af6fccceef6cff9c
gongshaotian <gstian5555@outlook.com>
2025-09-08 10:19:43 +0800

    profile run with max_num_batched_tokens

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -1637,7 +1637,7 @@ class GPUModelRunner(ModelRunnerBase):
 
         # 2. Dummy run
         self._dummy_run(
-            num_tokens=self.parallel_config.max_model_len,
+            num_tokens=self.parallel_config.max_num_batched_tokens,
             batch_size=self.parallel_config.max_num_seqs,
         )
 
