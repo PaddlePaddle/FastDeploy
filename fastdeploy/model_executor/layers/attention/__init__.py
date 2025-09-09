5b0e075db2fd31e0128de865b62607a280e45ed1
yangjianfengo1 <125249383+yangjianfengo1@users.noreply.github.com>
2025-09-09 10:56:50 +0800

    【FIX】Change the name of sparse attn from moba to plas (#3845)
    * 更新文档
    
    * 更新文档
    
    * 更新文档
    
    * 更新文档
    
    * 修改moba为plas
    
    * code style
    
    * update ci
    
    * code style
    
    * update ci

--- fastdeploy/model_executor/layers/attention/__init__.py
+++ fastdeploy/model_executor/layers/attention/__init__.py
@@ -20,7 +20,7 @@ from .block_multihead_attn_backend import BlockAttentionBackend
 from .flash_attn_backend import FlashAttentionBackend
 from .iluvatar_attn_backend import IluvatarAttnBackend
 from .mla_attention_backend import MLAAttentionBackend
-from .moba_attention_backend import MobaAttentionBackend
+from .moba_attention_backend import PlasAttentionBackend
 from .native_paddle_backend import PaddleNativeAttnBackend
 from .xpu_attn_backend import XPUAttentionBackend
 
@@ -35,5 +35,5 @@ __all__ = [
     "IluvatarAttnBackend",
     "BlockAttentionBackend",
     "Attention",
-    "MobaAttentionBackend",
+    "PlasAttentionBackend",
 ]
