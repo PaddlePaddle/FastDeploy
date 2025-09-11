f8db915d7f840e43d5418aae2a69ac0866375e3c
gongshaotian <gstian5555@outlook.com>
2025-09-11 17:47:44 +0800

    fix plas bug

--- tests/layers/test_plas_attention.py
+++ tests/layers/test_plas_attention.py
@@ -367,6 +367,7 @@ class TestPlasAttention(unittest.TestCase):
             enable_chunked_prefill=True,
             max_num_batched_tokens=8192,
             plas_attention_config=plas_attention_config,
+            graph_optimization_config=graph_optimization_config,
         )
 
         prompts = ["Hello world!"]
