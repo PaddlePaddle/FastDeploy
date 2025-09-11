4477886a2eca4138190e18541bfb829abce633e9
K11OntheBoat <ruianmaidanglao@163.com>
2025-09-11 11:48:40 +0000

    Support limit thinking lengths

--- fastdeploy/config.py
+++ fastdeploy/config.py
@@ -190,6 +190,7 @@ class ModelConfig:
         self.reasoning_parser = None
         self.pad_token_id: int = -1
         self.eos_tokens_lens: int = 2
+        self.think_end_id = None
         self.lm_head_fp32: bool = False
         self.model_format = "auto"
         self.runner = "auto"
