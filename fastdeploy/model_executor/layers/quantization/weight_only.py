f6b9b4da3befd68f283fe6835234747374eb0e79
Sunny-bot1 <592045536@qq.com>
2025-09-12 17:36:49 +0800

    fix

--- fastdeploy/model_executor/layers/quantization/weight_only.py
+++ fastdeploy/model_executor/layers/quantization/weight_only.py
@@ -146,7 +146,7 @@ class WeightOnlyConfig(QuantConfigBase):
                     and envs.FD_USE_MACHETE == "1"
                     and layer.weight_shape[1]
                     and layer.weight_shape[1] % 128 == 0
-                    and layer.bias is None
+                    and not layer.add_bias
                 ):
                     return MacheteWeightOnlyLinearMethod(self)
                 return GPUWeightOnlyLinearMethod(self)
