7296392affd9dd2e0cd66e64ace2cda0e6e2721d
chenjian <1435317881@qq.com>
2025-09-12 10:15:59 +0800

    [Optimize] optimize prefix cache in develop (#3890)
    * optimize prefix cache in release22
    
    * fix
    
    * fix
    
    * fix
    
    * add ci for v1
    
    * add unit test
    
    ---------
    
    Co-authored-by: xiegegege <46314656+xiegegege@users.noreply.github.com>

--- fastdeploy/engine/common_engine.py
+++ fastdeploy/engine/common_engine.py
@@ -525,10 +525,13 @@ class EngineService:
                 int(self.resource_manager.available_batch()),
                 self.cfg.max_prefill_batch,
             )
+            if self.cfg.model_config.enable_mm:
+                available_blocks = self.resource_manager.available_block_num()
+            else:
+                available_blocks = self.cfg.cache_config.max_block_num_per_seq
 
-            self.resource_manager.check_and_free_block_tables()
             tasks = self.scheduler.get_requests(
-                available_blocks=self.resource_manager.available_block_num(),
+                available_blocks=available_blocks,
                 block_size=self.cfg.cache_config.block_size,
                 reserved_output_blocks=self.cfg.cache_config.enc_dec_block_num,
                 max_num_batched_tokens=self.cfg.max_model_len,
