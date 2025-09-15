b77e43e87d8529563cf919f058934df196500fb0
nyx-c-language <nyxchaoji123@163.com>
2025-09-15 22:55:12 +0800

    add value check

--- tests/operators/test_tritonmoe_preprocess.py
+++ tests/operators/test_tritonmoe_preprocess.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 
 import numpy as np
@@ -12,7 +26,7 @@ class TestTritonMOEPreprocess(unittest.TestCase):
         np.random.seed(42)
 
     def _run_op(self, topk_ids_np, num_experts, GEMM_BLOCK_SIZE_M):
-        # Convert numpy to Paddle Tensor and run operator
+        """Convert numpy to Paddle Tensor and run operator"""
         topk_ids = paddle.to_tensor(topk_ids_np, dtype="int64")
         sorted_ids, expert_ids, num_tokens_post_pad = tritonmoe_preprocess(topk_ids, num_experts, GEMM_BLOCK_SIZE_M)
         return sorted_ids.numpy(), expert_ids.numpy(), num_tokens_post_pad.numpy()
@@ -20,6 +34,7 @@ class TestTritonMOEPreprocess(unittest.TestCase):
     def _check_output_shapes(
         self, sorted_ids, expert_ids, num_tokens_post_pad, topk_ids_np, num_experts, GEMM_BLOCK_SIZE_M
     ):
+        """Check output shapes and dtypes"""
         expected_max_num_tokens_padded = topk_ids_np.size + num_experts * (GEMM_BLOCK_SIZE_M - 1)
         self.assertEqual(sorted_ids.shape[0], expected_max_num_tokens_padded)
 
@@ -31,6 +46,52 @@ class TestTritonMOEPreprocess(unittest.TestCase):
         self.assertTrue(expert_ids.dtype == np.int32)
         self.assertTrue(num_tokens_post_pad.dtype == np.int32)
 
+    def _check_output_values_basic(self, sorted_ids, expert_ids, num_tokens_post_pad):
+        """Check expected values for the fixed example"""
+        expected_sorted_ids = np.array(
+            [
+                8,
+                12,
+                16,
+                16,
+                4,
+                9,
+                15,
+                16,
+                5,
+                10,
+                14,
+                16,
+                6,
+                11,
+                13,
+                16,
+                3,
+                7,
+                16,
+                16,
+                2,
+                16,
+                16,
+                16,
+                1,
+                16,
+                16,
+                16,
+                0,
+                16,
+                16,
+                16,
+            ],
+            dtype=np.int32,
+        )
+        np.testing.assert_array_equal(sorted_ids[: len(expected_sorted_ids)], expected_sorted_ids)
+
+        expected_expert_ids = np.array([0, 1, 2, 3, 4, 5, 6, 7], dtype=np.int32)
+        np.testing.assert_array_equal(expert_ids[: len(expected_expert_ids)], expected_expert_ids)
+
+        self.assertTrue(num_tokens_post_pad[0] % 4 == 0)
+
     def test_basic_case(self):
         """Basic fixed example test"""
         num_experts = 8
@@ -41,6 +102,7 @@ class TestTritonMOEPreprocess(unittest.TestCase):
         self._check_output_shapes(
             sorted_ids, expert_ids, num_tokens_post_pad, topk_ids_np, num_experts, GEMM_BLOCK_SIZE_M
         )
+        self._check_output_values_basic(sorted_ids, expert_ids, num_tokens_post_pad)
 
     def test_all_supported_num_experts(self):
         """Test all supported num_experts branches"""
@@ -59,7 +121,7 @@ class TestTritonMOEPreprocess(unittest.TestCase):
     def test_unsupported_num_experts(self):
         """Test unsupported num_experts raises OSError"""
         topk_ids_np = np.array([[0, 1], [1, 0]], dtype=np.int64)
-        unsupported_experts = [3, 9, 65, 129]  # unsupported values
+        unsupported_experts = [3, 9, 65, 129]
         GEMM_BLOCK_SIZE_M = 4
 
         for num_experts in unsupported_experts:
