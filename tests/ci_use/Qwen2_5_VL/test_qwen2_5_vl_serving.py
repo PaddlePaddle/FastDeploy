bd5ccace6720dd4b545d93637876760b0719734e
wangyafeng <wangyafeng@baidu.com>
2025-09-23 11:09:23 +0800

    [Model]qwen2.5VL support --use-cudagraph buffer and qwenvl test v2

--- tests/ci_use/Qwen2_5_VL/test_qwen2_5_vl_serving.py
+++ tests/ci_use/Qwen2_5_VL/test_qwen2_5_vl_serving.py
@@ -80,11 +80,7 @@ def setup_and_run_server():
     print("Pre-test port cleanup...")
     clean_ports()
 
-    base_path = os.getenv("MODEL_PATH")
-    if base_path:
-        model_path = os.path.join(base_path, "Qwen2.5-VL-7B-Instruct")
-    else:
-        model_path = "./Qwen2.5-VL-7B-Instruct"
+    model_path = "/ModelData/Qwen2.5-VL-7B-Instruct"
 
     log_path = "server.log"
     limit_mm_str = json.dumps({"image": 100, "video": 100})
