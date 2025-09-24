341c9d5daac0f6046c7d635b648d055bd7237d25
zhoutianzi666 <17801055074@163.com>
2025-09-24 14:29:50 +0800

    daima 好看

--- fastdeploy/model_executor/models/ernie4_5_moe.py
+++ fastdeploy/model_executor/models/ernie4_5_moe.py
@@ -945,11 +945,13 @@ class Ernie4_5_Model(nn.Layer):
         hidden_states = self.embed_tokens(ids_remove_padding=ids_remove_padding)
 
         residual = None
-        for i in range(self.num_layers):	
+        for i in range(self.num_layers):
             hidden_states, residual = self.layers[i](forward_meta, hidden_states, residual)
 
         hidden_states = hidden_states + residual
+
         out = self.norm(hidden_states)
+
         return out
 
 class Ernie4_5_MoeForCausalLM(ModelForCasualLM):
