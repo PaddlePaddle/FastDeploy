bdbb476070bd2a0ae95f95a9e8a8c525a1bcec15
gongshaotian <gstian5555@outlook.com>
2025-09-24 18:26:52 +0800

    1. delete check 2. fix plas attn test case

--- fastdeploy/config.py
+++ fastdeploy/config.py
@@ -1531,16 +1531,6 @@ class FDConfig:
                 self.guided_decoding_backend = "xgrammar"
 
         # Adjustment GraphOptConfig
-        if (
-            (self.speculative_config is not None and self.speculative_config.method is not None)
-            or (self.model_config is not None and self.model_config.enable_mm is True)
-            or (self.model_config is not None and self.load_config.dynamic_load_weight is True)
-            or (self.splitwise_role != "mixed")
-        ):
-            self.graph_opt_config.use_cudagraph = False
-            logger.info(
-                "CUDAGraph does not support to be started together with SpeculativeDecode and MultiModel temporarily, but has been automatically closed!"
-            )
         if self.model_config is not None and self.load_config.dynamic_load_weight is True:
             self.graph_opt_config.graph_opt_level = 0
             logger.info(
@@ -1549,6 +1539,7 @@ class FDConfig:
         if self.device_config is not None and self.device_config.device_type != "cuda":
             self.graph_opt_config.use_cudagraph = False
             logger.info(f"CUDAGraph only support on GPU, current device type is {self.device_config.device_type}!")
+
         if self.scheduler_config.splitwise_role == "mixed":
             self.model_config.moe_phase = MoEPhase(phase="prefill")
         elif self.scheduler_config.splitwise_role == "prefill":
@@ -1647,15 +1638,6 @@ class FDConfig:
             self.scheduler_config.check()
 
         # Check graph optimization config
-        if self.graph_opt_config.use_cudagraph:
-            if self.speculative_config is not None:
-                assert (
-                    self.speculative_config.method is None
-                ), "CUDAGraph does not support the simultaneous use of Speculative Decoding"
-            if self.model_config is not None:
-                assert (
-                    self.model_config.enable_mm is False
-                ), "CUDAGraph cannot be applied to multimodal model temporarily"
         if self.graph_opt_config.graph_opt_level > 0:
             if self.load_config is not None:
                 assert (
