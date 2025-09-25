58906ffe0d32bcd2416a07598220b7ffe7cad11d
zeroRains <linjunlu@zerorains.top>
2025-09-25 21:55:21 +0800

    update

--- scripts/check_approval.sh
+++ scripts/check_approval.sh
@@ -52,7 +52,7 @@ fi
 
 HAS_WORKER_OR_CONFIG_MODIFY=`git diff -U0 upstream/$BRANCH  --name-only | grep -E "fastdeploy/worker/|fastdeploy/config.py" || true`
 if [ ${HAS_WORKER_OR_CONFIG_MODIFY} ] && [ "${PR_ID}" != "" ]; then
-    echo_line1="You must have one FastDeploy RD (gongshaotian(gongshaotian), yuanlehome(liuyuanle) approval for modifing fastdeploy/config.py or fastdeploy/worker/* files.\n"
+    echo_line1="You must have one FastDeploy RD (gongshaotian(gongshaotian), yuanlehome(liuyuanle)) approval for modifing fastdeploy/config.py or fastdeploy/worker/* files.\n"
     check_approval "$echo_line1" 1 gongshaotian yuanlehome
 fi
 
