718aadce7f15349ef7b6a7ceacc8e62fd12da014
littledgg <1658565283@qq.com>
2025-09-25 22:21:36 +0800

    solve problem

--- fastdeploy/config.py
+++ fastdeploy/config.py
@@ -1148,8 +1148,8 @@ class FDConfig:
         if self.graph_opt_config.cudagraph_capture_sizes is None:
             self.graph_opt_config._set_cudagraph_sizes(max_num_seqs=self.parallel_config.max_num_seqs)
 
-        if self.speculative_config.method == "mtp":
-            max_shape = self.parallel_config.max_num_seqs * (self.speculative_config.num_model_steps + 1)
+        if self.speculative_config is not None and self.speculative_config.method == "mtp":
+            max_shape = self.parallel_config.max_num_seqs * (self.speculative_config.num_speculative_tokens + 1)
             if max_shape % 2 == 1:
                 max_shape = max_shape + 1
             self.graph_opt_config.init_with_cudagrpah_size(max_num_seqs=min(512, max_shape))
