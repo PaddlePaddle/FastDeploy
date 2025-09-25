a27df3d4ef1168905ad1fb293fc5faa6cf4707a9
ooooo <3164076421@qq.com>
2025-09-25 19:17:27 +0800

    test

--- tests/operators/test_cutlass_fp8_fp8_half_block_gemm_fused.py
+++ tests/operators/test_cutlass_fp8_fp8_half_block_gemm_fused.py
@@ -27,15 +27,15 @@ class TestFp8Fp8HalfBlockGemmFused(unittest.TestCase):
         paddle.seed(2025)
         self.prop = paddle.device.cuda.get_device_properties()
         self.sm_version = self.prop.major * 10 + self.prop.minor
-        print(f"sm version: {self.sm_version}")
+        print(f"Current SM Version: {self.sm_version}")
         self.E4M3_MAX_POS = 448.0
         os.environ["FLAGS_cuda_core_fp8_gemm"] = "1"
-        print(paddle.device.cuda.get_device_properties())
-        print(paddle.__git_commit__)
+        print(f"Device Properties: {paddle.device.cuda.get_device_properties()}")
+        print(f"Paddle Commit: {paddle.__git_commit__}")
 
     def test_block_gemm_case(self):
         if self.sm_version < 90:
-            self.skipTest("cutlass_fp8_fp8_half_block_gemm_fused only support sm90+")
+            self.skipTest("cutlass_fp8_fp8_half_block_gemm_fused only supports SM90+")
 
         nks = [
             [2048, 2048],
@@ -53,8 +53,11 @@ class TestFp8Fp8HalfBlockGemmFused(unittest.TestCase):
         combinations = product(m_values, nks)
 
         for m, (n, k) in combinations:
+            print(f"Testing with M={m}, N={n}, K={k}")
+
             x_bf16 = paddle.rand([m, k]).to(paddle.bfloat16)
             y_bf16 = paddle.rand([n, k]).to(paddle.bfloat16)
+            bias = paddle.rand([n]).to(paddle.bfloat16)
 
             x_scale = (
                 paddle.rand(
@@ -72,20 +75,24 @@ class TestFp8Fp8HalfBlockGemmFused(unittest.TestCase):
                 * 0.9
                 + 0.1
             )
-
-            x_scale_expanded = paddle.repeat_interleave(x_scale, repeats=128, axis=-2)[:k, :]
-            x_scale_expanded = x_scale_expanded.transpose([1, 0])
-            x_quant_fp32 = x_bf16.astype("float32") / x_scale_expanded
+            x_scale_expanded_for_quant = paddle.repeat_interleave(x_scale, repeats=128, axis=-2)[:k, :].transpose(
+                [1, 0]
+            )
+            x_quant_fp32 = x_bf16.astype("float32") / x_scale_expanded_for_quant
             x_fp8 = x_quant_fp32.clip(min=-self.E4M3_MAX_POS, max=self.E4M3_MAX_POS).astype("float8_e4m3fn")
 
             y_scale_n_dim = paddle.repeat_interleave(y_scale, repeats=128, axis=-2)
-            y_scale_expanded = paddle.repeat_interleave(y_scale_n_dim, repeats=128, axis=-1)
-            y_scale_expanded = y_scale_expanded[:n, :k]
-            y_quant_fp32 = y_bf16.astype("float32") / y_scale_expanded
+            y_scale_expanded_for_quant = paddle.repeat_interleave(y_scale_n_dim, repeats=128, axis=-1)[:n, :k]
+            y_quant_fp32 = y_bf16.astype("float32") / y_scale_expanded_for_quant
             y_fp8 = y_quant_fp32.clip(min=-self.E4M3_MAX_POS, max=self.E4M3_MAX_POS).astype("float8_e4m3fn")
 
-            ref_out = paddle.matmul(x_bf16, y_bf16, transpose_y=True)
-            bias = paddle.rand([n]).to(paddle.bfloat16)
+            x_dequant_bf16 = x_fp8.astype("bfloat16")
+            y_dequant_bf16 = y_fp8.astype("bfloat16")
+
+            x_ref_bf16 = x_dequant_bf16 * x_scale_expanded_for_quant
+            y_ref_bf16 = y_dequant_bf16 * y_scale_expanded_for_quant
+
+            ref_out = paddle.matmul(x_ref_bf16, y_ref_bf16, transpose_y=True)
 
             if bias is not None:
                 ref_out = ref_out + bias
@@ -108,6 +115,7 @@ class TestFp8Fp8HalfBlockGemmFused(unittest.TestCase):
                 rtol=5e-3,
                 atol=5e-3,
             )
+            print(f"M={m}, N={n}, K={k} Passed!")
 
 
 if __name__ == "__main__":
