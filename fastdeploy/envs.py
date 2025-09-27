6776ab887cb1041b8168ead6573ce2a848c753fd
ltd0924 <luotingdan@baidu.com>
2025-09-27 14:31:47 +0800

    [Feature] support fd decode response

--- fastdeploy/envs.py
+++ fastdeploy/envs.py
@@ -102,6 +102,8 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "FD_ZMQ_SEND_RESPONSE_SERVER_PORT": lambda: os.getenv("FD_ZMQ_SEND_RESPONSE_SERVER_PORT", "8201"),
     # LLMEngine recieve control command port, used when FD_ENABLE_INTERNAL_ADAPTER=1
     "FD_ZMQ_CONTROL_CMD_SERVER_PORTS": lambda: os.getenv("FD_ZMQ_CONTROL_CMD_SERVER_PORTS", "8202"),
+    # enable decode response
+    "FD_ENABLE_DECODE_RESPONSE": lambda: bool(int(os.getenv("FD_ENABLE_DECODE_RESPONSE", "0"))),
 }
 
 
