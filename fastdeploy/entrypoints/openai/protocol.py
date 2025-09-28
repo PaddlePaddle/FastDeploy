fd4ed68a5597b6cc1d383b24ebce9e42f62cebd5
Deleter-D <867909454@qq.com>
2025-09-28 11:18:44 +0800

    Revert "feat: add draft_logprobs for Speculative Decode MTP"
    This reverts commit d5a3c5c933ad0cbbae70cff6bbf9c317c0681ebc.

--- fastdeploy/entrypoints/openai/protocol.py
+++ fastdeploy/entrypoints/openai/protocol.py
@@ -405,7 +405,6 @@ class CompletionRequest(BaseModel):
     echo: Optional[bool] = False
     frequency_penalty: Optional[float] = None
     logprobs: Optional[int] = None
-    include_draft_logprobs: Optional[bool] = False
     # For logits and logprobs post processing
     temp_scaled_logprobs: bool = False
     top_p_normalized_logprobs: bool = False
@@ -541,7 +540,6 @@ class ChatCompletionRequest(BaseModel):
     frequency_penalty: Optional[float] = None
     logprobs: Optional[bool] = False
     top_logprobs: Optional[int] = 0
-    include_draft_logprobs: Optional[bool] = False
 
     # For logits and logprobs post processing
     temp_scaled_logprobs: bool = False
