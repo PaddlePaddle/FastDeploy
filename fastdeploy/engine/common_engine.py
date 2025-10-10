2a9e046f088a41e2b8d53c6bf2814e1f4a160395
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-10-10 22:59:16 +0800

    Update common_engine.py

--- fastdeploy/engine/common_engine.py
+++ fastdeploy/engine/common_engine.py
@@ -68,11 +68,9 @@ class EngineService:
             cfg (Config): Config object containing all the configuration parameters.
         """
         self.cfg = cfg
-
-        if self.cfg.cache_config.enable_prefix_caching:
-            self.cfg.cache_config.cache_queue_port = self.cfg.cache_config.cache_queue_port[
-                self.cfg.parallel_config.local_data_parallel_id
-            ]
+        self.cfg.cache_config.cache_queue_port = self.cfg.cache_config.cache_queue_port[
+            self.cfg.parallel_config.local_data_parallel_id
+        ]
 
         if self.cfg.parallel_config.enable_expert_parallel:
             self.llm_logger = get_logger(
