2ddec5d4c27aa809e7b6c24c917270059ab327b3
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-10-14 14:06:49 +0800

    Update token_processor.py

--- fastdeploy/output/token_processor.py
+++ fastdeploy/output/token_processor.py
@@ -396,7 +396,7 @@ class TokenProcessor:
             if self.tokens_counter[task_id] == 0:
                 if task.messages is not None:
                     result.prompt = task.messages
-                result.num_cached_tokens = task.num_cached_tokens
+            result.num_cached_tokens = task.num_cached_tokens
 
             is_prefill = task.disaggregate_info is not None and task.disaggregate_info["role"] == "prefill"
 
