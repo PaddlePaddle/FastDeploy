06b5441262b5698682cef42b14a46a87065be3fa
yuanlehome <yuanlehome@163.com>
2025-10-15 18:09:05 +0800

    fix

--- fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
+++ fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
@@ -255,7 +255,11 @@ class Ernie4_5_VLProcessor(Ernie4_5Processor):
         else:
             request["max_tokens"] = min(max_model_len - len(request["prompt_token_ids"]), request["max_tokens"])
         if request.get("reasoning_max_tokens") is None:
-            request["reasoning_max_tokens"] = max(int(request["max_tokens"] * 0.8), 1)
+            if request.get("enable_thinking"):
+                request["reasoning_max_tokens"] = max(int(request["max_tokens"] * 0.8), 1)
+        else:
+            if not request.get("enable_thinking"):
+                request["reasoning_max_tokens"] = None
         data_processor_logger.info(f"Processed request {request}")
 
         return request
