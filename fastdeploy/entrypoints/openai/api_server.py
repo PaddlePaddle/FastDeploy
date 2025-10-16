0053f1765ab5c2b5279c1b2e6cfff8d9016cdf34
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-10-16 19:43:43 +0800

    Update api_server.py

--- fastdeploy/entrypoints/openai/api_server.py
+++ fastdeploy/entrypoints/openai/api_server.py
@@ -107,21 +107,6 @@ class StandaloneApplication(BaseApplication):
         return self.application
 
 
-class StandaloneApplication(BaseApplication):
-    def __init__(self, app, options=None):
-        self.application = app
-        self.options = options or {}
-        super().__init__()
-
-    def load_config(self):
-        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 def load_engine():
     """
     load engine
