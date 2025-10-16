3f3b99ef3d048df78ad0d6f3b8f73e494f4eb1a8
lizexu <2694294196@qq.com>
2025-10-16 17:00:21 +0800

    fix

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -374,7 +374,7 @@ class GPUModelRunner(ModelRunnerBase):
                 self.share_inputs["input_ids"][idx : idx + 1, :length] = np.array(
                     input_ids[prefill_start_index:prefill_end_index]
                 )
-                # print("self.share_inputs['input_ids']", self.share_inputs["input_ids"])
+                print("self.share_inputs['input_ids']", self.share_inputs["input_ids"])
                 encoder_block_num = len(request.block_tables)
                 self.share_inputs["encoder_block_lens"][idx : idx + 1] = encoder_block_num
                 self.share_inputs["block_tables"][idx : idx + 1, :] = -1
