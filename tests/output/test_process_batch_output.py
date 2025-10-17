adad075d324fd1e20c37db1ac9ba75956694dadc
sunlei1024 <sunlei5788@gmail.com>
2025-10-17 10:58:24 +0000

    test: test_speculative_decoding_use_logprobs

--- tests/output/test_process_batch_output.py
+++ tests/output/test_process_batch_output.py
@@ -1,9 +1,11 @@
+import random
 import time
 import unittest
 from unittest.mock import Mock
 
 import paddle
 
+from fastdeploy.engine.request import RequestOutput
 from fastdeploy.output.token_processor import TokenProcessor
 
 paddle.set_device("cpu")
@@ -67,6 +69,14 @@ class MockResourceManager:
         pass
 
 
+class MockCachedGeneratedTokens:
+    def __init__(self):
+        self.cache = []
+
+    def put_results(self, results):
+        self.cache.extend(results)
+
+
 # Constants
 RECOVERY_STOP_SIGNAL = -3
 MAX_BSZ = 512
@@ -76,7 +86,6 @@ SPECULATE_MAX_BSZ = 256
 
 
 class TestTokenProcessorProcessBatchOutput(unittest.TestCase):
-
     def setup_token_processor(self, speculative_decoding=False, use_logprobs=False):
         """Helper method to setup TokenProcessor with different configurations"""
         cfg = MockConfig()
@@ -86,14 +95,16 @@ class TestTokenProcessorProcessBatchOutput(unittest.TestCase):
 
         processor = TokenProcessor.__new__(TokenProcessor)
         processor.cfg = cfg
-        processor.cached_generated_tokens = []
+        processor.cached_generated_tokens: MockCachedGeneratedTokens = MockCachedGeneratedTokens()
         processor.executor = Mock()
         processor.engine_worker_queue = Mock()
         processor.split_connector = Mock()
         processor.resource_manager = MockResourceManager()
-        task = MockTask()
-        processor.resource_manager.tasks_list = [task]
-        processor.tokens_counter = {task.request_id: 0}
+        task1 = MockTask()
+        task2 = MockTask()
+        processor.resource_manager.tasks_list = [task1, task2]
+        processor.resource_manager.stop_flags = [False, False]
+        processor.tokens_counter = {task1.request_id: 0, task2.request_id: 0}
         processor.total_step = 0
         processor.number_of_output_tokens = 0
         processor.prefill_result_status = {}
@@ -138,7 +149,7 @@ class TestTokenProcessorProcessBatchOutput(unittest.TestCase):
         elif use_logprobs:
             processor.output_tokens = paddle.full(shape=[MAX_BSZ * (K + 1) + 2, 1], fill_value=2, dtype="int64")
             processor.output_scores = paddle.full(shape=[MAX_BSZ * (K + 1), 1], fill_value=0.0, dtype="float32")
-            processor.output_ranks  = paddle.full(shape=[MAX_BSZ], fill_value=0, dtype="int64")
+            processor.output_ranks = paddle.full(shape=[MAX_BSZ], fill_value=0, dtype="int64")
         else:
             processor.output_tokens = paddle.full(shape=[MAX_BSZ + 2, 1], fill_value=2, dtype="int64")
 
@@ -149,42 +160,57 @@ class TestTokenProcessorProcessBatchOutput(unittest.TestCase):
         processor = self.setup_token_processor(speculative_decoding=True, use_logprobs=True)
 
         # stop_flag
-        processor.output_tokens[0, 0] = 2
-        # mtype
-        processor.output_tokens[1, 0] = 3 # target = 3, decode = 4
-        # batch     
-        processor.output_tokens[2, 0] = 2 
+        processor.output_tokens[0, 0].set_tensor(paddle.to_tensor(2))
+        # mtype target = 3, decode = 4
+        processor.output_tokens[1, 0].set_tensor(paddle.to_tensor(3))
+        # batch
+        processor.output_tokens[2, 0].set_tensor(paddle.to_tensor(2))
         # accept_num
-        processor.output_tokens[3, 0] = 3
-        processor.output_tokens[4, 0] = 3
+        processor.output_tokens[3, 0].set_tensor(paddle.to_tensor(3))
+        processor.output_tokens[4, 0].set_tensor(paddle.to_tensor(3))
 
         batch = processor.output_tokens[2, 0]
+        mtype = processor.output_tokens[3, 0]
         accept_num = [int(num[0]) for num in processor.output_tokens[3 : batch + 3]]
 
         # init
-        print(f"\nbatch: {batch}, accept_num: {accept_num}")
+        print(f"batch:{batch}, mtype:{mtype} accept_num: {accept_num}")
         for i in range(batch):
             for j in range(accept_num[i]):
+                token_index = 3 + MAX_BSZ + i * MAX_DRAFT_TOKENS * (K + 1) + j * (K + 1)
+                score_index = i * MAX_DRAFT_TOKENS * (K + 1) + j * (K + 1)
+                print(f"batch:{i}, accept:{j} token_index: {token_index} score_index: {score_index}")
                 for k in range(K + 1):
-                    index = (
-                        3
-                        + batch
-                        + i * MAX_DRAFT_TOKENS * (K + 1)
-                        + j * (K + 1)
-                        + k
-                    )
-                    print(f"i:{i}, j:{j} k:{k} index: {index}")
-                    processor.output_tokens[index, 0] = 5 + i * 10 + j * 2 + k
-                    processor.output_scores[i * MAX_DRAFT_TOKENS * (K + 1) + j * (K + 1) + k, 0] = float(
-                        0.1 * (5 + i * 10 + j * 2 + k)
-                    )
-                processor.output_ranks[i * MAX_DRAFT_TOKENS + j] = j + 1
+                    processor.output_tokens[token_index + k].set_tensor(paddle.to_tensor(random.randint(100, 100000)))
+                    processor.output_scores[score_index + k].set_tensor(paddle.to_tensor(random.random()))
+                processor.output_ranks[j].set_tensor(paddle.to_tensor(1))
 
-        print(f"{processor.output_tokens}")
-        print(f"{processor.output_scores}")
-        print(f"{processor.output_ranks}")
+        processor._process_batch_output()
 
-        # processor._process_batch_output()
+        batch_result_buffer: list[RequestOutput] = processor._batch_result_buffer
+
+        for i, request_output in enumerate(batch_result_buffer):
+            assert isinstance(request_output, RequestOutput)
+            assert len(request_output.outputs.token_ids) == accept_num[i]
+            assert len(request_output.outputs.top_logprobs) == 3
+            # tokens, scores, ranks
+            assert len(request_output.outputs.top_logprobs[0][0]) == K + 1
+            assert len(request_output.outputs.top_logprobs[1][0]) == K + 1
+            assert len(request_output.outputs.top_logprobs[2]) == accept_num[i]
+
+        # mtype = 4
+        processor.output_tokens[1, 0].set_tensor(paddle.to_tensor(4))
+        processor._process_batch_output()
+        cached_generated_tokens: MockCachedGeneratedTokens = processor.cached_generated_tokens
+        for c in cached_generated_tokens.cache:
+            assert isinstance(request_output, RequestOutput)
+            assert len(request_output.outputs.token_ids) == accept_num[i]
+            assert len(request_output.outputs.top_logprobs) == 3
+            assert len(request_output.outputs.draft_top_logprobs) == 3
+            # tokens, scores, ranks
+            assert len(request_output.outputs.draft_top_logprobs[0][0]) == K + 1
+            assert len(request_output.outputs.draft_top_logprobs[1][0]) == K + 1
+            assert len(request_output.outputs.draft_top_logprobs[2]) == accept_num[i]
 
 
 if __name__ == "__main__":
