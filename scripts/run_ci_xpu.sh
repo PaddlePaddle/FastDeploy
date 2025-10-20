3741dff120675891dc8eb99e88fc622433e68570
yyssys <atyangshuang@foxmail.com>
2025-10-20 10:56:52 +0000

    [XPU]add xpu ci w4a8 case

--- scripts/run_ci_xpu.sh
+++ scripts/run_ci_xpu.sh
@@ -178,6 +178,7 @@ if [ ${kv_block_test_exit_code} -ne 0 ]; then
     exit 1
 fi
 
+
 echo "============================开始EP并行测试!============================"
 sleep 5
 rm -rf log/*
@@ -205,5 +206,142 @@ if [ ${ep_exit_code} -ne 0 ]; then
     echo "log/workerlog.0"
     cat log/workerlog.0
     echo "EP并行 相关测试失败，请检查pr代码"
+    exit 1
+fi
+
+sleep 5
+
+# 起服务
+rm -rf log/*
+rm -f core*
+# pkill -9 python #流水线不执行这个
+#清空消息队列
+ipcrm --all=msg
+echo "============================开始W4A8 V0模式测试!============================"
+export ENABLE_V1_KVCACHE_SCHEDULER=0
+export XPU_VISIBLE_DEVICES="0,1,2,3"
+python -m fastdeploy.entrypoints.openai.api_server \
+    --model ${MODEL_PATH}/ERNIE-4.5-300B-A47B-W4A8C8-TP4-Paddle \
+    --port 8188 \
+    --tensor-parallel-size 4 \
+    --num-gpu-blocks-override 16384 \
+    --max-model-len 32768 \
+    --max-num-seqs 64 \
+    --quantization "W4A8"   > server.log 2>&1 &
+
+sleep 60
+# 探活
+TIMEOUT=$((15 * 60))
+INTERVAL=10            # 检查间隔（秒）
+ENDPOINT="http://0.0.0.0:8188/health"
+START_TIME=$(date +%s) # 记录开始时间戳
+echo "开始服务健康检查，最长等待时间：${TIMEOUT}秒"
+while true; do
+    # 计算已耗时
+    CURRENT_TIME=$(date +%s)
+    ELAPSED=$((CURRENT_TIME - START_TIME))
+
+    # 超时判断
+    if [ $ELAPSED -ge $TIMEOUT ]; then
+        echo -e "\n服务启动超时：经过 $((TIMEOUT/60)) 分钟服务仍未启动！"
+        cat server.log
+        cat log/workerlog.0
+        exit 1
+    fi
+
+    HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" -m 2 "$ENDPOINT" || true)
+
+    if [ "$HTTP_CODE" = "200" ]; then
+        echo -e "\n服务启动成功！耗时 ${ELAPSED} 秒"
+        break
+    else
+        sleep $INTERVAL
+    fi
+done
+
+cat server.log
+
+# 执行服务化推理
+python -m pytest tests/ci_use/XPU_45T/test_w4a8.py
+w4a8_test_exit_code=$?
+echo w4a8_test_exit_code is ${w4a8_test_exit_code}
+
+ps -efww | grep -E 'api_server' | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+ps -efww | grep -E '8188' | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+lsof -t -i :8188 | xargs kill -9 || true
+
+if [ ${w4a8_test_exit_code} -ne 0 ]; then
+    echo "log/workerlog.0"
+    cat log/workerlog.0
+    echo "w4a8 V0模式测试失败，请检查pr代码"
+    exit 1
+fi
+
+sleep 5
+
+# 起服务
+rm -rf log/*
+rm -f core*
+# pkill -9 python #流水线不执行这个
+#清空消息队列
+ipcrm --all=msg
+echo "============================开始W4A8 V1模式测试!============================"
+export ENABLE_V1_KVCACHE_SCHEDULER=1
+export XPU_VISIBLE_DEVICES="0,1,2,3"
+python -m fastdeploy.entrypoints.openai.api_server \
+    --model ${MODEL_PATH}/ERNIE-4.5-300B-A47B-W4A8C8-TP4-Paddle \
+    --port 8188 \
+    --tensor-parallel-size 4 \
+    --num-gpu-blocks-override 16384 \
+    --max-model-len 32768 \
+    --max-num-seqs 64 \
+    --quantization "W4A8"   > server.log 2>&1 &
+
+sleep 60
+# 探活
+TIMEOUT=$((15 * 60))
+INTERVAL=10            # 检查间隔（秒）
+ENDPOINT="http://0.0.0.0:8188/health"
+START_TIME=$(date +%s) # 记录开始时间戳
+echo "开始服务健康检查，最长等待时间：${TIMEOUT}秒"
+while true; do
+    # 计算已耗时
+    CURRENT_TIME=$(date +%s)
+    ELAPSED=$((CURRENT_TIME - START_TIME))
+
+    # 超时判断
+    if [ $ELAPSED -ge $TIMEOUT ]; then
+        echo -e "\n服务启动超时：经过 $((TIMEOUT/60)) 分钟服务仍未启动！"
+        cat server.log
+        cat log/workerlog.0
+        exit 1
+    fi
+
+    HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" -m 2 "$ENDPOINT" || true)
+
+    if [ "$HTTP_CODE" = "200" ]; then
+        echo -e "\n服务启动成功！耗时 ${ELAPSED} 秒"
+        break
+    else
+        sleep $INTERVAL
+    fi
+done
+
+cat server.log
+
+# 执行服务化推理
+python -m pytest tests/ci_use/XPU_45T/test_w4a8.py
+w4a8_test_exit_code=$?
+echo w4a8_test_exit_code is ${w4a8_test_exit_code}
+
+unset ENABLE_V1_KVCACHE_SCHEDULER
+ps -efww | grep -E 'api_server' | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+ps -efww | grep -E '8188' | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+lsof -t -i :8188 | xargs kill -9 || true
+
+if [ ${w4a8_test_exit_code} -ne 0 ]; then
+    echo "log/workerlog.0"
+    cat log/workerlog.0
+    echo "w4a8 V1模式测试失败，请检查pr代码"
     exit 1
 fi
