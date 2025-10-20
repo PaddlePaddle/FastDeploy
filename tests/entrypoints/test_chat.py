334d29aea3a7026505eb3c6220ba872f3abcf813
ltd0924 <32387785+ltd0924@users.noreply.github.com>
2025-10-20 23:20:45 +0800

    Update test_chat.py

--- tests/entrypoints/test_chat.py
+++ tests/entrypoints/test_chat.py
@@ -60,7 +60,7 @@ class TestChat(unittest.TestCase):
     def test_chat(self):
         outputs = self.llm.chat(messages=self.PROMPTS, sampling_params=None)
         self.assertEqual(len(self.PROMPTS), len(outputs))
-        self.assertEqual(outputs[-1].num_cached_tokens == outputs[-2].num_cached_tokens)
+        self.assertEqual(outputs[-1].num_cached_tokens, outputs[-2].num_cached_tokens)
         self.assertEqual(outputs[-1].num_cached_token, 64)
 
     def test_chat_with_tools(self):
