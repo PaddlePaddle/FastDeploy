d0458ecbaf79451b8b1334666557c15f182421d9
kevin <chengyf112@gmail.com>
2025-10-20 19:54:42 +0800

    update pre-commit

--- fastdeploy/input/ernie4_5_vl_processor/process.py
+++ fastdeploy/input/ernie4_5_vl_processor/process.py
@@ -587,4 +587,3 @@ class DataProcessor:
         req = pickle.dumps((mm_hashes, mm_items))
         socket.send_multipart([b"", req])
         data_processor_logger.info(f"Update cache of mm_hashes: {mm_hashes}")
-
