193bc9481acef139f66a46617aba126bd084a3cd
ApplEOFDiscord <wwy640130@163.com>
2025-10-22 18:45:29 +0800

    fix unit test

--- tests/input/test_qwen_vl_processor.py
+++ tests/input/test_qwen_vl_processor.py
@@ -101,7 +101,8 @@ class TestQwenVLProcessor(unittest.TestCase):
         self.patcher_parse_video.start()
 
         self.patcher_read_frames = patch(
-            "fastdeploy.input.qwen_vl_processor.process.read_frames", return_value=mock_read_frames(480, 640, 5, 2)
+            "fastdeploy.input.qwen_vl_processor.process.DataProcessor._load_and_process_video",
+            return_value=mock_read_frames(480, 640, 5, 2),
         )
         self.patcher_read_frames.start()
 
