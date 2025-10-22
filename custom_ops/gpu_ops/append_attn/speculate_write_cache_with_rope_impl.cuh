5a83fd276cc15308b28d33088db4963b386fcecc
freeliuzc <lzc842650834@gmail.com>
2025-10-22 22:53:52 +0800

    support mtp many features

--- custom_ops/gpu_ops/append_attn/speculate_write_cache_with_rope_impl.cuh
+++ custom_ops/gpu_ops/append_attn/speculate_write_cache_with_rope_impl.cuh
@@ -72,6 +72,7 @@ __global__ void append_speculate_cache_T_rope_qk_norm_kernel(
     int64_t linear_index = global_hi * head_size + threadIdx.x * VecSize;
     const int token_id = linear_index / hidden_size;
     const int ori_bi = batch_id_per_token[token_id];
+    if (ori_bi == -1) continue;
     if (seq_lens_decoder[ori_bi] == 0) continue;
     const int bias = linear_index % hidden_size;
     const int hi = bias / head_size;  // q + k + v
@@ -627,6 +628,7 @@ __global__ void append_speculate_cache_fp8_rope_qk_norm_dynamic_kernel(
   const int token_id = blockIdx.x;
 
   const int bid = batch_id_per_token[token_id];
+  if (bid == -1) return; // NOTE(gongshaotian): For CUDAGraph padding
 
   const int start_token_idx = cu_seqlens_q[bid];
   const int head_idx = blockIdx.y * NUM_WARPS + wid;
