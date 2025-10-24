60ae0bd3c2845979fd5e1a399b338cd5307216e6
kxz2002 <kongxiangzhe@baidu.com>
2025-10-24 14:18:31 +0800

    modify test_logprobs

--- tests/ce/server/test_logprobs.py
+++ tests/ce/server/test_logprobs.py
@@ -69,6 +69,7 @@ def test_unstream_without_logprobs():
         "total_tokens": 25,
         "completion_tokens": 3,
         "prompt_tokens_details": {"cached_tokens": 0},
+        "completion_tokens_details": {"reasoning_tokens": 0},
     }
 
 
