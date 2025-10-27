1bf5789192d281756a6a5332d2dd134dbdee1e98
kevin <chengyf112@gmail.com>
2025-10-27 21:36:48 +0800

    update v1 prefill batch

--- fastdeploy/worker/worker_process.py
+++ fastdeploy/worker/worker_process.py
@@ -283,10 +283,7 @@ class PaddleDisWorkerProc:
             # The first worker detects whether there are tasks in the task queue
             if local_rank == 0:
                 if self.task_queue.num_tasks() > 0:
-                    # VL only support 1 batch to prefill
-                    if envs.ENABLE_V1_KVCACHE_SCHEDULER or not (
-                        self.fd_config.model_config.enable_mm and self.worker.exist_prefill()
-                    ):
+                    if envs.ENABLE_V1_KVCACHE_SCHEDULER:
                         if self.nnode > 1 and self.parallel_config.tensor_parallel_size > self.max_chips_per_node:
                             self.task_queue.read_finish_flag.set(1)
                         else:
