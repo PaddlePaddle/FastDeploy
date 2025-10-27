443cb1a73663bc02b5960bed8a5106c08557a4d7
Zhang Yulong <35552275+ZhangYulongg@users.noreply.github.com>
2025-10-27 16:30:57 +0800

    Log errors when exit code is 1 or 124
    Added error logging for specific exit codes.

--- scripts/run_pre_ce.sh
+++ scripts/run_pre_ce.sh
@@ -45,6 +45,7 @@ for subdir in "$run_path"*/; do
                     fi
 
                     if [ "$exit_code" -eq 1 ] || [ "$exit_code" -eq 124 ]; then
+                        grep -r "ERROR" ./log
                         echo "[ERROR] $file 起服务或执行异常，exit_code=$exit_code"
                         if [ "$exit_code" -eq 124 ]; then
                             echo "[TIMEOUT] $file 脚本执行超过 6 分钟, 任务超时退出！"
