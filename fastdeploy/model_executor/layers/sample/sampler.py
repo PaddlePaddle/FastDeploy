98ede23d02b322405e8e1c9b64f9a0646279846e
gongshaotian <gstian5555@outlook.com>
2025-10-27 10:40:02 +0800

    1.fix the bug of draft model with ep 2.fix sampler bug

--- fastdeploy/model_executor/layers/sample/sampler.py
+++ fastdeploy/model_executor/layers/sample/sampler.py
@@ -556,6 +556,7 @@ class SpeculativeSampler(nn.Layer):
         max_model_len: int,
         share_inputs: List[paddle.Tensor],
         accept_all_drafts: bool = False,
+        reject_all_drafts: bool = False,
     ) -> paddle.Tensor:
         """ """
 
@@ -611,7 +612,7 @@ class SpeculativeSampler(nn.Layer):
             max_model_len,
             self.speculative_verify_window,
             True,  # enable_topp
-            self.speculative_benchmark_mode,
+            (self.speculative_benchmark_mode or reject_all_drafts),
             accept_all_drafts,
         )
 
