12575c6a94b263ed253f15964fa0033b14b53ded
kevin <chengyf112@gmail.com>
2025-10-29 22:16:20 +0800

    update code

--- fastdeploy/engine/sched/resource_manager_v1.py
+++ fastdeploy/engine/sched/resource_manager_v1.py
@@ -357,7 +357,7 @@ class ResourceManagerV1(ResourceManager):
         num_new_tokens = min(num_new_tokens, token_budget)
         request.with_image = False
 
-        if not self._is_mm_request(request):
+        if not self.config.model_config.enable_mm:
             return num_new_tokens
 
         request.with_image = False
