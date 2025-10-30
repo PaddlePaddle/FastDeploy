49e79e26d91459456acc98ee4012f5cfdfac0365
yangjianfeng01 <yangjianfeng01@baidu.com>
2025-10-30 20:16:07 +0800

    修复视频和图片分chunk

--- fastdeploy/engine/sched/resource_manager_v1.py
+++ fastdeploy/engine/sched/resource_manager_v1.py
@@ -28,9 +28,10 @@ import paddle
 
 from fastdeploy.engine.request import Request, RequestStatus, RequestType
 from fastdeploy.engine.resource_manager import ResourceManager
-from fastdeploy.input.utils import IDS_TYPE_FLAG
 from fastdeploy.metrics.metrics import main_process_metrics
 from fastdeploy.utils import llm_logger
+import os
+from fastdeploy.input.utils import IDS_TYPE_FLAG
 
 
 @dataclass
@@ -153,23 +154,6 @@ class ResourceManagerV1(ResourceManager):
 
             prompt_token_ids_len = len(request.prompt_token_ids)
 
-            if new_end_idx >= prompt_token_ids_len:
-                return num_new_tokens
-
-            if inputs.get("can_split_idx_list") is not None:
-                if new_end_idx >= prompt_token_ids_len:
-                    return num_new_tokens
-                patch_idx = inputs["patch_idx"][new_end_idx]
-                patch_map = inputs["patch_map"][patch_idx]
-                modal_id = patch_map["modal_id"]
-                if modal_id == IDS_TYPE_FLAG["text"]:
-                    return num_new_tokens
-                elif modal_id == IDS_TYPE_FLAG["video"]:
-                    can_split_idx_list = inputs["can_split_idx_list"]
-                    for i in range(len(can_split_idx_list)):
-                        if can_split_idx_list[i] >= new_end_idx:
-                            return can_split_idx_list[i] - pre_end_idx
-
             assert prompt_token_ids_len == len(inputs["patch_idx"]), (prompt_token_ids_len, len(inputs["patch_idx"]))
 
             # start
@@ -197,6 +181,14 @@ class ResourceManagerV1(ResourceManager):
             end_modal_id = end_patch_map["modal_id"]
             if end_modal_id > 0:
                 new_end_idx = end_patch_map["end_idx"]  # 当前模态结束位置
+            
+            if end_modal_id == IDS_TYPE_FLAG["video"]:
+                can_split_idx_list = inputs["can_split_idx_list"]
+                for i in range(len(can_split_idx_list)):
+                    if can_split_idx_list[i] >= new_end_idx:
+                        new_end_idx = can_split_idx_list[i]
+                        break
+
             num_new_tokens = new_end_idx - pre_end_idx
 
             request.image_end = end_patch_map["image_num"]
