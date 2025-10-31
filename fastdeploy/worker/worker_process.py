db609c64e4aa18d78a0a9dfe1c3963aaf49803c4
iosmers <yinwei_hust@163.com>
2025-10-31 12:05:57 +0000

    update

--- fastdeploy/worker/worker_process.py
+++ fastdeploy/worker/worker_process.py
@@ -823,6 +823,7 @@ def initialize_fd_config(args, ranks: int = 1, local_rank: int = 0) -> FDConfig:
     update_fd_config_for_mm(fd_config)
     if fd_config.load_config.load_choices == "default_v1" and not v1_loader_support(fd_config):
         fd_config.load_config.load_choices = "default"
+    print(f"fd_config.load_config.load_choices: {fd_config.load_config.load_choices}")
 
     architecture = fd_config.model_config.architectures[0]
     if "PaddleOCR" in architecture:
