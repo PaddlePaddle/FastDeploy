03fcbc47da4281286448262d5a93839e7055f117
plusNew001 <95567040+plusNew001@users.noreply.github.com>
2025-10-31 11:32:38 +0800

    [XPU] [CI] Lock xvllm version (#4715)
    * Modify XVLLM_PATH assignment in run_ci_xpu.sh
    
    Update XVLLM_PATH to point to the newly downloaded xvllm directory.
    
    * Lock XVLLM version in CI script
    
    Lock XVLLM version to avoid CI issues due to updates.
    
    * Change xvllm output download link to latest version
    
    Updated the download link for xvllm output to the latest version.

--- scripts/run_ci_xpu.sh
+++ scripts/run_ci_xpu.sh
@@ -25,7 +25,10 @@ python -m pip install paddlepaddle-xpu -i https://www.paddlepaddle.org.cn/packag
 echo "build whl"
 bash custom_ops/xpu_ops/download_dependencies.sh develop
 export CLANG_PATH=$(pwd)/custom_ops/xpu_ops/third_party/xtdk
-export XVLLM_PATH=$(pwd)/custom_ops/xpu_ops/third_party/xvllm
+# export XVLLM_PATH=$(pwd)/custom_ops/xpu_ops/third_party/xvllm
+# 由于xvllm更新，为了避免影响CI，暂时锁死版本
+wget https://klx-sdk-release-public.su.bcebos.com/xinfer/daily/eb/20251029/output.tar.gz --no-proxy && tar xf output.tar.gz && mv output xvllm
+export XVLLM_PATH=${PWD}/xvllm
 bash build.sh || exit 1
 
 echo "pip others"
