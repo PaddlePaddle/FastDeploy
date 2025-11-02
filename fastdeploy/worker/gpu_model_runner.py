76fc363e3aa7909fa5648542e48f00cf761f68eb
zhoutianzi666 <17801055074@163.com>
2025-11-02 16:57:44 +0800

    useless code

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -2289,9 +2289,6 @@ class GPUModelRunner(ModelRunnerBase):
                     self.speculative_config.num_speculative_tokens,
                 )
 
-            self.seq_lens_this_time_buffer[:num_running_requests].copy_(
-                self.share_inputs["seq_lens_this_time"][:num_running_requests], False
-            )
             return None
 
     def _pool(self, hidden_states: paddle.Tensor, num_running_requests: int) -> Optional[ModelRunnerOutput]:
