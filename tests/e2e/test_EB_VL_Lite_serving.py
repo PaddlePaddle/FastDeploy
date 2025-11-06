d36479914b0f7dd41400e1d838cf78e93720cb24
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2025-11-06 22:02:10 +0800

    [CI] Update ERNIE-4.5-VL baseline to adapt to MoE changes (#4867)

--- tests/e2e/test_EB_VL_Lite_serving.py
+++ tests/e2e/test_EB_VL_Lite_serving.py
@@ -204,9 +204,9 @@ def test_consistency_between_runs(api_url, headers, consistent_payload):
     # base result
     base_path = os.getenv("MODEL_PATH")
     if base_path:
-        base_file = os.path.join(base_path, "ernie-4_5-vl-base-tp2-fp32")
+        base_file = os.path.join(base_path, "ernie-4_5-vl-base-tp2-dev")
     else:
-        base_file = "ernie-4_5-vl-base-tp2-fp32"
+        base_file = "ernie-4_5-vl-base-tp2-dev"
     with open(base_file, "r") as f:
         content2 = f.read()
 
