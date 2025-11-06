50eb347f88a448c256c9ad6ce727e709b1ef72ff
zhoutianzi666 <17801055074@163.com>
2025-11-06 15:00:30 +0800

    commit

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -2099,8 +2099,9 @@ class GPUModelRunner(ModelRunnerBase):
             return model_output
 
         p = Thread(target=haha)
-        p.start()
-        p.join()
+        print(p)
+        # p.start()
+        # p.join()
         model_output = haha()
 
         model_output = model_output[0]
