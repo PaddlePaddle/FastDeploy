97b44a0b890d2ee421e50916a80e041a1b06bfea
plusNew001 <95567040+plusNew001@users.noreply.github.com>
2025-11-06 20:16:31 +0800

    Update run_w4a8.py

--- tests/ci_use/XPU_45T/run_w4a8.py
+++ tests/ci_use/XPU_45T/run_w4a8.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 
 import openai
-import os
+
 
 def test_w4a8():
     ip = "0.0.0.0"
