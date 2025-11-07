d13504c7711e0add7f5effa54bbd7909c7e7a0ea
zhupengyang <1165938320@qq.com>
2025-11-07 16:37:23 +0800

    [XPU] fix ep_tp all2all ci (#4876)

--- scripts/run_ci_xpu.sh
+++ scripts/run_ci_xpu.sh
@@ -352,7 +352,7 @@ export BKCL_RDMA_VERBS=1
 
 export enable_expert_parallel=1
 export enable_tensor_parallel=1
-export EP_TP_SPLIT_MODE=1
+export FD_EP_TP_STRATEGY=all_to_all
 
 python -m pytest -s --timeout=600 tests/ci_use/XPU_45T/run_ep.py
 ep_exit_code=$?
@@ -366,7 +366,7 @@ unset XSHMEM_QP_NUM_PER_RANK
 unset BKCL_RDMA_VERBS
 unset enable_expert_parallel
 unset enable_tensor_parallel
-unset EP_TP_SPLIT_MODE
+unset FD_EP_TP_STRATEGY
 stop_processes
 
 if [ ${ep_exit_code} -ne 0 ]; then
