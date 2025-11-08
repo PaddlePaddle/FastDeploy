6dfa202de17be8b5705b52b9dc02e2fe0cbe0cff
zhoutianzi666 <17801055074@163.com>
2025-11-09 00:25:42 +0800

    commit

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -2081,31 +2081,31 @@ class GPUModelRunner(ModelRunnerBase):
         # 2. Padding inputs for cuda graph
         self.padding_cudagraph_inputs()
 
-        model_output = None
+        model_output = [None]
 
         def haha():
 
             # 3. Run model
             if self.enable_mm:
-                model_output = self.model(
+                model_output[0] = self.model(
                     self.share_inputs["ids_remove_padding"],
                     self.share_inputs["image_features"],
                     self.forward_meta,
                 )
             else:
-                model_output = self.model(
+                model_output[0] = self.model(
                     ids_remove_padding=self.share_inputs["ids_remove_padding"],
                     forward_meta=self.forward_meta,
                 )
 
-            return model_output
+            return model_output[0]
 
         self.p = Thread(target=haha)
-        # p.start()
-        # p.join()
-        model_output = haha()
+        self.p.start()
+        self.p.join()
+        # model_output = haha()
 
-        # model_output = model_output[0]
+        model_output = model_output[0]
         if self.use_cudagraph:
             model_output = model_output[: self.real_token_num]
 
