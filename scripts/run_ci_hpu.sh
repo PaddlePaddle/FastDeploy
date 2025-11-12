60e02a7ec0ec95861ddfcd4d10b1d8f52e7f8a53
plusNew001 <95567040+plusNew001@users.noreply.github.com>
2025-11-12 17:34:53 +0800

    Rename json directory to nlohmann_json
    Rename extracted json directory to nlohmann_json.

--- scripts/run_ci_hpu.sh
+++ scripts/run_ci_hpu.sh
@@ -32,7 +32,7 @@ pip install https://paddle-qa.bj.bcebos.com/HPU/paddle_intel_hpu-0.0.1-cp310-cp3
 echo "build whl"
 wget -q https://paddle-qa.bj.bcebos.com/suijiaxin/HPU/third-party/DeepGEMM.tar.gz && tar -xzf DeepGEMM.tar.gz -C custom_ops/third_party/
 wget -q https://paddle-qa.bj.bcebos.com/suijiaxin/HPU/third-party/cutlass.tar.gz && tar -xzf cutlass.tar.gz -C custom_ops/third_party/
-wget -q https://paddle-qa.bj.bcebos.com/suijiaxin/HPU/third-party/json.tar.gz && tar -xzf json.tar.gz -C custom_ops/third_party/
+wget -q https://paddle-qa.bj.bcebos.com/suijiaxin/HPU/third-party/json.tar.gz && tar -xzf json.tar.gz -C custom_ops/third_party/ && mv custom_ops/third_party/json custom_ops/third_party/nlohmann_json
 chmod +x build.sh
 bash build.sh || exit 1
 pip install dist/fastdeploy_intel_hpu-2.3.0.dev0-py3-none-any.whl --force-reinstall
