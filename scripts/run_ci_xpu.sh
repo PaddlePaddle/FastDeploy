da41d9d52e680a7300cb0787513b0bce0a276455
bukejiyu <395822456@qq.com>
2025-11-12 05:37:28 +0000

    xpu

--- scripts/run_ci_xpu.sh
+++ scripts/run_ci_xpu.sh
@@ -84,8 +84,7 @@ python -m fastdeploy.entrypoints.openai.api_server \
     --num-gpu-blocks-override 16384 \
     --max-model-len 32768 \
     --max-num-seqs 128 \
-    --quantization wint4  \
-    --load-choices default  > server.log 2>&1 &
+    --quantization wint4 > server.log 2>&1 &
 
 sleep 60
 # 探活
@@ -160,8 +159,7 @@ python -m fastdeploy.entrypoints.openai.api_server \
     --num-gpu-blocks-override 16384 \
     --max-model-len 32768 \
     --max-num-seqs 64 \
-    --quantization "W4A8" \
-    --load-choices default  > server.log 2>&1 &
+    --quantization "W4A8"   > server.log 2>&1 &
 
 sleep 60
 # 探活
@@ -239,8 +237,7 @@ python -m fastdeploy.entrypoints.openai.api_server \
     --enable-mm \
     --mm-processor-kwargs '{"video_max_frames": 30}' \
     --limit-mm-per-prompt '{"image": 10, "video": 3}' \
-    --reasoning-parser ernie-45-vl \
-    --load-choices default > server.log 2>&1 &
+    --reasoning-parser ernie-45-vl > server.log 2>&1 &
 
 sleep 60
 # 探活
