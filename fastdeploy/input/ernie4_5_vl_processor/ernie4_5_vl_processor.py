a0a05eb55dd7c7079d20652e13a4abb0e93a902b
kxz2002 <115912648+kxz2002@users.noreply.github.com>
2025-11-13 23:52:54 +0800

    [BugFix] adjust max_tokens and min_tokens when continue to generate tokens (#5010)
    * fix max and min tokens initial commit
    
    * fix double subtraction
    
    * add unit tests

--- fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
+++ fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
@@ -252,8 +252,11 @@ class Ernie4_5_VLProcessor(Ernie4_5Processor):
         else:
             raise ValueError(f"Request must contain 'prompt', or 'messages': {request}")
 
+        completion_token_len = 0
         if request.get("completion_token_ids"):
+            completion_token_len = len(request.get("completion_token_ids"))
             self.append_completion_tokens(outputs, request["completion_token_ids"])
+
         outputs = self.pack_outputs(outputs)
         request["prompt_token_ids"] = outputs["input_ids"].tolist()
         request["prompt_token_ids_len"] = len(request["prompt_token_ids"])
@@ -262,12 +265,17 @@ class Ernie4_5_VLProcessor(Ernie4_5Processor):
         # 截断超过长度限制的prompt
         if max_model_len is not None and len(request["prompt_token_ids"]) > max_model_len:
             request["prompt_token_ids"] = request["prompt_token_ids"][: max_model_len - 1]
+
+        tmp_max_tokens = 0
         if request.get("max_tokens") is None:
             request["max_tokens"] = max(1, max_model_len - len(request["prompt_token_ids"]))
+            tmp_max_tokens = request["max_tokens"]
         else:
-            request["max_tokens"] = min(max_model_len - len(request["prompt_token_ids"]), request["max_tokens"])
+            tmp_max_tokens = min(
+                max_model_len - len(request["prompt_token_ids"]), max(0, request["max_tokens"] - completion_token_len)
+            )
         if request.get("reasoning_max_tokens") is None:
-            request["reasoning_max_tokens"] = max(int(request["max_tokens"] * 0.8), 1)
+            request["reasoning_max_tokens"] = max(int(tmp_max_tokens * 0.8), 1)
         data_processor_logger.info(f"Processed request {request}")
 
         if request.get("top_p") is not None and request.get("top_p") < _SAMPLING_EPS:
