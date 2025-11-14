03568a3cd771ef9f4a609f419499fb8b92b4ef47
zhoutianzi666 <17801055074@163.com>
2025-11-14 11:16:11 +0800

    add

--- tests/layers/test_attention_layer.py
+++ tests/layers/test_attention_layer.py
@@ -50,6 +50,7 @@ from fastdeploy.model_executor.layers.rotary_embedding import get_rope
 from fastdeploy.model_executor.models.ernie4_5_moe import Ernie4_5_Attention
 from fastdeploy.model_executor.ops.gpu import get_padding_offset
 
+os.environ.setdefault("DG_NVCC_OVERRIDE_CPP_STANDARD", "17")
 
 class TestAttentionPerformance(unittest.TestCase):
     def setUp(self):
