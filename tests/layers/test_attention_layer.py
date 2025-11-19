2a18524dbbe4a41f032fc33f707ab3275a162821
zhoutianzi666 <17801055074@163.com>
2025-11-19 14:58:44 +0800

    first commit

--- tests/layers/test_attention_layer.py
+++ tests/layers/test_attention_layer.py
@@ -119,10 +119,10 @@ class TestAttentionPerformance(unittest.TestCase):
         config_dict = {
             "architectures": ["Ernie4_5_MoeForCausalLM"],
             "dtype": "bfloat16",
-            "hidden_size": 5120,
+            "hidden_size": 4096,
             "max_position_embeddings": 131072,
             "max_model_len": 36 * 1024 + 1024,
-            "num_attention_heads": 40,
+            "num_attention_heads": 32,
             "num_key_value_heads": 4,
             "num_hidden_layers": 57,
         }
