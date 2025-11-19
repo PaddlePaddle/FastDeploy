2c1ec9b1e234975529df0a12a0b2f9d2aa0779d0
liyonghua0910 <lyhhh37@outlook.com>
2025-11-19 11:57:01 +0000

    [fix] skip send_signal if kv signal not inited for gpu and xpu

--- custom_ops/gpu_ops/remote_cache_kv_ipc.h
+++ custom_ops/gpu_ops/remote_cache_kv_ipc.h
@@ -82,12 +82,14 @@ struct RemoteCacheKvIpc {
         }
 
         void CUDART_CB send_signal() {
-            msg_sed.mtext[1] = layer_id_;
-            if ((msgsnd(msgid, &msg_sed, (MAX_BSZ * 3 + 2) * 4, 0)) == -1) {
-                printf("kv signal full msg buffer\n");
+            if (inited) {
+                msg_sed.mtext[1] = layer_id_;
+                if ((msgsnd(msgid, &msg_sed, (MAX_BSZ * 3 + 2) * 4, 0)) == -1) {
+                    printf("kv signal full msg buffer\n");
+                }
+                layer_id_ = (layer_id_ + 1);
+                assert(layer_id_ <= num_layers_);
             }
-            layer_id_ = (layer_id_ + 1);
-            assert(layer_id_ <= num_layers_);
         }
     };
 
