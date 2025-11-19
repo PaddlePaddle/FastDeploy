da7c92a52a8e20dcb87247e4742e75bdc59762d4
LiqinruiG <37392159+LiqinruiG@users.noreply.github.com>
2025-11-19 18:43:42 +0800

    [BugFix] rollback max_tokens and min_tokens when continue to infer (#5082)
    * [BugFix] rollback  max_tokens and min_tokens when continue to infer
    
    * [BugFix] rollback  max_tokens and min_tokens when continue to infer
    
    * [fix] add more logger info:  max_tokens
    
    ---------
    
    Co-authored-by: liqinrui <liqinrui@baidu.com>

--- fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
+++ fastdeploy/input/ernie4_5_vl_processor/ernie4_5_vl_processor.py
@@ -264,14 +264,12 @@ class Ernie4_5_VLProcessor(Ernie4_5Processor):
         if max_model_len is not None and len(request["prompt_token_ids"]) > max_model_len:
             request["prompt_token_ids"] = request["prompt_token_ids"][: max_model_len - 1]
 
-        tmp_max_tokens = 0
         if request.get("max_tokens") is None:
             request["max_tokens"] = max(1, max_model_len - len(request["prompt_token_ids"]))
-            tmp_max_tokens = request["max_tokens"]
         else:
             request["max_tokens"] = min(max_model_len - len(request["prompt_token_ids"]), request["max_tokens"])
         if request.get("reasoning_max_tokens") is None:
-            request["reasoning_max_tokens"] = max(int(tmp_max_tokens * 0.8), 1)
+            request["reasoning_max_tokens"] = max(int(request["max_tokens"] * 0.8), 1)
         data_processor_logger.info(f"Processed request {request}")
 
         if request.get("top_p") is not None and request.get("top_p") < _SAMPLING_EPS:
