460ba08dd4ab14d73c191c7dbe04132cbc00b544
zhangcaiji <zhangcaiji123@163.com>
2025-11-25 09:04:45 +0000

    use deepep in paddle

--- scripts/run_ci_xpu.sh
+++ scripts/run_ci_xpu.sh
@@ -319,12 +319,6 @@ export XSHMEM_MODE=1
 export XSHMEM_QP_NUM_PER_RANK=32
 export BKCL_RDMA_VERBS=1
 
-wget -q https://paddle-qa.bj.bcebos.com/xpu_third_party/xDeepEP.tar.gz
-tar -xzf xDeepEP.tar.gz
-cd xDeepEP
-bash build.sh
-cd -
-
 export port_num=$((8188 + XPU_ID * 100))
 # 启动服务
 python -m fastdeploy.entrypoints.openai.api_server \
