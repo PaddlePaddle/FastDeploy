324fb430ceb4fdb6a80d8581ee557cf34598a96e
yuanlehome <yuanlehome@163.com>
2025-11-26 17:11:09 +0800

    fix

--- fastdeploy/model_executor/layers/linear.py
+++ fastdeploy/model_executor/layers/linear.py
@@ -872,7 +872,7 @@ class RowParallelLinear(LinearBase):
         )
         if add_bias:
             assert with_bias, "with_bias must be True when add_bias is True."
-            if self.tp_size > 0:
+            if self.tp_size > 1:
                 set_weight_attrs(self.bias, {"tp_row_bias": True})
         assert self.quant_method is not None
         create_weight_kwargs = dict(
@@ -883,7 +883,7 @@ class RowParallelLinear(LinearBase):
             ),
             model_format=fd_config.model_config.model_format,
         )
-        if self.tp_size > 0:
+        if self.tp_size > 1:
             create_weight_kwargs["split_axis"] = 0
             create_weight_kwargs["is_distributed"] = True
         self.quant_method.create_weights(**create_weight_kwargs)
@@ -910,7 +910,7 @@ class RowParallelLinear(LinearBase):
 
         out = self.quant_method.apply(self, x)
 
-        if self.reduce_results and self.nranks > 1:
+        if self.reduce_results and self.tp_size > 1:
             out = tensor_model_parallel_all_reduce(out, self.tp_group)
 
         return out
