2054605a744bcf39b0f25c44a937fc84a38836b1
ckl117 <ckl117@163.com>
2025-12-01 15:28:24 +0800

    check

--- fastdeploy/model_executor/models/glm4_moe.py
+++ fastdeploy/model_executor/models/glm4_moe.py
@@ -498,7 +498,7 @@ class Glm4MoeForCausalLM(ModelForCasualLM):
         """
         empty_input_forward
         """
-        fake_hidden_states = paddle.ones(
+        fake_hidden_states = paddle.empty(
             shape=[0, self.fd_config.model_config.hidden_size],
             dtype=paddle.get_default_dtype(),
         )
