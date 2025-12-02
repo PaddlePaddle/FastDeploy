24287181db77baf12a975a5a20de599fe90ca66a
rainyfly <1435317881@qq.com>
2025-12-02 11:43:08 +0800

    robust health check

--- fastdeploy/envs.py
+++ fastdeploy/envs.py
@@ -132,6 +132,8 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "FD_GET_RESPONSE_FROM_D_TIMEOUT": lambda: int(os.getenv("FD_GET_RESPONSE_FROM_D_TIMEOUT", "5")),
     # Timeout for first token from P in PD disaggregation
     "FD_GET_FIRST_TOKEN_FROM_P_TIMEOUT": lambda: int(os.getenv("FD_GET_RESPONSE_FROM_D_TIMEOUT", "300")),
+    # Timeout for token processor health check
+    "FD_TOKEN_PROCESSOR_HEALTH_TIMEOUT": lambda: int(os.getenv("FD_TOKEN_PROCESSOR_HEALTH_TIMEOUT", "120")),
 }
 
 
--- fastdeploy/output/token_processor.py
+++ fastdeploy/output/token_processor.py
@@ -106,6 +106,10 @@ class TokenProcessor:
         self.executor = ThreadPoolExecutor(max_workers=1)
         self.prefill_result_status = dict()
         self._finalizer = weakref.finalize(self, self._cleanup_resources)
+        # health monitor
+        self.timestamp_for_alive_before_handle_batch = None
+        self.timestamp_for_alive_after_handle_batch = None
+        self.health_lock = threading.Lock()
 
     def _cleanup_resources(self):
         """Cleaning up shared memory resources"""
@@ -273,6 +277,18 @@ class TokenProcessor:
                 llm_logger.error(f"Recieve message error: {e}")
                 continue
 
+    def healthy(self):
+        """
+        whether token processor is healthy
+        """
+        with self.health_lock:
+            if self.timestamp_for_alive_after_handle_batch is None:  # has entered handle batch
+                if time.time() - self.timestamp_for_alive_before_handle_batch > envs.FD_TOKEN_PROCESSOR_HEALTH_TIMEOUT:
+                    return False
+                else:
+                    return True
+            return True
+
     def process_sampling_results(self):
         """
         read tokens from paddle inference engine and process
@@ -329,7 +345,14 @@ class TokenProcessor:
                         continue
                     llm_logger.debug(f"rank_id {rank_id} self.output_tokens[0, 0] {self.output_tokens[0, 0]}")
                 # self._process_prefill_metrics()
+                with self.health_lock:
+                    self.timestamp_for_alive_before_handle_batch = time.time()
+                    self.timestamp_for_alive_after_handle_batch = None
                 self._process_batch_output()
+                with self.health_lock:
+                    self.timestamp_for_alive_before_handle_batch = None
+                    self.timestamp_for_alive_after_handle_batch = time.time()
+
             except Exception as e:
                 llm_logger.info(f"while get input_data error: {e} {traceback.format_exc()!s}")
 
