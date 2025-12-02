d93fca779d9be4a1dc4e1a3dad264d690a155fba
zhoutianzi666 <17801055074@163.com>
2025-12-02 15:50:02 +0800

    coommit

--- tests/layers/test_ep_moe_expert_dispatch_fp8.py
+++ tests/layers/test_ep_moe_expert_dispatch_fp8.py
@@ -1,17 +1,14 @@
-import json
-import os
-import shutil
 import unittest
-import fastdeploy
 
 import numpy as np
 import paddle
-import paddle.device.cuda.graphs as graphs
-from fastdeploy.model_executor.ops.gpu import get_padding_offset
+
+import fastdeploy
 
 np.random.seed(20160703)
 
 paddle.set_default_dtype("bfloat16")
+
 
 class TestFusedMoE(unittest.TestCase):
     def setUp(self) -> None:
@@ -20,12 +17,12 @@ class TestFusedMoE(unittest.TestCase):
     def test_ffn(self):
         paddle.seed(10)
         recv_x = paddle.randn([128, 4096], dtype="bfloat16").cast(paddle.float8_e4m3fn)
-        recv_x_scale = paddle.randn([128, 4096//128]).cast("float32")
+        recv_x_scale = paddle.randn([128, 4096 // 128]).cast("float32")
         gate_out = paddle.randn([128, 8], dtype="float32")
         recv_topk_idx = paddle.topk(gate_out, k=8, axis=-1)[1]
-        recv_topk_idx[:,3:5] = -1
+        recv_topk_idx[:, 3:5] = -1
         recv_topk_weights = paddle.topk(gate_out, k=8, axis=-1)[0]
-        
+
         tmp0 = [0] * 8
         tmp1 = [0] * 8
         recv_topk_idx_list = recv_topk_idx.flatten().numpy().tolist()
@@ -34,26 +31,27 @@ class TestFusedMoE(unittest.TestCase):
                 tmp0[ele] += 1
         for idx in range(len(tmp1)):
             tmp1[idx] = (tmp0[idx] + 127) // 128 * 128
-        
+
         token_all_num = sum(tmp1)
         baseline_m_indices = paddle.zeros([token_all_num]).cast("int32") - 1
         for idx in range(len(tmp1)):
             start = sum(tmp1[:idx])
-            baseline_m_indices[start:start+tmp0[idx]] = idx
+            baseline_m_indices[start : start + tmp0[idx]] = idx
 
-        
         tmp0 = paddle.to_tensor(tmp0).cast("int32")
         tmp1 = paddle.to_tensor(tmp1).cast("int32")
-    
-        (permute_input,
-        permute_scale,
-        permute_indices_per_token,
-        recv_num_tokens_per_expert_list_cumsum,
-        recv_num_tokens_per_expert_list_padded_cumsum,
-        dst_weights,
-        dst_indices,
-        cumsum_idx_gpu,
-        m_indices) = fastdeploy.model_executor.ops.gpu.ep_moe_expert_dispatch_fp8(
+
+        (
+            permute_input,
+            permute_scale,
+            permute_indices_per_token,
+            recv_num_tokens_per_expert_list_cumsum,
+            recv_num_tokens_per_expert_list_padded_cumsum,
+            dst_weights,
+            dst_indices,
+            cumsum_idx_gpu,
+            m_indices,
+        ) = fastdeploy.model_executor.ops.gpu.ep_moe_expert_dispatch_fp8(
             recv_x,
             recv_x_scale,
             recv_topk_idx,
@@ -67,15 +65,17 @@ class TestFusedMoE(unittest.TestCase):
 
         def haha():
             for i in range(100):
-                fastdeploy.model_executor.ops.gpu.ep_moe_expert_dispatch_fp8(recv_x,
-                recv_x_scale,
-                recv_topk_idx,
-                recv_topk_weights,
-                tmp0,
-                tmp1,
-                True,  # use_in_ep
-                token_all_num)
-        
+                fastdeploy.model_executor.ops.gpu.ep_moe_expert_dispatch_fp8(
+                    recv_x,
+                    recv_x_scale,
+                    recv_topk_idx,
+                    recv_topk_weights,
+                    tmp0,
+                    tmp1,
+                    True,  # use_in_ep
+                    token_all_num,
+                )
+
         num_tests = 20
 
         start_events = [paddle.device.cuda.Event(enable_timing=True) for _ in range(num_tests)]
@@ -90,6 +90,7 @@ class TestFusedMoE(unittest.TestCase):
 
         times = np.array([round(s.elapsed_time(e), 1) for s, e in zip(start_events, end_events)])[1:]
         print(times[-5:])
+
 
 if __name__ == "__main__":
     unittest.main()
