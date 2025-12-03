410b502fffaceabf80e7b3fe37b173d1c9c8f9f4
ming1753 <ideaminghp@163.com>
2025-12-03 18:37:59 +0800

    fix bug

--- fastdeploy/engine/common_engine.py
+++ fastdeploy/engine/common_engine.py
@@ -1002,7 +1002,6 @@ class EngineService:
                 if len(results) == 0:
                     time.sleep(0.005)
                     continue
-                self.llm_logger.info(f"results: {results}")
                 if envs.FD_ENABLE_INTERNAL_ADAPTER:
                     new_contents = []
                     for step_batch_results in results:
