44e2eb95ca32575bb5028966fa32ff2edf996d87
zhoutianzi666 <17801055074@163.com>
2025-12-04 11:50:33 +0800

    commit

--- fastdeploy/worker/gpu_worker.py
+++ fastdeploy/worker/gpu_worker.py
@@ -131,8 +131,8 @@ class GpuWorker(WorkerBase):
                 f"\nPaddle allocated memory: {paddle_allocated_mem_before_run / Gb}",
             )
         )
-
-        self.model_runner.profile_run()        
+        # 2. Profile run
+        self.model_runner.profile_run()
         set_random_seed(self.fd_config.model_config.seed)
 
         # 3. Statistical memory information
