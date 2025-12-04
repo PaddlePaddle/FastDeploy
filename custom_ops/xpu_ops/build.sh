739baeb8da082c103d628d0325778e7da89321ae
SigureMo <sigure.qaq@gmail.com>
2025-12-04 12:42:11 +0800

    [Build] Exit if neither modern nor legacy wheel dir not found

--- custom_ops/xpu_ops/build.sh
+++ custom_ops/xpu_ops/build.sh
@@ -54,6 +54,8 @@ elif [ -d "${LEGACY_PACKAGE_DIR}" ]; then
     CUSTOM_OP_DLL_PATH="${TMP_PACKAGE_DIR}/fastdeploy_ops_pd_.so"
 else
     echo -e "${RED}[Error]${NONE} Neither modern nor legacy directory for xpu ops found in ${OPS_TMP_DIR}"
+    echo -e "${BLUE}[INFO]${NONE} Maybe the compilation failed, please clean the build directory and try again."
+    exit 1
 fi
 
 mkdir -p ${TMP_PACKAGE_DIR}/libs
