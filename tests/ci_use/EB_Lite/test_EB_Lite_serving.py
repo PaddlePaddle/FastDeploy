a66ecf03e0bf9d6134606821451eb8d0dcf20d54
lizexu <2694294196@qq.com>
2025-12-05 17:48:46 +0800

    fix

--- tests/ci_use/EB_Lite/test_EB_Lite_serving.py
+++ tests/ci_use/EB_Lite/test_EB_Lite_serving.py
@@ -81,6 +81,8 @@ def setup_and_run_server():
         "wint4",
         "--graph-optimization-config",
         '{"cudagraph_capture_sizes": [1]}',
+        "--guided-decoding-backend",
+        "auto",
     ]
 
     # Start subprocess in new process group
