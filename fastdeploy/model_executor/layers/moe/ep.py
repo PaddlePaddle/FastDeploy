11f96d81bb9e578a39ba137458088a669559ffc9
zhoutianzi666 <17801055074@163.com>
2025-12-10 13:03:25 +0800

    update tbo

--- fastdeploy/model_executor/layers/moe/ep.py
+++ fastdeploy/model_executor/layers/moe/ep.py
@@ -295,7 +295,6 @@ class DeepEPEngine:
             use_fp8=use_fp8,
             async_finish=False,
             return_recv_hook=True,
-            num_per_channel=quant_group_size,
         )
 
         return packed_recv_x, recv_expert_count, handle, dispatch_hook
@@ -536,6 +535,8 @@ class EPPrefillRunner(EPRunner):
         )
 
     def set_allocate_on_comm_stream(allocate_on_comm_stream: bool = False):
+        if EPPrefillRunner.allocate_on_comm_stream == allocate_on_comm_stream:
+            return
         logger.info(
             f"set allocate_on_comm_stream to {allocate_on_comm_stream}, this will force Prefill dispatch's output tensor is allocated on communication stream"
         )
