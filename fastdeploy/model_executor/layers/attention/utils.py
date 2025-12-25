5d80b16f3d681c5cb9d8a300a3c204634cc906aa
K11OntheBoat <ruianmaidanglao@163.com>
2025-12-25 22:02:36 +0800

    pd_split

--- fastdeploy/model_executor/layers/attention/utils.py
+++ fastdeploy/model_executor/layers/attention/utils.py
@@ -15,9 +15,15 @@
 """
 
 import os
+from typing import TYPE_CHECKING
 
 from fastdeploy.config import FDConfig
 from fastdeploy.platforms import current_platform
+
+if TYPE_CHECKING:
+    from fastdeploy.model_executor.forward_meta import ForwardMeta
+
+import paddle
 
 
 def init_rank_and_device_id(fd_config: FDConfig):
@@ -40,3 +46,29 @@ def init_rank_and_device_id(fd_config: FDConfig):
         device_id = cuda_visible_devices[rank_index]
 
     return rank, device_id
+
+
+def split_decodes_and_prefills(
+    forward_meta: "ForwardMeta",
+):
+    """
+    Prefill and decode phases are split to allow the use of specialized attention kernels.
+    return num_decodes, num_prefills, num_decode_tokens, num_prefill_tokens
+    """
+    num_running_requests = forward_meta.seq_lens_this_time.shape[0]
+    seq_lens_encoder_running = forward_meta.seq_lens_encoder[:num_running_requests]
+    actual_token_nums = paddle.sum(forward_meta.seq_lens_this_time).item()
+
+    if seq_lens_encoder_running[0].item() > 0:
+        return 0, num_running_requests, 0, actual_token_nums
+
+    if seq_lens_encoder_running[-1].item() == 0:
+        return num_running_requests, 0, actual_token_nums, 0
+
+    is_prefill = seq_lens_encoder_running > 0
+    first_prefill = is_prefill.int().argmax().item()
+    num_decodes = first_prefill
+    num_prefills = num_running_requests - num_decodes
+    num_decode_tokens = paddle.sum(forward_meta.seq_lens_this_time[:first_prefill]).item()
+    num_prefill_tokens = actual_token_nums - num_decode_tokens
+    return num_decodes, num_prefills, num_decode_tokens, num_prefill_tokens
