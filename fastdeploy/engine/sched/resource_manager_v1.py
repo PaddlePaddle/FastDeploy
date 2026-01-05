9eb3674138308bd030f9bb550537f0354cf744df
rainyfly <1435317881@qq.com>
2026-01-06 00:27:11 +0800

    optimize performance

--- fastdeploy/engine/sched/resource_manager_v1.py
+++ fastdeploy/engine/sched/resource_manager_v1.py
@@ -210,6 +210,7 @@ class ResourceManagerV1(ResourceManager):
             envs.FD_RESERVE_MIN_OUTPUT_BLOCK_NUM_FOR_DECODE_WHEN_SCHEDULE_NEW_PREFILL
         )  # int
         self.current_reserve_output_block_num = self.init_reserve_output_block_num
+        self.can_relax_prefill_strategy = True
 
     def allocated_slots(self, request: Request):
         return len(request.block_tables) * self.config.cache_config.block_size
@@ -305,6 +306,7 @@ class ResourceManagerV1(ResourceManager):
                 can_schedule = True
                 break
         self.current_reserve_output_block_num = self.init_reserve_output_block_num
+        self.can_relax_prefill_strategy = False
         return can_schedule
 
     def _update_mm_hashes(self, request):
@@ -523,6 +525,19 @@ class ResourceManagerV1(ResourceManager):
                     request, self.config.cache_config.block_size, request.num_total_tokens - 1
                 )
 
+    def _get_can_schedule_prefill_threshold_block(self, request, num_chunk_new_block):
+        if self.can_relax_prefill_strategy:
+            can_schedule_block_num_threshold = num_chunk_new_block
+        else:
+            can_schedule_block_num_threshold = (
+                request.need_prefill_tokens + self.config.cache_config.block_size - 1
+            ) // self.config.cache_config.block_size + len(self.running) * self.current_reserve_output_block_num
+            if self.config.speculative_config.method is not None:
+                can_schedule_block_num_threshold = min(
+                    can_schedule_block_num_threshold + 1, self.config.cache_config.max_block_num_per_seq
+                )
+        return can_schedule_block_num_threshold
+
     def schedule(self):
         """
         Try to pull a batch of requests from the waiting queue and schedule them.
@@ -703,11 +718,9 @@ class ResourceManagerV1(ResourceManager):
 
                         num_new_tokens = self._get_num_new_tokens(request, token_budget)
                         num_new_block = self.get_new_block_nums(request, num_new_tokens)
-                        can_schedule_block_num_threshold = (
-                            request.need_prefill_tokens + self.config.cache_config.block_size - 1
-                        ) // self.config.cache_config.block_size + len(
-                            self.running
-                        ) * self.current_reserve_output_block_num
+                        can_schedule_block_num_threshold = self._get_can_schedule_prefill_threshold_block(
+                            request, num_new_block
+                        )
                         # Allocate blocks to prefill
                         if self.cache_manager.can_allocate_gpu_blocks(can_schedule_block_num_threshold):
                             if not request.get("skip_allocate", False):
@@ -753,11 +766,9 @@ class ResourceManagerV1(ResourceManager):
                                 break
                         num_new_tokens = self._get_num_new_tokens(request, token_budget)
                         num_new_block = self.get_new_block_nums(request, num_new_tokens)
-                        can_schedule_block_num_threshold = (
-                            request.need_prefill_tokens + self.config.cache_config.block_size - 1
-                        ) // self.config.cache_config.block_size + len(
-                            self.running
-                        ) * self.current_reserve_output_block_num
+                        can_schedule_block_num_threshold = self._get_can_schedule_prefill_threshold_block(
+                            request, num_new_block
+                        )
                         # Allocate blocks to prefill
                         if self.cache_manager.can_allocate_gpu_blocks(can_schedule_block_num_threshold):
                             if not request.get("skip_allocate", False):
@@ -785,13 +796,14 @@ class ResourceManagerV1(ResourceManager):
 
             if scheduled_reqs:
                 llm_logger.debug(f"schedued_reqs: {scheduled_reqs}")
-
+                self.current_reserve_output_block_num = max(
+                    int(self.current_reserve_output_block_num - self.decay_output_block_num),
+                    self.min_reserve_output_block_num,
+                    0,
+                )
+                if self.current_reserve_output_block_num == 0:
+                    self.can_relax_prefill_strategy = True
             self.update_metrics()
-            self.current_reserve_output_block_num = max(
-                int(self.current_reserve_output_block_num - self.decay_output_block_num),
-                self.min_reserve_output_block_num,
-                0,
-            )
 
             return scheduled_reqs, error_reqs
 
