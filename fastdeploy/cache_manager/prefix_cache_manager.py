68241d479cbbbafef5d5989cc1112400c9d8c884
kevin <chengyf112@gmail.com>
2026-01-06 16:47:05 +0800

    update code

--- fastdeploy/cache_manager/prefix_cache_manager.py
+++ fastdeploy/cache_manager/prefix_cache_manager.py
@@ -1777,11 +1777,12 @@ class PrefixCacheManager:
                 prefix_block_key.extend(extra_keys)
                 hash_value = get_hash_str(current_block, prefix_block_key)
                 prefix_block_key = [hash_value]
+                allocated_block_id = gpu_block_ids.pop(0)
                 if hash_value in node.children:
                     node = node.children[hash_value]
                     node.req_id_set.add(request.request_id)
+                    self.recycle_gpu_blocks(allocated_block_id)
                     continue
-                allocated_block_id = gpu_block_ids.pop(0)
                 node_id = self.node_id_pool.pop()
                 unique_node_ids.append(node_id)
                 new_last_node = BlockNode(
