3fa5465445d84acd9df6ae47a1e55414118a8c2f
xiaoluomi <1037819816@qq.com>
2026-01-07 15:28:26 +0800

    support_lastnorm_gather_split_dev

--- fastdeploy/spec_decode/mtp.py
+++ fastdeploy/spec_decode/mtp.py
@@ -1004,7 +1004,7 @@ class MTPProposer(Proposer):
                 )
 
                 # 4. Compute logits, Sample
-                logits = self.model.compute_logits(hidden_states)
+                logits = self.model.compute_logits(hidden_states, forward_meta=self.forward_meta)
                 if self.enable_logprob and self.enable_draft_logprob and substep == 0:
                     first_token_logits = self.model.compute_logits(self.model_inputs["first_token_hidden_states"])
 
