5210f8dd229860dc73266fe89de1c10b561c19ac
yangjianfengo1 <125249383+yangjianfengo1@users.noreply.github.com>
2026-01-09 14:11:47 +0800

    [Cherry-Pick][CI] Increase the shape of w4afp8 gemm(#5957) (#5948)
    * 增加w4afp8 gemm shape
    
    * 增加w4afp8 shape
    
    * code style
    
    * code style

--- custom_ops/utils/auto_gen_w4afp8_gemm_kernel.py
+++ custom_ops/utils/auto_gen_w4afp8_gemm_kernel.py
@@ -14,7 +14,8 @@
 import os
 import re
 
-file_dir = "./gpu_ops/w4afp8_gemm/"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_dir = os.path.join(script_dir, "..", "gpu_ops", "w4afp8_gemm") + os.sep
 
 gemm_template_head = """
 #pragma once
@@ -85,7 +86,30 @@ void w4afp8_gemm_M{M}_N{N}_G{GROUPSIZE}_K{K}_E{EXPERTS}_P{PADDING}_{TYPE}(
 """
 
 # [M, K, Number of experts, token Padding Size, weight K group size]
-gemm_case = [[256, 256, 2, 0, 128], [512, 256, 2, 0, 128]]
+gemm_case = [
+    [256, 256, 2, 0, 128],
+    [512, 256, 2, 0, 128],
+    [7168, 7168, 6, 8192, 128],  # num_max_dispatch_tokens_per_rank=128
+    [7168, 3584, 6, 8192, 128],  # num_max_dispatch_tokens_per_rank=128
+    [7168, 7168, 6, 10240, 128],  # num_max_dispatch_tokens_per_rank=160
+    [7168, 3584, 6, 10240, 128],  # num_max_dispatch_tokens_per_rank=160
+    [7168, 7168, 6, 12288, 128],  # num_max_dispatch_tokens_per_rank=192
+    [7168, 3584, 6, 12288, 128],  # num_max_dispatch_tokens_per_rank=192
+    [7168, 7168, 6, 16384, 128],  # num_max_dispatch_tokens_per_rank=256
+    [7168, 3584, 6, 16384, 128],  # num_max_dispatch_tokens_per_rank=256
+    [7168, 7168, 6, 20480, 128],  # num_max_dispatch_tokens_per_rank=320
+    [7168, 3584, 6, 20480, 128],  # num_max_dispatch_tokens_per_rank=320
+    [7168, 7168, 7, 8192, 128],  # num_max_dispatch_tokens_per_rank=128
+    [7168, 3584, 7, 8192, 128],  # num_max_dispatch_tokens_per_rank=128
+    [7168, 7168, 7, 10240, 128],  # num_max_dispatch_tokens_per_rank=160
+    [7168, 3584, 7, 10240, 128],  # num_max_dispatch_tokens_per_rank=160
+    [7168, 7168, 7, 12288, 128],  # num_max_dispatch_tokens_per_rank=192
+    [7168, 3584, 7, 12288, 128],  # num_max_dispatch_tokens_per_rank=192
+    [7168, 7168, 7, 16384, 128],  # num_max_dispatch_tokens_per_rank=256
+    [7168, 3584, 7, 16384, 128],  # num_max_dispatch_tokens_per_rank=256
+    [7168, 7168, 7, 20480, 128],  # num_max_dispatch_tokens_per_rank=320
+    [7168, 3584, 7, 20480, 128],  # num_max_dispatch_tokens_per_rank=320
+]
 
 dtype = ["BF16"]
 
