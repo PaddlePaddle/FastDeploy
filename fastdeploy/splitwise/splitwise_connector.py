b6fd88d94f0d3d8ed99ba0566b0d0bc0965bbf03
liyonghua0910 <lyhhh37@outlook.com>
2026-01-09 07:29:50 +0000

    [fix] fix send_cache_failed_num

--- fastdeploy/splitwise/splitwise_connector.py
+++ fastdeploy/splitwise/splitwise_connector.py
@@ -24,7 +24,6 @@ import zmq
 
 from fastdeploy import envs
 from fastdeploy.engine.request import Request, RequestOutput
-from fastdeploy.metrics.metrics import main_process_metrics
 from fastdeploy.utils import get_logger
 
 
@@ -155,7 +154,6 @@ class SplitwiseConnector:
                 self.logger.warning(f"_send_message: Send queue full for {addr}")
             except Exception as e:
                 self.logger.error(f"_send_message: Send to {addr} failed: {e}, {str(traceback.format_exc())}")
-                main_process_metrics.send_cache_failed_num.inc()
                 self._close_connection(addr)
         except Exception as e:
             self.logger.error(f"_send_message: Message preparation failed: {e}")
