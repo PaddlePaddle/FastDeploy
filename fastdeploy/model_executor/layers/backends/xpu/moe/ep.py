68e84db3fc6494d929ef6a4bc1b209266e90154e
zhangcaiji <zhangcaiji123@163.com>
2026-01-16 08:37:49 +0000

    [bugfix] export FD_XPU_MOE_FFN_QUANT_TYPE_MAP=w_channelwise_int4_a_tokenwise_int8:8-

--- fastdeploy/model_executor/layers/backends/xpu/moe/ep.py
+++ fastdeploy/model_executor/layers/backends/xpu/moe/ep.py
@@ -419,7 +419,7 @@ class XPUEPDecoderRunner(XPUEPRunner):
         **kwargs,
     ):
         expertwise_scale = kwargs.get("expertwise_scale", None)
-        use_fp8 = expertwise_scale is not None
+        use_fp8 = kwargs.get("use_fp8", False)
 
         (
             recv_hidden_states,
