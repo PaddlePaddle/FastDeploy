9e2ff9e17ae9bc7c459d2e511621b1e82406c1fa
xiaoluomi <1037819816@qq.com>
2026-01-18 21:25:23 +0800

    commit

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -1438,8 +1438,8 @@ class GPUModelRunner(ModelRunnerBase):
             elif self.enable_logprob:
                 self.max_logprobs = None if not self.speculative_decoding else 0
 
-        token_num_cpu = self.share_inputs["seq_lens_this_time"].numpy().sum().item()
-        token_num_cpu += 1
+        self.token_num_cpu = self.share_inputs["seq_lens_this_time"].numpy().sum().item()
+        self.token_num_cpu += 0
 
         # Remove padding
         (
@@ -1450,7 +1450,7 @@ class GPUModelRunner(ModelRunnerBase):
             output_cum_offsets,
             output_padding_offset,
         ) = pre_process(
-            token_num_cpu,
+            self.token_num_cpu,
             self.share_inputs["input_ids"],
             self.share_inputs["seq_lens_this_time"],
             self.speculative_decoding,
