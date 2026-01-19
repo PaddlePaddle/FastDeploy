55dfa3f8fc4c3e8ff8217a6609a04c485d8e8a92
zhoutianzi666 <17801055074@163.com>
2026-01-19 14:07:14 +0800

    comment

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -1439,7 +1439,7 @@ class GPUModelRunner(ModelRunnerBase):
                 self.max_logprobs = None if not self.speculative_decoding else 0
 
         self.token_num_cpu = self.share_inputs["seq_lens_this_time"].numpy().sum().item()
-        self.token_num_cpu += 0
+        self.token_num_cpu += 1
 
         # Remove padding
         (
