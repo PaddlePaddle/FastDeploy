3220d307f74faa1b61a949082b310952546a2d7f
lizexu <2694294196@qq.com>
2026-01-20 13:46:45 +0800

    fix

--- fastdeploy/model_executor/layers/moe/fused_moe_wint2_backend.py
+++ fastdeploy/model_executor/layers/moe/fused_moe_wint2_backend.py
@@ -278,7 +278,7 @@ class CutlassWint2FusedMoeMethod(Wint2MoeMethod):
             topk_idx,
             expert_idx_per_token,
             dequant_scale,
-            max_tokens_per_expert,
+            _,
         ) = moe_expert_dispatch(
             x,
             gate_out,
