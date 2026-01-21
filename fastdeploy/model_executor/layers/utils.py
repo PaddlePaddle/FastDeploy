6a774faa5ff29931ae572bad4b97f722c5662f1e
fanxiangyu <fxyfxy777@163.com>
2026-01-21 17:24:19 +0800

    code style

--- fastdeploy/model_executor/layers/utils.py
+++ fastdeploy/model_executor/layers/utils.py
@@ -225,7 +225,7 @@ def per_block_cast_to_fp8(x: Tensor, block_size: list = [128, 128]) -> Tuple[Ten
     copy from FastDeploy/custom_ops/gpu_ops/fp8_deep_gemm/tests/test_core.py.
     """
     from fastdeploy.model_executor.ops.gpu.deep_gemm import ceil_div
-    print("shape:",x.shape,"type: ",x.dtype,"strides:",x.strides)
+
     assert x.dim() == 2
     m, n = x.shape
     x_padded = paddle.zeros(
