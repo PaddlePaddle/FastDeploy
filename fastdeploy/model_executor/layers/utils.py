65adb307d386b7447f051539fc50fed5efa365e9
fxyfxy777 <137464345+fxyfxy777@users.noreply.github.com>
2026-01-22 09:50:57 +0800

    [BugFix] fix weight quant op (#6137)
    * fix weight quant
    
    * fix weight quant
    
    * bit equal
    
    * code style

--- fastdeploy/model_executor/layers/utils.py
+++ fastdeploy/model_executor/layers/utils.py
@@ -236,12 +236,18 @@ def per_block_cast_to_fp8(x: Tensor, block_size: list = [128, 128]) -> Tuple[Ten
         dtype=x.dtype,
     )
     x_padded[:m, :n] = x
-    from paddle.incubate.nn.functional.fp8 import fp8_quant_blockwise
-
-    x_q, scale = fp8_quant_blockwise(
-        x_padded, quant_method="128x128", input_transpose=False, output_scale_transpose=False, using_pow2_scale=False
+    x_view = paddle.view(
+        x_padded,
+        (-1, block_size[0], x_padded.shape[1] // block_size[1], block_size[1]),
     )
-    return x_q[:m, :n].contiguous(), scale
+
+    x_abs = paddle.abs(x_view).astype(paddle.float32)
+    x_amax = paddle.amax(x_abs, axis=(1, 3), keepdim=True)
+    x_amax = paddle.clip(x_amax, min=1e-4)
+    x_scaled = (x_view * (448.0 / x_amax)).astype(paddle.float8_e4m3fn)
+    return x_scaled.view_as(x_padded)[:m, :n].contiguous(), (
+        paddle.view(1.0 / (448.0 / x_amax), (x_view.shape[0], x_view.shape[2]))
+    )
 
 
 def per_token_cast_to_fp8(x: Tensor) -> Tuple[Tensor, Tensor]:
