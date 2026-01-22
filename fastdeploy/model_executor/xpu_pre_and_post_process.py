05ce7262cc63f2e0fb6f31760efb1e8659640325
iosmers <yinwei_hust@163.com>
2026-01-22 03:02:17 +0000

    to fix pd tp1 bug

--- fastdeploy/model_executor/xpu_pre_and_post_process.py
+++ fastdeploy/model_executor/xpu_pre_and_post_process.py
@@ -104,7 +104,8 @@ def xpu_pre_process(
     seq_lens_encoder: Optional[paddle.Tensor] = None,
     seq_lens_decoder: Optional[paddle.Tensor] = None,
     is_profiling: bool = False,
-    forward_meta=None,
+    forward_meta: XPUForwardMeta = None,
+    use_cudagraph: bool = False,
 ) -> XPUForwardMeta:
     """ """
     max_len = input_ids.shape[1]
@@ -224,11 +225,14 @@ def xpu_pre_process(
     xpu_forward_meta.ids_remove_padding = adjusted_input
     # Set forward_meta.is_profiling to True to skip init_kv_signal_per_query for attention backends
     xpu_forward_meta.is_profiling = is_profiling
-    if forward_meta is None:
-        return xpu_forward_meta
+    if use_cudagraph:
+        if forward_meta is None:
+            return xpu_forward_meta
+        else:
+            forward_meta.copy_from(xpu_forward_meta)
+            return forward_meta
     else:
-        forward_meta.copy_from(xpu_forward_meta)
-        return forward_meta
+        return xpu_forward_meta
 
 
 def xpu_process_output(
