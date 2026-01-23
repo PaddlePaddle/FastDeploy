2c7211207110f28758a244d4ba17370797558cc6
zhoutianzi666 <17801055074@163.com>
2026-01-23 20:15:57 +0800

    commit

--- fastdeploy/worker/output.py
+++ fastdeploy/worker/output.py
@@ -262,11 +262,6 @@ class ModelOutputData:
     input_ids: paddle.Tensor
 
     """
-        stop nums for every sequence
-    """
-    stop_nums: paddle.Tensor
-
-    """
         for speculative decoding
         full hidden states before lm_head
     """
