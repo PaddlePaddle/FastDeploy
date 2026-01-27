3e9236d46d7fd28b6a9c66bff7200656973b6e73
zhoutianzi666 <17801055074@163.com>
2026-01-27 11:48:04 +0800

    commit

--- tests/operators/test_flash_mask_attn.py
+++ tests/operators/test_flash_mask_attn.py
@@ -27,21 +27,21 @@ class TestFlashMaskAttention(unittest.TestCase):
         self.bsz = 1
         self.num_head = 8
         self.num_kv_head = 1
-        self.q_seq_len = 888
-        self.k_seq_len = 1024
+        self.q_len = 888
+        self.k_len = 1024
         self.head_dim = 128
-        np.random.seed(self.q_seq_len)
+        np.random.seed(self.q_len)
 
     def naive_attn(self, q_input, k_input, v_input, mask):
 
-        new_q = q_input.reshape([self.q_seq_len, self.num_head, self.head_dim])
+        new_q = q_input.reshape([self.q_len, self.num_head, self.head_dim])
         new_k = (
-            k_input.reshape([self.k_seq_len + self.q_seq_len, 1, self.head_dim])
+            k_input.reshape([self.k_len + self.q_len, 1, self.head_dim])
             .tile([1, self.num_head, 1])
             .contiguous()
         )
         new_v = (
-            v_input.reshape([self.k_seq_len + self.q_seq_len, 1, self.head_dim])
+            v_input.reshape([self.k_len + self.q_len, 1, self.head_dim])
             .tile([1, self.num_head, 1])
             .contiguous()
         )
@@ -49,25 +49,25 @@ class TestFlashMaskAttention(unittest.TestCase):
         p = paddle.einsum("ilk, jlk->lij", new_q, new_k)
         p = p / (np.sqrt(self.head_dim))
 
-        tmp_zeros = np.zeros((self.q_seq_len, self.q_seq_len + self.k_seq_len)) - 1
+        tmp_zeros = np.zeros((self.q_len, self.q_len + self.k_len)) - 1
         cpu_mask = mask.cpu().numpy()
-        for i in range(self.q_seq_len):
+        for i in range(self.q_len):
             tmp_zeros[i][cpu_mask[2 * i] : cpu_mask[2 * i + 1]] = 0
         mask = tmp_zeros * 1000
         mask = paddle.to_tensor(mask, dtype=q_input.dtype)
         p = p + mask[None, :]
         p = paddle.nn.functional.softmax(p, -1)
 
-        out = paddle.einsum("lij, jlk->ilk", p, new_v).reshape([self.q_seq_len, self.num_head * self.head_dim])
+        out = paddle.einsum("lij, jlk->ilk", p, new_v).reshape([self.q_len, self.num_head * self.head_dim])
         return out
 
     def paddle_flash_attn_mask(self, q_input, k_input, v_input, attn_out, mask):
         bsz = self.bsz
-        cu_seq_q = paddle.arange(bsz + 1) * self.q_seq_len
-        cu_seq_k = paddle.arange(bsz + 1) * (self.q_seq_len + self.k_seq_len)
+        cu_seq_q = paddle.arange(bsz + 1) * self.q_len
+        cu_seq_k = paddle.arange(bsz + 1) * (self.q_len + self.k_len)
         cu_seq_q = cu_seq_q.astype("int32")
         cu_seq_k = cu_seq_k.astype("int32")
-        seq_len_encoder = paddle.ones(bsz) * self.q_seq_len
+        seq_len_encoder = paddle.ones(bsz) * self.q_len
         seq_len_encoder = seq_len_encoder.astype("int32")
 
         flash_mask_attention(
@@ -85,12 +85,12 @@ class TestFlashMaskAttention(unittest.TestCase):
         )
 
     def test_flash_mask_attention(self):
-        q_input = paddle.randn([self.q_seq_len, self.num_head * self.head_dim], dtype="bfloat16")
-        k_input = paddle.randn([self.q_seq_len + self.k_seq_len, self.num_kv_head, self.head_dim], dtype="bfloat16")
-        v_input = paddle.randn([self.q_seq_len + self.k_seq_len, self.num_kv_head, self.head_dim], dtype="bfloat16")
+        q_input = paddle.randn([self.q_len, self.num_head * self.head_dim], dtype="bfloat16")
+        k_input = paddle.randn([self.q_len + self.k_len, self.num_kv_head, self.head_dim], dtype="bfloat16")
+        v_input = paddle.randn([self.q_len + self.k_len, self.num_kv_head, self.head_dim], dtype="bfloat16")
 
-        mask_start = paddle.zeros([self.q_seq_len], dtype="int32")
-        mask_end = paddle.zeros([self.q_seq_len], dtype="int32") + self.q_seq_len + self.k_seq_len
+        mask_start = paddle.zeros([self.q_len], dtype="int32")
+        mask_end = paddle.zeros([self.q_len], dtype="int32") + self.q_len + self.k_len
         mask = paddle.stack([mask_start, mask_end], axis=-1).reshape([-1])
 
         naive_attn_out = self.naive_attn(q_input, k_input, v_input, mask)
