1ea789174db6a15db1d743c47230cad4cd60f31d
ophilia-lee <58770600+ophilia-lee@users.noreply.github.com>
2026-01-27 14:54:07 +0800

    [benchmark]支持SGLang/VLLM获取cached tokens
    [benchmark]支持SGLang/VLLM获取cached tokens

--- benchmarks/backend_request_func.py
+++ benchmarks/backend_request_func.py
@@ -281,7 +281,6 @@ async def async_request_eb_openai_chat_completions(
                             elif usage := data.get("usage", {}):
                                 output.output_tokens = usage.get("completion_tokens", 0)
                                 output.prompt_tokens = usage.get("prompt_tokens", 0)
-
                                 if output.prompt_len == 0:
                                     if data["usage"] and data["usage"].get("prompt_tokens_details", {}):
                                         output.prompt_len = (
