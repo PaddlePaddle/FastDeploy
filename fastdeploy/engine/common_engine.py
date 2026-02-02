d4ef08b8cc990f512f17c8d1777639c1810d28f1
luukunn <981429396@qq.com>
2026-02-02 20:03:35 +0800

    fix

--- fastdeploy/engine/common_engine.py
+++ fastdeploy/engine/common_engine.py
@@ -1140,7 +1140,7 @@ class EngineService:
                     try:
                         if not envs.ENABLE_V1_DATA_PROCESSOR:
                             request = Request.from_dict(data)
-                        # request.metrics.scheduler_recv_req_time = time.time()
+                        request.metrics.scheduler_recv_req_time = time.time()
                         main_process_metrics.requests_number.inc()
                         trace_print(LoggingEventName.PREPROCESSING_END, data["request_id"], data.get("user", ""))
                         trace_print(LoggingEventName.REQUEST_SCHEDULE_START, data["request_id"], data.get("user", ""))
