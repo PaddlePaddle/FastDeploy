631eeb68f50c2c5d0922065d543829aae7024b15
Echo-Nie <nyxchaoji123@163.com>
2026-02-06 12:10:12 +0000

    fix ci

--- fastdeploy/model_executor/pre_and_post_process.py
+++ fastdeploy/model_executor/pre_and_post_process.py
@@ -229,7 +229,7 @@ def pre_process(
     if specific_platform and not speculative_decoding:
         # Note(ZKK): This case's code is very simple!
         ids_remove_padding, batch_id_per_token, cu_seqlens_q, cu_seqlens_k = get_padding_offset(
-            input_ids, seq_lens_this_time, token_num_cpu
+            input_ids, seq_lens_this_time, None, None, token_num_cpu
         )
         return (
             ids_remove_padding,
