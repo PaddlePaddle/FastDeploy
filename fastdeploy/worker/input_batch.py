28dbdebb96c841550ab3b391aa7d4d8722fadecb
Deleter-D <867909454@qq.com>
2026-02-25 16:36:12 +0800

    fix decode_states

--- fastdeploy/worker/input_batch.py
+++ fastdeploy/worker/input_batch.py
@@ -293,7 +293,7 @@ class InputBatch:
             # For V1_KVCACHE_SCHEDULER
             self.step_draft_tokens = paddle.full(
                 shape=[max_num_seqs, max_draft_token_num + 1],
-                fill_value=0,
+                fill_value=-1,
                 dtype="int64",
             )
             self.step_seq_lens_this_time = paddle.full([max_num_seqs, 1], 0, dtype="int32")
