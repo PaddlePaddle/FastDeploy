7e212820e2916b654cd98a99762aadb2c7dbc58a
Sunny-bot1 <592045536@qq.com>
2026-02-26 20:09:37 +0800

    update

--- fastdeploy/model_executor/layers/linear.py
+++ fastdeploy/model_executor/layers/linear.py
@@ -258,7 +258,7 @@ class LinearBase(nn.Layer):
         Raises:
             NotImplementedError: If the weight dtype is not float8 or act dtype is not equal to weight dtype.
         """
-        if self.weight_dtype == paddle.float32:
+        if self.weight_dtype == "float32":
             linear_out = self.quant_method.apply(self, x.cast("float32"))
         else:
             linear_out = self.quant_method.apply(self, x)
