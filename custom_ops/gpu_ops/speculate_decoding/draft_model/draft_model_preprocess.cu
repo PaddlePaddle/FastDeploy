8bf33e3cc846ba022f33018b952bc6681d83aaee
Deleter-D <867909454@qq.com>
2026-03-06 15:47:46 +0800

    fix

--- custom_ops/gpu_ops/speculate_decoding/draft_model/draft_model_preprocess.cu
+++ custom_ops/gpu_ops/speculate_decoding/draft_model/draft_model_preprocess.cu
@@ -224,17 +224,22 @@ __global__ void draft_model_preprocess_kernel(
         }
         if (stop_flags[tid]) {
           stop_flags[tid] = false;
+          seq_lens_decoder[tid] =
+              base_model_seq_len_decoder - base_model_seq_len_this_time;
+          step_idx[tid] =
+              base_model_step_idx[tid] - base_model_seq_len_this_time;
+        } else {
+          // 2: Last base model generated token and first MTP token
+          int recompute_limit = max(base_model_seq_len_this_time - 2, 0);
+          const int recompute_token_num_now =
+              min(recompute_token_num[tid], recompute_limit);
+          seq_lens_decoder[tid] -= recompute_token_num_now;
+          step_idx[tid] -= recompute_token_num_now;
+          mask_rollback[tid] += recompute_token_num_now;
+          // NOTE(liuzichang): Used for PD-split mode and future dynamic
+          // strategies.
+          recompute_token_num[tid] = num_model_step - 1;
         }
-        // 2: Last base model generated token and first MTP token
-        int recompute_limit = max(base_model_seq_len_this_time - 2, 0);
-        const int recompute_token_num_now =
-            min(recompute_token_num[tid], recompute_limit);
-        seq_lens_decoder[tid] -= recompute_token_num_now;
-        step_idx[tid] -= recompute_token_num_now;
-        mask_rollback[tid] += recompute_token_num_now;
-        // NOTE(liuzichang): Used for PD-split mode and future dynamic
-        // strategies.
-        recompute_token_num[tid] = num_model_step - 1;
         for (int i = 0; i < accept_num_now; i++) {
           draft_tokens_now[i] = accept_tokens_now[i];
           const int pre_id_pos =
