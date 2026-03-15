7046e885c2c12649b6cf12354548d97becf988b0
ckl117 <ckl117@163.com>
2026-03-15 11:51:07 +0800

    fa4 support causal

--- fastdeploy/model_executor/layers/attention/flash_attn_backend.py
+++ fastdeploy/model_executor/layers/attention/flash_attn_backend.py
@@ -125,7 +125,7 @@ def flash_attn_func(
         assert (
             flashmask_attention_v4 is not None
         ), "Cannot import flashmask_attention from flash_mask.cute.interface, please install it first"
-        assert attn_mask_q is not None, "FA4 requires attn_mask_q"
+        # assert attn_mask_q is not None, "FA4 requires attn_mask_q"
         assert num_heads is not None
         assert kv_num_heads is not None
         original_flash_attn_version = paddle.base.framework.get_flags(["FLAGS_flash_attn_version"])[
@@ -139,7 +139,7 @@ def flash_attn_func(
                     k.reshape([1, -1, kv_num_heads, head_dim]),
                     v.reshape([1, -1, kv_num_heads, head_dim]),
                     startend_row_indices=attn_mask_q,
-                    causal=False,
+                    causal=causal,
                     return_softmax_lse=True,
                     training=True,
                 )
@@ -154,7 +154,7 @@ def flash_attn_func(
             k.reshape([1, -1, kv_num_heads, head_dim]),
             v.reshape([1, -1, kv_num_heads, head_dim]),
             startend_row_indices=attn_mask_q,
-            causal=False,
+            causal=causal,
         )
     else:
         if version == 3:
@@ -323,6 +323,7 @@ class FlashAttentionBackend(AttentionBackend):
         forward_meta: ForwardMeta,
     ):
         metadata = forward_meta.attention_metadata
+        can_prefill_causal = getattr(forward_meta, "can_prefill_causal", self.causal)
 
         if self.pd_disaggregation_mode == "per_query":
             metadata.kv_signal_data_list[layer.layer_id] = init_signal_layerwise(
@@ -375,7 +376,7 @@ class FlashAttentionBackend(AttentionBackend):
                     forward_meta.max_len_tensor_cpu[2],
                     self.block_size,
                 )
-                if FLASH_ATTN_VERSION == 4 or forward_meta.attn_mask_offsets is not None:
+                if forward_meta.attn_mask_offsets is not None and (not can_prefill_causal):
                     metadata.attn_mask_q = get_attn_mask_q(
                         cu_seqlens_q=forward_meta.cu_seqlens_q,
                         cu_seqlens_k=metadata.cu_seqlens_k,
@@ -430,7 +431,7 @@ class FlashAttentionBackend(AttentionBackend):
                 max_seqlen_q=forward_meta.max_len_tensor_cpu[0],
                 max_seqlen_k=forward_meta.max_len_tensor_cpu[3],
                 attn_mask_q=metadata.attn_mask_q,
-                causal=self.causal,
+                causal=can_prefill_causal,
                 num_heads=self.num_heads,
                 kv_num_heads=self.kv_num_heads,
                 head_dim=self.head_dim,
