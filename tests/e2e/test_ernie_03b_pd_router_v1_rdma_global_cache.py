f3b7136119d3988ca0dd380b8c4455beaab2cd1d
juncaipeng <13006307475@163.com>
2026-03-17 08:25:16 +0000

    up docker image

--- tests/e2e/test_ernie_03b_pd_router_v1_rdma_global_cache.py
+++ tests/e2e/test_ernie_03b_pd_router_v1_rdma_global_cache.py
@@ -42,8 +42,8 @@ from utils.serving_utils import (
 FD_CONNECTOR_PORT = int(os.getenv("FD_CONNECTOR_PORT", 8433))
 FD_ROUTER_PORT = int(os.getenv("FD_ROUTER_PORT", 8533))
 FD_RDMA_PORT = int(os.getenv("FD_RDMA_PORT", 8623))
-FD_MOONCAKE_MASTER_PORT = int(os.getenv("FD_MOONCAKE_MASTER_PORT", 8723))
-FD_MOONCAKE_METADATA_PORT = int(os.getenv("FD_MOONCAKE_METADATA_PORT", 8823))
+FD_MOONCAKE_MASTER_PORT = FD_RDMA_PORT + 1
+FD_MOONCAKE_METADATA_PORT = FD_RDMA_PORT + 2
 
 # List of ports to clean before and after tests
 PORTS_TO_CLEAN = [
