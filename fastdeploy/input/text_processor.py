7dc4645b671216022abbc3116e37adb12a82a451
luukunn <981429396@qq.com>
2026-03-20 15:24:53 +0800

    Optimize CPU utilization

--- fastdeploy/input/text_processor.py
+++ fastdeploy/input/text_processor.py
@@ -76,6 +76,34 @@ class BaseDataProcessor(ABC):
         set_value(request, "presence_penalty", 0.0)
         return request
 
+    @abstractmethod
+    def process_request(self, request, **kwargs):
+        """
+        Preprocess the request
+
+        Args:
+            request (Dict): may contain text and messages fields
+            **kwargs: others
+
+        Returns:
+            bool: Whether preprocessing is successful
+            str: error message
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def process_response(self, response_dict):
+        """
+        Preprocess the response
+
+        Args:
+            response_dict (Dict): response for engine, contain ids fields
+
+        Returns:
+            Dict: response contain text fields
+        """
+        raise NotImplementedError
+
     def text2ids(self, text, max_model_len=None):
         """
         text to token ids
@@ -266,6 +294,112 @@ class DataProcessor(BaseDataProcessor):
             logits_processors_args.pop("think_prompt_last_token_id", None)
         return logits_processors_args
 
+    def process_request(self, request, max_model_len=None, **kwargs):
+        """
+        Preprocess the request
+
+        Args:
+            request (Dict): may contain text and messages fields
+
+        Returns:
+            bool: Whether preprocessing is successful
+            str: error message
+        """
+        data_processor_logger.info(f"Start processing request: {request}")
+        request = self._apply_default_parameters(request)
+        if request.get("eos_token_ids") is None or len(request.eos_token_ids) == 0:
+            request.eos_token_ids = self.eos_token_ids
+
+        # processing stop_sequences and stop_token_ids
+        process_stop_token_ids(request, self.update_stop_seq)
+
+        # processing bad_words
+        bad_words = request.get("bad_words")
+        bad_words_token_ids = request.get("bad_words_token_ids")
+        if bad_words:
+            bad_words_token_ids = self.update_bad_words(bad_words, bad_words_token_ids)
+            request["bad_words_token_ids"] = bad_words_token_ids
+
+        logits_processors_args = request.get("logits_processors_args") or {}
+        think_stop_sentence = logits_processors_args.get("think_stop_sentence")
+        if isinstance(think_stop_sentence, str) and think_stop_sentence:
+            newline_token_ids = self.encode_with_cache("\n", max_model_len, add_special_tokens=False)
+            sentence_token_ids = self.encode_with_cache(think_stop_sentence, max_model_len, add_special_tokens=False)
+            logits_processors_args["think_stop_sentence_token_ids"] = newline_token_ids + sentence_token_ids
+            logits_processors_args.pop("think_stop_sentence", None)
+            request["logits_processors_args"] = logits_processors_args
+
+        # processing prompt_token_ids
+        if request.prompt_token_ids is None or len(request.prompt_token_ids) == 0:
+            if request.prompt is not None:
+                prompt = request.prompt
+                add_special_tokens = request.get("add_special_tokens", False)
+                assert isinstance(prompt, str) or (
+                    isinstance(prompt, list) and all([isinstance(t, int) for t in prompt])
+                ), f"prompt must be a string or a list of integers, but got {type(prompt)}"
+                if isinstance(prompt, list):  # if prompt is a token id list
+                    request.prompt_token_ids = prompt
+                else:
+                    request.prompt_token_ids = self.text2ids(
+                        request.prompt, max_model_len, add_special_tokens=add_special_tokens
+                    )
+            elif request.messages is not None:
+                if self.tokenizer.chat_template is None:
+                    raise ValueError("This model does not support chat_template.")
+                task = request.to_dict()
+                chat_template_kwargs = kwargs.get("chat_template_kwargs", {})
+                if chat_template_kwargs:
+                    if isinstance(chat_template_kwargs, dict):
+                        for k, v in chat_template_kwargs.items():
+                            if k not in task or task[k] is None:
+                                task[k] = v
+                    else:
+                        raise ValueError("Invalid input: chat_template_kwargs must be a dict")
+                task.setdefault("enable_thinking", True)
+                request.prompt_token_ids = self.messages2ids(task, **chat_template_kwargs)
+            else:
+                raise ValueError(f"The request should have `input_ids`, `text` or `messages`: {request}.")
+
+        if len(request.prompt_token_ids) == 0:
+            raise ValueError("Invalid input: prompt_token_ids must be a non-empty sequence of token IDs")
+
+        # truncate prompts that exceed the length limit
+        if max_model_len is not None and len(request.prompt_token_ids) > max_model_len:
+            request.prompt_token_ids = request.prompt_token_ids[: max_model_len - 1]
+
+        logits_processors_args = request.get("logits_processors_args") or {}
+        logits_processors_args = self._update_thinking_prompt_state(request.prompt_token_ids, logits_processors_args)
+        request["logits_processors_args"] = logits_processors_args
+
+        max_tokens = max_model_len - len(request.prompt_token_ids)
+        if request.get("max_tokens") is None:
+            request.set("max_tokens", max(1, max_tokens))
+        else:
+            request.set("max_tokens", min(max_tokens, request.get("max_tokens")))
+        if request.get("temperature") < _SAMPLING_EPS:
+            # zero temperature means greedy decoding: set top_k=1 to force argmax
+            request.set("temperature", 1)
+            request.set("top_k", 1)
+        if request.get("top_p") < _SAMPLING_EPS:
+            request.set("top_p", _SAMPLING_EPS)
+        if self.reasoning_parser:
+            model_status = self.reasoning_parser.get_model_status(request.prompt_token_ids)
+            parts = request.request_id.split("_")
+            if len(parts) > 1:
+                real_req_id = parts[0]
+                index = int(parts[1])
+                n = request.get("n", 1)
+                for idx in range(index * n, (index + 1) * n):
+                    self.model_status_dict[f"{real_req_id}_{idx}"] = model_status
+            else:
+                self.model_status_dict[request.request_id] = model_status
+            request.enable_thinking = model_status == "think_start"
+
+        if request.get("response_max_tokens") is not None and request.enable_thinking is False:
+            request["max_tokens"] = min(request["response_max_tokens"], request["max_tokens"])
+        data_processor_logger.info(f"Processed request: {request}")
+        return request
+
     def process_request_dict(self, request, max_model_len=None, **kwargs):
         """
         Preprocess the request
@@ -304,17 +438,10 @@ class DataProcessor(BaseDataProcessor):
         # processing prompt_token_ids
         if not request.get("prompt_token_ids"):
             if request.get("prompt"):
-                prompt = request["prompt"]
-                assert isinstance(prompt, str) or (
-                    isinstance(prompt, list) and all(isinstance(t, int) for t in prompt)
-                ), f"prompt must be a string or a list of integers, but got {type(prompt)}"
-                if isinstance(prompt, list):
-                    request["prompt_token_ids"] = prompt
-                else:
-                    add_special_tokens = request.get("add_special_tokens", False)
-                    request["prompt_token_ids"] = self.text2ids(
-                        prompt, max_model_len, add_special_tokens=add_special_tokens
-                    ).tolist()
+                add_special_tokens = request.get("add_special_tokens", False)
+                request["prompt_token_ids"] = self.text2ids(
+                    request["prompt"], max_model_len, add_special_tokens=add_special_tokens
+                ).tolist()
             elif request.get("messages"):
                 if self.tokenizer.chat_template is None:
                     raise ValueError("This model does not support chat_template.")
@@ -355,7 +482,6 @@ class DataProcessor(BaseDataProcessor):
             request["top_k"] = 1
         if request.get("top_p") < _SAMPLING_EPS:
             request["top_p"] = _SAMPLING_EPS
-            request["top_k"] = 1
         if self.reasoning_parser:
             model_status = self.reasoning_parser.get_model_status(request["prompt_token_ids"])
             parts = request["request_id"].split("_")
@@ -376,6 +502,39 @@ class DataProcessor(BaseDataProcessor):
         full_text = self.tokenizer.decode(token_ids, **kwargs)
         return full_text
 
+    def process_response(self, response_dict, **kwargs):
+        """
+        Preprocess the response
+
+        Args:
+            response_dict (Dict): response for engine, contain ids fields
+
+        Returns:
+            Dict: response contain text fields
+        """
+        req_id = response_dict.request_id
+        token_ids = response_dict.outputs.token_ids
+        if token_ids[-1] == self.tokenizer.eos_token_id:
+            token_ids = token_ids[:-1]
+        full_text = self.tokenizer.decode(token_ids)
+        response_dict.outputs.text = full_text
+        if self.reasoning_parser:
+            reasoning_content, text = self.reasoning_parser.extract_reasoning_content(
+                full_text, response_dict, self.model_status_dict[req_id]
+            )
+            response_dict.outputs.text = text
+            response_dict.outputs.reasoning_content = reasoning_content
+        if self.tool_parser_obj:
+            tool_parser = self.tool_parser_obj(self.tokenizer)
+            tool_call_info = tool_parser.extract_tool_calls(full_text, response_dict)
+            if tool_call_info.tools_called:
+                response_dict.outputs.tool_calls = tool_call_info.tool_calls
+        if req_id in self.model_status_dict:
+            del self.model_status_dict[req_id]
+        data_processor_logger.info(f"req_id:{req_id}, token_ids: {token_ids}")
+
+        return response_dict
+
     def process_response_dict_normal(self, response_dict, **kwargs):
         """
         Preprocess the response
@@ -390,15 +549,10 @@ class DataProcessor(BaseDataProcessor):
         is_end = response_dict["finished"]
         req_id = response_dict["request_id"]
         request = kwargs.get("request", None)
-        direct_decode = kwargs.get("direct_decode", False)
         if is_end and len(token_ids) > 0 and not kwargs.get("include_stop_str_in_output"):
             if token_ids[-1] in self.eos_token_ids:
                 token_ids = token_ids[:-1]
-        if direct_decode:
-            delta_text = self.tokenizer.decode(token_ids)
-            previous_texts = ""
-        else:
-            delta_text, _, previous_texts = self.ids2tokens(token_ids, req_id)
+        delta_text, _, previous_texts = self.ids2tokens(token_ids, req_id)
         if is_end:
             full_text = previous_texts + delta_text
             response_dict["outputs"]["completion_tokens"] = full_text
@@ -418,9 +572,8 @@ class DataProcessor(BaseDataProcessor):
                 tool_call_info = tool_parser.extract_tool_calls(full_text, request)
                 if tool_call_info.tools_called:
                     response_dict["outputs"]["tool_calls"] = tool_call_info.tool_calls
-            if req_id in self.decode_status:
-                data_processor_logger.info(f"req_id:{req_id}, decode_status: {self.decode_status[req_id]}")
-                del self.decode_status[req_id]
+            data_processor_logger.info(f"req_id:{req_id}, decode_status: {self.decode_status[req_id]}")
+            del self.decode_status[req_id]
             if req_id in self.model_status_dict:
                 del self.model_status_dict[req_id]
         return response_dict
@@ -439,6 +592,7 @@ class DataProcessor(BaseDataProcessor):
         req_id = response_dict["request_id"]
         token_ids = response_dict["outputs"]["token_ids"]
         request = kwargs.get("request", None)
+        response_dict["outputs"]["enable_parser"] = False
 
         if is_end and len(token_ids) > 0 and not kwargs.get("include_stop_str_in_output"):
             if token_ids[-1] in self.eos_token_ids:
@@ -450,6 +604,7 @@ class DataProcessor(BaseDataProcessor):
         response_dict["outputs"]["tool_calls"] = None
         response_dict["outputs"]["reasoning_content"] = ""
         if self.reasoning_parser:
+            response_dict["outputs"]["enable_parser"] = True
             reasoning_delta_message = self.reasoning_parser.extract_reasoning_content_streaming(
                 previous_texts,
                 previous_texts + delta_text,
@@ -469,6 +624,7 @@ class DataProcessor(BaseDataProcessor):
                 if not is_end:
                     response_dict["outputs"]["skipped"] = True
         if self.tool_parser_obj:
+            response_dict["outputs"]["enable_parser"] = True
             if req_id not in self.tool_parser_dict:
                 self.tool_parser_dict[req_id] = self.tool_parser_obj(self.tokenizer)
             tool_parser = self.tool_parser_dict[req_id]
@@ -597,38 +753,38 @@ class DataProcessor(BaseDataProcessor):
                 # history token ids & history token strings & befer decode str
                 self.decode_status[task_id] = [[], [], ""]
 
-            previous_token_ids = self.decode_status[task_id][0]
+            status = self.decode_status[task_id]
+            status[0].extend(token_id)
             decode_str = self.tokenizer.batch_decode(
-                [previous_token_ids + token_id],
+                [status[0]],
                 skip_special_tokens=True,
                 clean_up_tokenization_spaces=False,
             )
             if isinstance(decode_str, list) and len(decode_str):
-                new_str = decode_str[0].replace(self.decode_status[task_id][2], "", 1)
-                self.decode_status[task_id][1].append(new_str)
-                self.decode_status[task_id][2] = decode_str[0]
+                new_str = decode_str[0].replace(status[2], "", 1)
+                status[1].append(new_str)
+                status[2] = decode_str[0]
             else:
                 new_str = ""
-            self.decode_status[task_id][0] += token_id
             return new_str
         else:
             if task_id not in self.decode_status:
                 # prefix offset & read offset & history token ids & history token strings
                 self.decode_status[task_id] = [0, 0, [], ""]
 
-            prefix_offset = self.decode_status[task_id][0]
-            read_offset = self.decode_status[task_id][1]
-            previous_token_ids = self.decode_status[task_id][2]
-            previous_texts = self.decode_status[task_id][3]
-            decode_str, prefix_offset, read_offset = self.tokenizer.decode_token(
-                previous_token_ids + token_id, prefix_offset, read_offset
-            )
-            self.decode_status[task_id][0] = prefix_offset
-            self.decode_status[task_id][1] = read_offset
-            self.decode_status[task_id][2] += token_id
-            self.decode_status[task_id][3] += decode_str
+            status = self.decode_status[task_id]
+            previous_texts = status[3]
 
-            return decode_str, previous_token_ids, previous_texts
+            # Extend in-place first, then pass the full list to decode_token
+            # Avoids creating an O(n) temporary list every token
+            status[2].extend(token_id)
+
+            decode_str, prefix_offset, read_offset = self.tokenizer.decode_token(status[2], status[0], status[1])
+            status[0] = prefix_offset
+            status[1] = read_offset
+            status[3] += decode_str
+
+            return decode_str, status[2], previous_texts
 
     def _load_tokenizer(self):
         """
