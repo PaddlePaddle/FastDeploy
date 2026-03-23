b2d0d243493c8ed1c8c6b874638b6658936a5dc0
Nyakku Shigure <sigure.qaq@gmail.com>
2026-03-23 22:28:35 +0800

    cache _is_package_installed

--- fastdeploy/utils.py
+++ fastdeploy/utils.py
@@ -33,6 +33,7 @@ import time
 import traceback
 from datetime import datetime
 from enum import Enum
+from functools import cache
 from http import HTTPStatus
 from importlib.metadata import PackageNotFoundError, distribution
 from logging.handlers import BaseRotatingHandler
@@ -1310,6 +1311,7 @@ def do_nothing(*args, **kwargs):
     return decorator
 
 
+@cache
 def _is_package_installed(dist_name: str) -> bool:
     try:
         distribution(dist_name)
