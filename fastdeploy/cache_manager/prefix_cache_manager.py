ba9318a75f7d29cd7a54c7ea5e676fee22cd2c60
liyonghua0910 <lyhhh37@outlook.com>
2026-03-25 07:44:37 +0000

    [fix] fix ep control for engine cache queue

--- fastdeploy/cache_manager/prefix_cache_manager.py
+++ fastdeploy/cache_manager/prefix_cache_manager.py
@@ -217,7 +217,7 @@ class PrefixCacheManager:
             is_server=False,
             num_client=tensor_parallel_size,
             client_id=0,
-            local_data_parallel_id=self.local_data_parallel_id,
+            local_data_parallel_id=0,
         )
 
         current_dir_path = os.path.split(os.path.abspath(__file__))[0]
--- fastdeploy/worker/worker_process.py
+++ fastdeploy/worker/worker_process.py
@@ -608,6 +608,7 @@ class PaddleDisWorkerProc:
             if self.parallel_config.use_ep:
                 pendings = all_gather_values(len(self.cached_control_reqs), self.parallel_config.ep_group)
                 if all([p > 0 for p in pendings]):
+                    logger.info(f"Rank: {self.local_rank} Detected all ep ranks have pending control tasks.")
                     self.run_control_method(self.cached_control_reqs.pop(0))
 
             if (
