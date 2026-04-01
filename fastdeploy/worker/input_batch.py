7f6a73897717e4f418fecd05f7570d953daa03ad
Sunny-bot1 <592045536@qq.com>
2026-04-01 11:17:59 +0800

    del last_seq_lens_encoder

--- fastdeploy/worker/input_batch.py
+++ fastdeploy/worker/input_batch.py
@@ -833,9 +833,6 @@ class ProposerInputBatch(InputBatch):
             self.last_seq_lens_this_time = paddle.full_like(
                 self.target_model_input_batch["seq_lens_this_time"], fill_value=-1, dtype="int32"
             )
-            self.last_seq_lens_encoder = paddle.full_like(
-                self.target_model_input_batch["seq_lens_encoder"], fill_value=0, dtype="int32"
-            )
         self.input_ids_len = paddle.zeros(shape=[self.scheduler_config.max_num_seqs, 1], dtype="int64", device="cpu")
         self.temp_scaled_logprobs = self.target_model_input_batch["temp_scaled_logprobs"]
         self.top_p_normalized_logprobs = self.target_model_input_batch["top_p_normalized_logprobs"]
@@ -1012,7 +1009,6 @@ class ProposerInputBatch(InputBatch):
             # Reset last sequence lengths if applicable
             if self.num_model_steps > 1:
                 fill_paddle_tensor(self, "last_seq_lens_this_time", -1)
-                fill_paddle_tensor(self, "last_seq_lens_encoder", 0)
 
             # Reset input IDs length
             fill_paddle_tensor(self, "input_ids_len", 0)
