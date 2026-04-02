892d67e07502a3f1ef5dffad13079581685882d4
liyonghua0910 <lyhhh37@outlook.com>
2026-04-02 02:46:38 +0000

    [fix] prevent requests from entering running state without a slot

--- fastdeploy/engine/common_engine.py
+++ fastdeploy/engine/common_engine.py
@@ -909,8 +909,7 @@ class EngineService:
                     time.sleep(0.005)
 
             except RuntimeError as e:
-                if "cannot schedule new futures after shutdown" in str(e):
-                    break
+                raise e
             except Exception as e:
                 err_msg = "Error happend while insert task to engine: {}, {}.".format(e, str(traceback.format_exc()))
                 self.llm_logger.error(err_msg)
