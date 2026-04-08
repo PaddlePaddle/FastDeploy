2b785e52ef05308a3bc19592ae18a18036d443c5
Deleter-D <867909454@qq.com>
2026-04-08 15:54:28 +0800

    Revert "VL overlap (#4084)"
    This reverts commit 725da10522f740642b88cadf26b31f5fcac9e4d5.

--- custom_ops/gpu_ops/moe/deepgemm_preprocess.cu
+++ custom_ops/gpu_ops/moe/deepgemm_preprocess.cu
@@ -17,12 +17,8 @@
 
 template <typename scalar_t>
 __global__ void
-cuda_kernel(const scalar_t *__restrict__ topk_ids,
-            int32_t *__restrict__ res,
-            int32_t *__restrict__ res_padded,
-            int32_t *__restrict__ token_nums_this_rank_padded,
-            size_t numel,
-            int num_experts) {
+cuda_kernel(const scalar_t *__restrict__ topk_ids, int32_t *__restrict__ res,
+            int32_t *__restrict__ res_padded, size_t numel, int num_experts) {
 
   extern __shared__ int32_t tokens_per_ep[];
 
@@ -42,42 +38,22 @@ cuda_kernel(const scalar_t *__restrict__ topk_ids,
     res[i] = tokens_per_ep[i];
     res_padded[i] = (res[i] + 127) / 128 * 128;
   }
-
-  if (threadIdx.x == 0) {
-    int32_t sum = 0;
-    for (int i = 0; i < num_experts; ++i) {
-      sum += res_padded[i];
-    }
-    *token_nums_this_rank_padded = sum;
-  }
 }
 
-std::vector<paddle::Tensor> count_tokens_per_expert_func(const paddle::Tensor &topk_ids,
+paddle::Tensor count_tokens_per_expert_func(const paddle::Tensor &topk_ids,
                                             int64_t num_experts) {
 
   int topk_ids_numel = topk_ids.shape()[0] * topk_ids.shape()[1];
-  auto token_nums_per_expert = GetEmptyTensor({2, num_experts}, paddle::DataType::INT32, topk_ids.place());
-  auto token_nums_this_rank_padded = paddle::full({1}, 0, paddle::DataType::INT32, topk_ids.place());
 
-  int *h_token_nums_this_rank_padded;
-  cudaHostAlloc((void **)&h_token_nums_this_rank_padded,
-                sizeof(int),
-                cudaHostAllocDefault);
+  auto token_nums_per_expert = paddle::empty(
+      {2, num_experts}, paddle::DataType::INT32, topk_ids.place());
 
   auto stream = topk_ids.stream();
   using scalar_t = int64_t;
 
   cuda_kernel<<<1, 1024, num_experts * sizeof(int32_t), stream>>>(
-      topk_ids.data<scalar_t>(),
-      token_nums_per_expert.data<int32_t>(),
-      token_nums_per_expert.data<int32_t>() + num_experts,
-      token_nums_this_rank_padded.data<int32_t>(),
-      topk_ids_numel,
+      topk_ids.data<scalar_t>(), token_nums_per_expert.data<int32_t>(),
+      token_nums_per_expert.data<int32_t>() + num_experts, topk_ids_numel,
       num_experts);
-
-  cudaMemcpyAsync(h_token_nums_this_rank_padded, token_nums_this_rank_padded.data<int>(), sizeof(int), cudaMemcpyDeviceToHost, stream);
-  cudaStreamSynchronize(stream);
-  auto out = paddle::full({1}, *h_token_nums_this_rank_padded, paddle::DataType::INT32, paddle::CPUPlace());
-  cudaFreeHost(h_token_nums_this_rank_padded);
-  return {token_nums_per_expert, out};
+  return token_nums_per_expert;
 }
