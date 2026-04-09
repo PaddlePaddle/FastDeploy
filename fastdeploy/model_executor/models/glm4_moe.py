629ca6b9d5075e3e95a42871b988bc34336f2d51
zhangbo9674 <zhangbo54@baidu.com>
2026-04-09 21:11:10 +0800

    refine code

--- fastdeploy/model_executor/models/glm4_moe.py
+++ fastdeploy/model_executor/models/glm4_moe.py
@@ -16,6 +16,7 @@
 
 from __future__ import annotations
 
+import os
 import re
 from functools import partial
 
@@ -24,7 +25,6 @@ from paddle import nn
 from paddleformers.transformers import PretrainedModel
 from paddleformers.utils.log import logger
 
-from fastdeploy import envs
 from fastdeploy.config import FDConfig
 from fastdeploy.model_executor.forward_meta import ForwardMeta
 from fastdeploy.model_executor.graph_optimization.decorator import (
@@ -314,7 +314,7 @@ class Glm4MoeDecoderLayer(nn.Layer):
         residual: paddle.Tensor = None,
     ):
         """ """
-        proxy_rmsnorm = rms_norm_func if envs.FD_ENABLE_RL == 1 else None
+        proxy_rmsnorm = rms_norm_func if int(os.getenv("FD_USE_PHI_RMSNORM", "0")) == 1 else None
         hidden_states, residual = self.input_layernorm(
             hidden_states, residual_input=residual, forward_meta=forward_meta, proxy_rmsnorm=proxy_rmsnorm
         )
