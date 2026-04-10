dcdeeaddcbaabac4b85de0cf70cc98fdcfa94ef9
zhangbo9674 <82555433+zhangbo9674@users.noreply.github.com>
2026-04-10 16:02:37 +0800

    [RL] change rms norm for glm (#7269)
    * change rms norm for glm
    
    * refine code
    
    * refine code
    
    * refine code

--- fastdeploy/envs.py
+++ fastdeploy/envs.py
@@ -219,6 +219,8 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "FD_USE_PHI_MOE_TOPK": lambda: bool(int(os.getenv("FD_USE_PHI_MOE_TOPK", "0"))),
     # Whether to use phi MOE permute,if 1,use paddle op.
     "FD_USE_PHI_MOE_PERMUTE": lambda: bool(int(os.getenv("FD_USE_PHI_MOE_PERMUTE", "0"))),
+    # Whether to use phi rms_norm,if 1,use paddle op.
+    "FD_USE_PHI_RMSNORM": lambda: bool(int(os.getenv("FD_USE_PHI_RMSNORM", "0"))),
     # Control class SiluAndMul to use swiglu or fusid_bias_act operator in the forward_cuda function
     "FD_SiluAndMul_USE_PHI_SWIGLU": lambda: bool(int(os.getenv("FD_SiluAndMul_USE_PHI_SWIGLU", "0"))),
     # Reserve output blocks for decoding requests when schedule new prefill requests
