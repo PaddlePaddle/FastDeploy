93ea07c2ccb63f8bc8ea1db2899109f875c4abbc
luukunn <981429396@qq.com>
2026-04-13 14:19:16 +0800

    fix

--- fastdeploy/entrypoints/openai/protocol.py
+++ fastdeploy/entrypoints/openai/protocol.py
@@ -229,7 +229,7 @@ class FunctionDefinition(BaseModel):
     name: str
     description: Optional[str] = None
     parameters: Optional[dict[str, Any]] = None
-    strict: bool = False
+    strict: Optional[bool] = None
 
 
 class ChatCompletionToolsParam(BaseModel):
