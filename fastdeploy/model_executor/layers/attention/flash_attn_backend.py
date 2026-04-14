55e7a971ff118ea9b23bcca939895347cc2f3755
ckl117 <ckl117@163.com>
2026-04-14 20:59:07 +0800

    check init_flash_attn_version log

--- fastdeploy/model_executor/layers/attention/flash_attn_backend.py
+++ fastdeploy/model_executor/layers/attention/flash_attn_backend.py
@@ -89,7 +89,7 @@ def init_flash_attn_version():
                 logger.info(f"The current platform[sm{get_sm_version()}] can't import Flash Attention V4.")
 
         if FLASH_ATTN_VERSION is None:
-            if sm_version >= 89 and any(num >= 89 for num in paddle.version.cuda_archs()):
+            if sm_version == 90 and 90 in paddle.version.cuda_archs():
                 FLASH_ATTN_VERSION = 3
                 logger.info("The current platform supports Flash Attention V3.")
             else:
