55d1a0537c6a499a7641c925955ee7576e829ddb
lonelygsh <490991455@qq.com>
2026-04-15 16:01:44 +0800

    update

--- fastdeploy/model_executor/utils.py
+++ fastdeploy/model_executor/utils.py
@@ -132,7 +132,7 @@ def slice_fn(weight_or_paramter, output_dim, start, end, step=1):
 def process_weight_transpose(layer, weight_name):
     weight = getattr(layer, weight_name)
     if not weight._is_initialized():
-        logger.info("权重没初始化啊！")
+        logger.warning(f"Weight {weight_name} not initialized, skipping transpose.")
         return
     if len(weight.shape) == 2:
         weight_shape = weight.shape[::-1]
