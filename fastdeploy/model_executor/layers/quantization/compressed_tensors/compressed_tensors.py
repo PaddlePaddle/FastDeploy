c1068099dedf07371c58cf40c708543329d3a666
Toryn <1584340372@qq.com>
2026-04-21 15:30:42 +0800

    loading

--- fastdeploy/model_executor/layers/quantization/compressed_tensors/compressed_tensors.py
+++ fastdeploy/model_executor/layers/quantization/compressed_tensors/compressed_tensors.py
@@ -4,10 +4,11 @@ from ...moe import FusedMoE
 from .compressed_tensors_moe import CompressedTensorsMoEMethod
 
 class CompressedTensorsConfig(QuantConfigBase):
-    def __init__(self):
+    def __init__(self, is_checkpoint_bf16: bool = False):
         self.quant_max_bound = 0
         self.quant_min_bound = 0
         self.quant_round_type = 0
+        self.is_checkpoint_bf16 = is_checkpoint_bf16
     
     def name(self) -> str:
         return "compressed-tenosrs"
