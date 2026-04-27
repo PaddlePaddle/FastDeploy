ce002973685b38ff6d309d052c192b9e88717dfe
JYChen <zoooo0820@qq.com>
2026-04-27 20:19:09 +0800

    fix fp8 infer error (#7627)

--- fastdeploy/model_executor/layers/quantization/block_wise_fp8.py
+++ fastdeploy/model_executor/layers/quantization/block_wise_fp8.py
@@ -155,7 +155,7 @@ def deep_gemm_fp8_gemm_nt(
                 linear_out,
             )
             if bias is not None:
-                linear_out = paddle.add(linear_out, bias)
+                linear_out.add_(bias)
     else:
         fp8_gemm_nt(
             (x, x_scale_tensor),
@@ -370,7 +370,7 @@ class BlockWiseFP8LinearMethod(QuantMethodBase):
             )
             x_scale_tensor = x_scale_tensor.T[: x.shape[0], ...]
 
-        if get_sm_version() == 100 and current_platform.is_cuda():
+        if get_sm_version() >= 100 and current_platform.is_cuda():
             deep_gemm_fp8_gemm_nt(
                 x,
                 x_scale_tensor,
@@ -391,5 +391,4 @@ class BlockWiseFP8LinearMethod(QuantMethodBase):
             )
             if layer.with_bias:
                 linear_out = paddle.add(linear_out, layer.bias)
-
         return linear_out
