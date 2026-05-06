5d9eace9d52257ac85731c3d94a3086a0a545547
wujiji <yuzzhewu@163.com>
2026-05-06 11:27:29 +0800

    [Iluvatar] Support wi4a16_group_gemv for optimize moe wint4 performance

--- custom_ops/iluvatar_ops/wi4a16_group_gemv.cu
+++ custom_ops/iluvatar_ops/wi4a16_group_gemv.cu
@@ -15,7 +15,7 @@
 #include "helper.h"
 #include "iluvatar_context.h"
 
-std::vector<paddle::Tensor> W8A16GroupGemv(
+std::vector<paddle::Tensor> WI4A16GroupGemv(
     const paddle::Tensor& x,
     const paddle::Tensor& weight,
     const paddle::Tensor& weight_scale,
@@ -25,44 +25,57 @@ std::vector<paddle::Tensor> W8A16GroupGemv(
   auto dev_ctx = static_cast<const phi::CustomContext*>(
       paddle::experimental::DeviceContextPool::Instance().Get(x.place()));
   auto stream = static_cast<const cudaStream_t>(dev_ctx->stream());
+
   const auto& x_dims = x.dims();
   const auto& w_dims = weight.dims();
   const auto& ws_dims = weight_scale.dims();
   const auto& tokens_per_expert_dims = tokens_per_expert.dims();
+  const auto& zeros_dims = weight_zeros.dims();
   // [m, k]
   PD_CHECK(x_dims.size() == 2, "x should be 2D");
-  // [n_experts, n, k]
+  // [n_experts, n // 2, k]
   PD_CHECK(w_dims.size() == 3, "weight should be 3D");
-  // [n_experts, n]
-  PD_CHECK(ws_dims.size() == 2, "weight_scale should be 2D");
+  // [n_experts, k // group_size, n]
+  PD_CHECK(ws_dims.size() == 3, "weight_scale should be 3D");
+  // [n_experts, k // group_size, n]
+  PD_CHECK(zeros_dims.size() == 3, "weight_zeros should be 3D");
   // [n_experts]
   PD_CHECK(tokens_per_expert_dims.size() == 1,
            "tokens_per_expert should be 1D");
-  PD_CHECK(group_size == -1);
+  PD_CHECK(group_size == 128);
   auto m = x_dims[0];
   auto k = x_dims[1];
   auto n_experts = w_dims[0];
-  auto n = w_dims[1];
+  auto n = w_dims[1] * 2;
   PD_CHECK(w_dims[2] == k);
   PD_CHECK(ws_dims[0] == n_experts);
-  PD_CHECK(ws_dims[1] == n);
+  PD_CHECK(ws_dims[1] == k / group_size);
+  PD_CHECK(ws_dims[2] == n);
+  PD_CHECK(zeros_dims[0] == n_experts);
+  PD_CHECK(zeros_dims[1] == k / group_size);
+  PD_CHECK(zeros_dims[2] == n);
   PD_CHECK(tokens_per_expert_dims[0] == n_experts);
 
-  PD_CHECK(tokens_per_expert.dtype() == paddle::DataType::INT32);
   PD_CHECK(x.dtype() == paddle::DataType::BFLOAT16 ||
            x.dtype() == paddle::DataType::FLOAT16);
   PD_CHECK(weight.dtype() == paddle::DataType::INT8);
   PD_CHECK(weight_scale.dtype() == x.dtype());
+  PD_CHECK(weight_zeros.dtype() == x.dtype());
+  PD_CHECK(tokens_per_expert.dtype() == paddle::DataType::INT32);
+
   PD_CHECK(x.is_contiguous());
   PD_CHECK(weight.is_contiguous());
   PD_CHECK(weight_scale.is_contiguous());
+  PD_CHECK(weight_zeros.is_contiguous());
+  PD_CHECK(tokens_per_expert.is_contiguous());
 
   auto output = GetEmptyTensor({m, n}, x.dtype(), x.place());
+
   cuinferHandle_t handle = iluvatar::getContextInstance()->getIxInferHandle();
   cuinferPointerMode_t cuinfer_ptr_mode = CUINFER_POINTER_MODE_HOST;
   cuinferOperation_t transa = CUINFER_OP_T;
   cuinferOperation_t transb = CUINFER_OP_N;
-  cudaDataType_t Atype = CUDA_R_8I;
+  cudaDataType_t Atype = CUDA_R_4I;
   cudaDataType_t Btype;
   if (x.dtype() == paddle::DataType::FLOAT16) {
     Btype = CUDA_R_16F;
@@ -89,6 +102,7 @@ std::vector<paddle::Tensor> W8A16GroupGemv(
   cust_device_param.sortedId = nullptr;
   cust_device_param.bias = nullptr;
   cust_device_param.scale = weight_scale.data();
+  cust_device_param.zero = weight_zeros.data();
   cust_device_param.nSize = tokens_per_expert.data<int32_t>();
 
   int lda = k;
@@ -134,7 +148,7 @@ std::vector<paddle::Tensor> W8A16GroupGemv(
                                     weight.data(),
                                     Atype,
                                     lda,
-                                    0,
+                                    0,  // n * k
                                     x.data(),
                                     Btype,
                                     ldb,
@@ -154,23 +168,24 @@ std::vector<paddle::Tensor> W8A16GroupGemv(
   return {output};
 }
 
-std::vector<std::vector<int64_t>> W8A16GroupGemvInferShape(
+std::vector<std::vector<int64_t>> WI4A16GroupGemvInferShape(
     const std::vector<int64_t>& x_shape,
     const std::vector<int64_t>& weight_shape) {
-  return {{x_shape[0], weight_shape[1]}};
+  return {{x_shape[0], weight_shape[1] * 2}};
 }
-std::vector<paddle::DataType> W8A16GroupGemvInferDtype(
+
+std::vector<paddle::DataType> WI4A16GroupGemvInferDtype(
     const paddle::DataType& input_dtype) {
   return {input_dtype};
 }
 
-PD_BUILD_STATIC_OP(w8a16_group_gemv)
+PD_BUILD_STATIC_OP(wi4a16_group_gemv)
     .Inputs(
         {"x", "weight", "weight_scale", "weight_zeros", "tokens_per_expert"})
     .Outputs({"output"})
     .Attrs({
         "group_size:int",
     })
-    .SetKernelFn(PD_KERNEL(W8A16GroupGemv))
-    .SetInferShapeFn(PD_INFER_SHAPE(W8A16GroupGemvInferShape))
-    .SetInferDtypeFn(PD_INFER_DTYPE(W8A16GroupGemvInferDtype));
+    .SetKernelFn(PD_KERNEL(WI4A16GroupGemv))
+    .SetInferShapeFn(PD_INFER_SHAPE(WI4A16GroupGemvInferShape))
+    .SetInferDtypeFn(PD_INFER_DTYPE(WI4A16GroupGemvInferDtype));
