240c8084c7a77bd68c2ed0374f1ff43605c26f3a
zhupengyang <zhu_py@qq.com>
2026-05-13 12:36:21 +0000

    merge pr7805

--- fastdeploy/worker/xpu_model_runner.py
+++ fastdeploy/worker/xpu_model_runner.py
@@ -1610,6 +1610,9 @@ class XPUModelRunner(ModelRunnerBase):
             # 2. Padding inputs for cuda grph
             self.padding_cudagraph_inputs()
 
+            if self.share_inputs["ids_remove_padding"].shape[0] <= 0:
+                return None
+
             # NOTE(wufeisheng): If `not_need_stop`` is False, it means the current worker is in an idle state.
             # This logic is not used in TP (Tensor Parallelism) mode. However, in EP (Expert Parallelism) mode,
             # when there is data on other runner, the current runner is required to execute part of the model.
