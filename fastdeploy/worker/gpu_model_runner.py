c9e1bf59f97a002ad5574d9fc6c4c88fb7d6b23e
RAM <gstian5555@outlook.com>
2026-05-21 11:17:26 +0800

    [RL] Reset buffer size of `slot_mapping` (#7866)
    * Reset buffer size of R3
    
    * refine code

--- fastdeploy/worker/gpu_model_runner.py
+++ fastdeploy/worker/gpu_model_runner.py
@@ -1395,7 +1395,9 @@ class GPUModelRunner(ModelRunnerBase):
         )
         block_size = self.cache_config.block_size
         block_idx = position_ids // block_size  # [num_tokens]
-        assert self.forward_meta.batch_id_per_token.shape == block_idx.shape
+        assert (
+            self.forward_meta.batch_id_per_token.shape == block_idx.shape
+        ), f"batch_id_per_token.shape:{self.forward_meta.batch_id_per_token.shape} != block_idx.shape:{block_idx.shape}"
         block_ids = self.forward_meta.block_tables[self.forward_meta.batch_id_per_token, block_idx]  # [num_tokens]
         block_offset = position_ids % block_size  # [num_tokens]
         slot_mapping = self.share_inputs["slot_mapping_buffer"][:current_total_tokens]
