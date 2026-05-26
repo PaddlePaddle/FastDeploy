0edaab1476bc33f542040203f499612470d2fd77
Tryorish <2575550173@qq.com>
2026-05-26 10:51:38 +0800

    revert

--- fastdeploy/worker/input_batch.py
+++ fastdeploy/worker/input_batch.py
@@ -488,7 +488,7 @@ class InputBatch:
                     self.image_features_list[i2],
                     self.image_features_list[i1],
                 )
-            swap_data(self.rope_emb, i1, i2)
+            swap_data(self.share_inputs["rope_emb"], i1, i2)
             swap_data(self.decode_states, i1, i2)
             swap_data(self.attn_mask_offsets_full, i1, i2)
         # Swap mask rollback
