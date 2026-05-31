7029b29e71a5d6d3ca291773290488382e36f234
zhoutianzi666 <17801055074@163.com>
2026-05-31 16:23:00 +0800

    commit

--- custom_ops/gpu_ops/append_attn/multiquery_attention_c16_impl.cuh
+++ custom_ops/gpu_ops/append_attn/multiquery_attention_c16_impl.cuh
@@ -197,7 +197,7 @@ __global__ void multi_query_append_attention_kernel(
                      : chunk_len) /
       (num_frags_z * 16);
   uint32_t k_smem_offset_r = smem_t::get_permuted_offset<num_vecs_per_head>(
-      8 * (tid / 16) + tid % 8, (tid % 16) / 8);
+      tid / 16 * 8 + tid % 8, tid % 16 / 8);
 
   uint32_t v_smem_offset_r =
       smem_t::get_permuted_offset<num_vecs_per_head>(tid % 16, tid / 16);
