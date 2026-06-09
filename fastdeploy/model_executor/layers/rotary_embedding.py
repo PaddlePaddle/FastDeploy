c7d99e8995cba257313508be45c8527b918815ac
zhoutianzi666 <17801055074@163.com>
2026-06-09 11:28:42 +0800

    commit

--- fastdeploy/model_executor/layers/rotary_embedding.py
+++ fastdeploy/model_executor/layers/rotary_embedding.py
@@ -47,7 +47,7 @@ class ErnieRotaryEmbedding:
         if current_platform.is_xpu() or paddle.is_compiled_with_custom_device("iluvatar_gpu"):
             rot_emb = paddle.zeros((2, bsz, max_seq_len, 1, self.rotary_dim), dtype="float32")
             emb = paddle.concat([freqs, freqs], axis=-1)
-            assert emb.shape == (bsz, max_seq_len, self.rotary_dim)
+            assert emb.shape == [bsz, max_seq_len, self.rotary_dim]
         elif current_platform.is_gcu():
             # shape: [B, S, D]
             rot_emb = paddle.concat([freqs.cos(), freqs.sin()], axis=-1)
