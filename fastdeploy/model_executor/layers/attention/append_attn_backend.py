db7d2606a3a9fd76b0299bc55d2a96099e0644a5
chang-wenbin <changwenbin@baidu.com>
2026-06-10 18:32:17 +0800

    update v_head_dim

--- fastdeploy/model_executor/layers/attention/append_attn_backend.py
+++ fastdeploy/model_executor/layers/attention/append_attn_backend.py
@@ -177,7 +177,7 @@ class AppendAttentionBackend(AttentionBackend):
         self.num_heads: int = num_heads
         self.group_size: int = self.num_heads // self.kv_num_heads
         self.head_dim: int = fd_config.model_config.head_dim
-        self.v_head_dim: int = v_head_dim if v_head_dim is not None else self.head_dim
+        self.v_head_dim: int = getattr(fd_config.model_config, "v_head_dim", self.head_dim)
         self.external_norm_rope: bool = True if v_head_dim != head_dim else False
         self.num_layers: int = fd_config.model_config.num_hidden_layers
 
